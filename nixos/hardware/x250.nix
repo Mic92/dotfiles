a9036dac91889919454031f321cb6eb0dd2994ce
Joerg Thalheim <joerg@thalheim.io>
2017-10-13 18:58:41 +0100

    x250: remove intel graphics

--- nixos/hardware/x250.nix
+++ nixos/hardware/x250.nix
@@ -2,7 +2,7 @@
 
 {
    # modesetting driver leads to freezes with newer kernel at the moment (> 4.4)
-   services.xserver.videoDrivers = [ "intel" ];
+   services.xserver.videoDrivers = [ "modsettings" ];
 
    services.xserver.libinput.enable = true;
    hardware.trackpoint.emulateWheel = true;
