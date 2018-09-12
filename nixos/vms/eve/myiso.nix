ced42bec404da6f6bcf12c173eb8c7af12ba85ce
Jörg Thalheim <joerg@thalheim.io>
2018-09-12 21:45:56 +0100

    cleanup old stuff from eve

--- nixos/vms/eve/myiso.nix
+++
@@ -1,5 +0,0 @@
-(import <nixpkgs/nixos/lib/eval-config.nix> {
- system = builtins.currentSystem;
- modules = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-             ./myconfig.nix ];
-}).config.system.build.isoImage
