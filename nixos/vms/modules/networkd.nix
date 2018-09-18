f1fb4f695087771eb5e5d45f18f59816cf55d0cd
Jörg Thalheim <joerg@thalheim.io>
2018-09-19 00:06:03 +0100

    networkd: also disable systemd-udev-settle.

--- nixos/vms/modules/networkd.nix
+++ nixos/vms/modules/networkd.nix
@@ -1,8 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
+  systemd.network.enable = true;
+  networking.useNetworkd = true;
+
   # often hangs
   systemd.services.systemd-networkd-wait-online.enable = false;
 
   networking.dhcpcd.enable = false;
 
-  #services.resolved.enable = false;
+  # this leads to timeouts for some devices (virtualbox or tinc adapter)
+  systemd.services.systemd-udev-settle.serviceConfig.ExecStart = ["" "${pkgs.coreutils}/bin/true"];
 }
