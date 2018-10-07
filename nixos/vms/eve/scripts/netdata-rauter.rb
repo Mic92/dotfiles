e141ead336b6e98e05e098e4f94149818c81d8ad
Jörg Thalheim <joerg@thalheim.io>
2018-10-07 11:05:24 +0100

    cleanup more stuff

--- nixos/vms/eve/scripts/netdata-rauter.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-req = open('http://rauter.evenet.dn42:19999/api/v1/allmetrics?format=prometheus')
-req.each_line do |l|
-  l.gsub!(/#.*/,"")
-  if l =~ /([^{]+){[^}]+}\s+(\d+)\s+\d+/
-    metric, val = $1, $2
-    puts "#{metric.gsub("_", ".")} #{val} #{Time.new.utc.to_i}"
-  end
-end
