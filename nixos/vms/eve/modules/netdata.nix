482f1861c258fefb3c21912aadd2a715dd869259
Jörg Thalheim <joerg@thalheim.io>
2018-10-12 10:36:23 +0100

    netdata: enable postgres + nginx

--- nixos/vms/eve/modules/netdata.nix
+++ nixos/vms/eve/modules/netdata.nix
@@ -1,3 +1,18 @@
-{
-  services.netdata.enable = true;
+{ pkgs, ... }: {
+  imports = [
+    ../../modules/netdata.nix
+  ];
+  systemd.services.netdata = {
+    serviceConfig.Environment="PYTHONPATH=${pkgs.netdata}/libexec/netdata/python.d/python_modules";
+    path = with pkgs; [
+      (python3.withPackages (ps: [ ps.psycopg2 ]))
+      nodejs
+    ];
+  };
+  environment.etc."netdata/python.d/postgres.conf".text = ''
+    socket:
+      name     : 'local'
+      user     : 'netdata'
+      database : 'postgres'
+  '';
 }
