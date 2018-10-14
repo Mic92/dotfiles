6055132accffd1b317de5aa654cfcd6ff7953eb4
Jörg Thalheim <joerg@thalheim.io>
2018-10-14 12:25:29 +0100

    netdata: expose to tinc vpn

--- nixos/vms/modules/netdata.nix
+++ nixos/vms/modules/netdata.nix
@@ -1,11 +1,18 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   services.netdata = {
     enable = true;
     config = {
       global = {
-        "bind to" = "127.0.0.1:19999 ${config.networking.retiolum.ipv4}:19999 [${config.networking.retiolum.ipv6}]:19999";
+        "bind to" = (lib.concatStringsSep " " [
+          "127.0.0.1:19999"
+          "${config.networking.retiolum.ipv4}:19999"
+          "[${config.networking.retiolum.ipv6}]:19999"
+        ]);
+        "error log" = "stderr";
       };
     };
   };
   networking.firewall.interfaces."tinc.retiolum".allowedTCPPorts = [ 19999 ];
+
+  # TODO create /etc/netdata
 }
