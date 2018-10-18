9f4852a5f0b45399dcda24a10fcb4e8841775fa2
Jörg Thalheim <joerg@thalheim.io>
2018-10-19 00:22:14 +0100

    add icinga2 monitoring

---
+++ nixos/vms/eve/modules/icinga-sync.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  systemd.services.icinga-sync = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.openssh ];
+    serviceConfig = {
+      RuntimeDirectory = "icinga-sync";
+      WorkingDirectory = "/run/icinga-sync/";
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = with pkgs; [
+        "${coreutils}/bin/install -m644 ${./eve.thalheim.io.conf} eve.thalheim.io.conf"
+        "${openssh}/bin/scp -o 'ProxyJump mic92@srv04.bsd.services' eve.thalheim.io.conf \\
+          mic92@icingamaster:eve.thalheim.io.conf"
+        "${openssh}/bin/ssh -J mic92@srv04.bsd.services mic92@icingamaster \\
+          'sudo systemctl reload icinga2 || sudo icinga2 daemon -C'"
+      ];
+    };
+  };
+}
