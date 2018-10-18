a47736da2edccbef422278027150c8c181222cc4
Jörg Thalheim <joerg@thalheim.io>
2018-10-18 11:00:50 +0100

    retire monit

--- nixos/vms/eddie.nix
+++ nixos/vms/eddie.nix
@@ -19,7 +19,6 @@
         ./modules/users.nix
         ./modules/retiolum.nix
         ./modules/mosh.nix
-        ./modules/monit.nix
         ./modules/overlay.nix
         ./modules/tracing.nix
         ./modules/packages.nix
