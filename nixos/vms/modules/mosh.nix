a3c0621b4bda8690642d81add97c1717ef94ea59
Jörg Thalheim <joerg@thalheim.io>
2018-10-26 12:20:53 +0100

    mosh: fix ssh service

--- nixos/vms/modules/mosh.nix
+++ nixos/vms/modules/mosh.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, ... }: {
   programs.mosh.enable = true;
 
-  networking.firewall.allowedUDPPortRanges = lib.optionals (config.openssh.enable) [
+  networking.firewall.allowedUDPPortRanges = lib.optionals (config.services.openssh.enable) [
     # Mosh
     { from = 60000; to = 60010; }
   ];
