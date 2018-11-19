61b470add1086ed72038d9aaf81cabd2ea9aad8b
Jörg Thalheim <joerg@thalheim.io>
2018-11-19 10:50:05 +0000

    cntr: simplify build environment

--- nixos/environments/cntr/default.nix
+++ nixos/environments/cntr/default.nix
@@ -2,35 +2,10 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
   buildInputs = [
-    rustfmt
-    gdbgui
-    fuse
-    bc
-    gdbm
-    xfsprogs
-    libtool
-    autoreconfHook
-    pkgconfig
-    m4
-    bashInteractive
-    dbench
-    fio
-    glibc.debug
-    gperftools
-    # kcachegrind
-    gnumake
     rustc
     cargo
-    #rustup
-    #musl
-    #latest.rustChannels.nightly.rust
-    #latest.rustChannels.nightly.cargo
-
-    #(e2fsprogs.overrideAttrs (old: {
-    #  patches = [./0001-fix-stackoverflow-in-fgetversion.patch];
-    #}))
-  ] ++ xfstests.buildInputs
-    ++ xfstests.nativeBuildInputs;
+    rustup
+  ];
   hardeningDisable = [ "format" ];
   DUMP_OUTPUT="true";
 }
