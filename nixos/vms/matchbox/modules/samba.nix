1aad6d3c6fb64cea7b7d719af6cef8ddb66d2f22
Jörg Thalheim <joerg@thalheim.io>
2019-01-05 00:32:20 +0100

    matchbox/samba: make configuration fancy

--- nixos/vms/matchbox/modules/samba.nix
+++ nixos/vms/matchbox/modules/samba.nix
@@ -1,43 +1,55 @@
 { pkgs, ... }: {
-  services.samba.enable = true;
-  services.samba.package = pkgs.sambaFull;
-  services.samba.extraConfig = ''
-    workgroup = WORKGROUP
-    netbios name = MATCHBOX
-    server string = Raspberry Pi
-    #hosts allow = 192.168.1. 127.0.0.1 169.254. 172.23.75.
-    interfaces = eth*
-    map to guest = Bad User
-    max log size = 50
-    dns proxy = no
-    load printers = yes
-    printing = cups
-    printcap name = cups
-    security = user
-    log level = 3
+  services.samba = {
+    enable = true;
+    package = pkgs.sambaFull;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      netbios name = MATCHBOX
+      server string = Raspberry Pi
+      #hosts allow = 192.168.1. 127.0.0.1 169.254. 172.23.75.
+      interfaces = eth*
+      map to guest = Bad User
+      max log size = 50
+      dns proxy = no
+      load printers = yes
+      printing = cups
+      printcap name = cups
+      security = user
+    '';
+    shares = {
+      printers = {
+        comment = "All Printers";
+        path = "/var/spool/samba";
+        public = "yes";
+        browseable = "yes";
+        # to allow user 'guest account' to print.
+        "guest ok" = "yes";
+        writable = "no";
+        printable = "yes";
+        "create mode" = 0700;
+      };
+      public = {
+        comment = "Netzlaufwerk";
+        path = "/mnt/hdd/public";
+        public = "yes";
+        "only guest" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "2777";
+        writable = "yes";
+        printable = "no";
+      };
+    };
+  };
 
-    [printers]
-      comment = All Printers
-      path = /var/spool/samba
-      public = yes
-      browseable = yes
-      # to allow user 'guest account' to print.
-      guest ok = yes
-      writable = no
-      printable = yes
-      create mode = 0700
+  systemd.tmpfiles.rules = [
+    "d /var/spool/samba 1777 root root -"
+  ];
 
-    [public]
-      comment = Netzlaufwerk
-      path = /mnt/hdd/public
-      public = yes
-      only guest = yes
-      create mask = 0644
-      directory mask = 2777
-      writable = yes
-      printable = no
-  '';
+  networking.firewall.allowedTCPPorts = [
+    139 445 # smbd
+  ];
 
-  networking.firewall.allowedTCPPorts = [ 139 445 ];
-  networking.firewall.allowedUDPPorts = [ 631 ];
+  networking.firewall.allowedUDPPorts = [
+    137 138 # nmbd
+  ];
 }
