a75e81e5e6ce11c677893cc5b04991e50f4b1101
Jörg Thalheim <joerg@thalheim.io>
2019-01-18 17:20:48 +0000

    Start emacs via socket activation

--- nixpkgs-config/emacs.nix
+++ nixpkgs-config/emacs.nix
@@ -1,9 +1,18 @@
-{ pkgs, ...}:
-{
-  programs.emacs.package = pkgs.emacs;
-  programs.emacs.enable = true;
+{ pkgs, ... }:
 
+let
+  editorScript = pkgs.writeScriptBin "emacseditor" ''
+    #!${pkgs.runtimeShell}
+    if [ -z "$1" ]; then
+      exec ${pkgs.emacs}/bin/emacsclient -s $XDG_RUNTIME_DIR/emacs --create-frame --alternate-editor ${pkgs.emacs}/bin/emacs
+    else
+      exec ${pkgs.emacs}/bin/emacsclient -s $XDG_RUNTIME_DIR/emacs --alternate-editor ${pkgs.emacs}/bin/emacs "$@"
+    fi
+  '';
+
+in {
   home.packages = with pkgs; [
+    editorScript
     gocode
     godef
     gocode
@@ -12,4 +21,20 @@
     impl
     gometalinter
   ];
+
+  systemd.user.sockets.emacs-daemon = {
+    Socket.ListenStream = "%t/emacs";
+    Install.WantedBy = [ "sockets.target" ];
+  };
+
+  systemd.user.services.emacs-daemon = {
+    Unit.RefuseManualStart = true;
+    Service = {
+      Type = "forking";
+      ExecStart = "${pkgs.zsh}/bin/zsh -c 'source ~/.zshrc; exec ${pkgs.emacs}/bin/emacs --daemon'";
+      ExecStop = "${pkgs.emacs}/bin/emacsclient --eval (kill-emacs)";
+      Restart = "always";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
 }
