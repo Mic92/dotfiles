1b3b108adabc6039c78203dddab27b7900e95076
Jörg Thalheim <joerg@thalheim.io>
2019-11-19 13:04:04 +0000

    eve: update krops & nixpkgs

--- nixos/vms/krops.nix
+++ nixos/vms/krops.nix
@@ -1,8 +1,8 @@
 let
   krops = (import <nixpkgs> {}).fetchgit {
     url = https://cgit.krebsco.de/krops/;
-    rev = "ad4c3cad0a04d1e733b3d3007d4a1236b0b66353";
-    sha256 = "0v07hpjywjfbyfg7zz0kdd2n4wwz7f1qs4mccqsa5zvrbhsd9dvq";
+    rev = "refs/tags/v1.17.0";
+    sha256 = "150jlz0hlb3ngf9a1c9xgcwzz1zz8v2lfgnzw08l3ajlaaai8smd";
   };
 
   lib = import "${krops}/lib";
@@ -11,7 +11,7 @@ let
   source = lib.evalSource [{
     nixpkgs.git = {
       clean.exclude = ["/.version-suffix"];
-      ref = "28ce25204a349892878e489328498db35517a9be";
+      ref = "d0e66064bc964859a87570b19c7f47a0ca79f97c";
       url = https://github.com/Mic92/nixpkgs;
     };
     dotfiles.file.path = toString ./../..;
