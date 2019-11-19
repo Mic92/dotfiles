3ee465dc516626e9b68360b0a83a66272a6424c7
Jörg Thalheim <joerg@thalheim.io>
2019-11-19 13:03:30 +0000

    install-iso: right efi flags & chmod tmp

--- nixos/images/install-iso.nix
+++ nixos/images/install-iso.nix
@@ -2,7 +2,7 @@
 # $ nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=./install-iso.nix
 # $ dd if=result/iso/nixos-*.iso of=/dev/sdb
 # iso>
-# iso> sgdisk -n 1:0:+500M -N 2 -t 1:ef02 -t 2:8304 /dev/sda
+# iso> sgdisk -n 1:0:+500M -N 2 -t 1:ef00 -t 2:8304 /dev/sda
 # iso> mkfs.vfat -b32 /dev/sda1
 # iso> zpool create -f zroot /dev/disk/by-partuuid/046fdb0b-114f-4435-9d8c-957ac73b5cd2
 # iso> nc -6 -w 120 -l -p 8023 | zfs receive -F zroot
@@ -34,5 +34,6 @@
 # mount /dev/sda1 /mnt/boot/
 # mount -t zfs zroot/root/home /mnt/home/
 # mount -t zfs zroot/root/tmp /mnt/tmp/
+# chmod 777 /mnt/tmp
 # nixos-generate-config  --root /mnt
 # nixos-install
