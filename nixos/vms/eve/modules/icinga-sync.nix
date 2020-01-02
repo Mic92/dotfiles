99001afa345fd76b52fc11d514250b4322602cca
Jörg Thalheim <joerg@thalheim.io>
2020-01-02 19:54:15 +0000

    icinga-sync: new jumphost

--- nixos/vms/eve/modules/icinga-sync.nix
+++ nixos/vms/eve/modules/icinga-sync.nix
@@ -10,9 +10,9 @@
       RemainAfterExit = true;
       ExecStart = with pkgs; [
         "${coreutils}/bin/install -m644 ${./eve.thalheim.io.conf} eve.thalheim.io.conf"
-        "${openssh}/bin/scp -o 'ProxyJump mic92@srv04.bsd.services' eve.thalheim.io.conf \\
+        "${openssh}/bin/scp -o 'ProxyJump mic92@fw02.bsd.services' eve.thalheim.io.conf \\
           mic92@icingamaster:eve.thalheim.io.conf"
-        "${openssh}/bin/ssh -J mic92@srv04.bsd.services mic92@icingamaster \\
+        "${openssh}/bin/ssh -J mic92@fw02.bsd.services mic92@icingamaster \\
           'sudo service icinga2 reload || sudo icinga2 daemon -C'"
       ];
     };
