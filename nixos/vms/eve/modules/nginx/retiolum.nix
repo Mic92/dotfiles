79d470b936178f1d9f6f9cc39f9658f1307362b3
Jörg Thalheim <joerg@thalheim.io>
2020-01-03 13:35:09 +0000

    retiolum: add stockholm auto-merge

--- nixos/vms/eve/modules/nginx/retiolum.nix
+++ nixos/vms/eve/modules/nginx/retiolum.nix
@@ -1,7 +1,38 @@
+{ pkgs, ... }:
+
 {
   services.nginx.virtualHosts."retiolum.thalheim.io" = {
     useACMEHost = "thalheim.io";
     forceSSL = true;
     root = "/var/lib/gogs/builds/retiolum.thalheim.io";
+  };
+
+  systemd.timers.stockholm-autosync = {
+    wantedBy = ["multi-user.target"];
+    timerConfig.OnBootSec="60min";
+    timerConfig.OnUnitActiveSec="60min";
+  };
+
+  systemd.services.stockholm-autosync = {
+    script = ''
+      set -eux -o pipefail
+      readonly workdir=/var/lib/gogs/builds/stockholm-auto-update
+      readonly downstream=gogs@git.thalheim.io:Mic92/stockholm
+      readonly upstream=https://cgit.krebsco.de/stockholm 
+      if [[ ! -d "$workdir" ]]; then
+        git clone "$downstream" "$workdir"
+      fi
+      cd "$workdir"
+      git fetch origin master
+      git reset --hard origin/master
+      git pull --rebase "$upstream" master
+      git push origin +master
+    '';
+    path = [ pkgs.git ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "gogs";
+    };
   };
 }
