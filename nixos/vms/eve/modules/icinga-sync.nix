8261f24b73f95f99cc88151b5e9c5892bfc6b3c0
Jörg Thalheim <joerg@thalheim.io>
2020-01-04 06:21:37 +0000

    eve/icinga: add api password, fix sieve port

--- nixos/vms/eve/modules/icinga-sync.nix
+++ nixos/vms/eve/modules/icinga-sync.nix
@@ -2,19 +2,26 @@
 {
   systemd.services.icinga-sync = {
     wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.openssh ];
+    path = with pkgs; [ openssh gnused ];
+    script = ''
+      sed \
+        -e "s!@API_PASSWORD@!$(</run/keys/icinga-api-password)!" \
+        -e "s!@PUSHOVER_EMAIL@!$(</run/keys/icinga-pushover-email)!" \
+        ${./eve.thalheim.io.conf} \
+        > eve.thalheim.io.conf
+      scp -o 'ProxyJump mic92@fw02.bsd.services' eve.thalheim.io.conf \
+        mic92@icingamaster:eve.thalheim.io.conf
+      ssh -J mic92@fw02.bsd.services mic92@icingamaster \
+        'sudo service icinga2 reload || sudo icinga2 daemon -C'
+    '';
     serviceConfig = {
       RuntimeDirectory = "icinga-sync";
       WorkingDirectory = "/run/icinga-sync/";
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = with pkgs; [
-        "${coreutils}/bin/install -m644 ${./eve.thalheim.io.conf} eve.thalheim.io.conf"
-        "${openssh}/bin/scp -o 'ProxyJump mic92@fw02.bsd.services' eve.thalheim.io.conf \\
-          mic92@icingamaster:eve.thalheim.io.conf"
-        "${openssh}/bin/ssh -J mic92@fw02.bsd.services mic92@icingamaster \\
-          'sudo service icinga2 reload || sudo icinga2 daemon -C'"
-      ];
     };
   };
+
+  krops.secrets.files.icinga-api-password = {};
+  krops.secrets.files.icinga-pushover-email = {};
 }
