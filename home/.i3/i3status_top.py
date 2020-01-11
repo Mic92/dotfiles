bfbb567510e3c143a7a27e74fefe7e4570e648a6
Jörg Thalheim <joerg@thalheim.io>
2020-01-11 08:14:51 +0000

    i3: add batteries dynamically

--- home/.i3/i3status_top.py
+++ home/.i3/i3status_top.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import glob
+import os
 import subprocess
 
 import color
@@ -26,19 +28,21 @@ status.register(
 icons.nerdfont(status, "")
 
 bat_status = {"DIS": "-", "CHR": "+", "FULL": "↯"}
-for bat in ["BAT0"]:
+for bat_path in glob.glob("/sys/class/power_supply/BAT*"):
+    bat = os.path.basename(bat_path)
     status.register(
         "battery",
         format="{status}{percentage:.2f}% [{glyph}] {remaining:%E%hh:%Mm}",
         alert=True,
-        alert_percentage=5,
+        alert_percentage=10,
         battery_ident=bat,
         status=bat_status,
         color=color.text_normal,
         charging_color=color.text_normal,
         full_color=color.text_normal,
         critical_color=color.text_down,
-        hints={"separator": False, "separator_block_width": 5} if bat == "BAT1" else {},
+        hints={"separator": False,
+               "separator_block_width": 5} if bat == "BAT1" else {},
     )
 
 icons.nerdfont(status, "")
