0d1f82e5d8a9fa600928b66680c19d07045ecaf6
Jörg Thalheim <joerg@thalheim.io>
2020-01-28 23:59:52 +0000

    home-assistant/weather: switch back to hourly notifications

--- nixos/eve/modules/home-assistant/weather.nix
+++ nixos/eve/modules/home-assistant/weather.nix
@@ -1,21 +1,26 @@
  { ... }: {
    services.home-assistant.config = {
      script = {
-       notify_weather.sequence = [{
-         service = "notify.mobile_app_jorg_s_xiaomi";
+       notify_weather.sequence = let
          data_template = {
            title = "Weather";
-           message = ''{{ states("sensor.dark_sky_summary_0d") }} (rain propability {{ states("sensor.dark_sky_precip_probability_0d") }}%)'';
-           data.photo.url = ''https://hass.thalheim.io{{ state_attr("sensor.dark_sky_summary_0d", "entity_picture")}}'';
+           message = ''{{ states("sensor.dark_sky_summary_0h") }} (rain propability {{ states("sensor.dark_sky_precip_probability_0h") }}%)'';
+           data.photo.url = ''https://hass.thalheim.io{{ state_attr("sensor.dark_sky_summary_0h", "entity_picture")}}'';
          };
+       in [{
+         service = "notify.mobile_app_jorg_s_xiaomi";
+         inherit data_template;
        } {
          service = "notify.mobile_app_beatrice";
-         data_template = {
-           title = "Weather";
-           message = ''{{ states("sensor.dark_sky_summary_0d") }}'';
-           data.photo.url = ''https://hass.thalheim.io{{ state_attr("sensor.dark_sky_summary_0d", "entity_picture")}}'';
-         };
+         inherit data_template;
+       } {
+         service = "input_boolean.turn_on";
+         entity_id = "input_boolean.rain_notified_today";
        }];
+     };
+     input_boolean.rain_notified_today = {
+       name = "Rain notified today";
+       icon = "mdi:weather-cloudy";
      };
      sensor = [{
        platform = "darksky";
@@ -32,16 +37,26 @@
          "precip_type"
          "precip_probability"
        ];
-       forecast = [ 0 ];
+       hourly_forecast = [ 0 ];
      }];
-    automations = [{
-      alias = "Rainy day notification";
+     automation = [{
+       alias = "Rainy day notification";
+       trigger = {
+         platform = "state";
+         entity_id = "sensor.dark_sky_precip_0h";
+         to = "rain";
+       };
+       action.service = "script.notify_weather";
+    } {
+      alias = "Reset rain notified today";
       trigger = {
-        platform = "state";
-        entity_id = "sensor.dark_sky_precip_0d";
-        to = "rain";
+        platform = "time";
+        at = "00:00:01";
       };
-      action.service = "script.notify_weather";
+      action = [{
+        service = "input_boolean.turn_off";
+        entity_id = "input_boolean.rain_notified_today";
+      }];
     }];
    };
  }
