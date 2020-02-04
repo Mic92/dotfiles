00264b4a857bc3f0ed71cdae07eb9d0995767f35
Jörg Thalheim <joerg@thalheim.io>
2020-02-04 11:20:17 +0000

    add wee-slack

--- nixpkgs-config/eve.nix
+++ nixpkgs-config/eve.nix
@@ -4,5 +4,16 @@
   imports = [
     ./common.nix
   ];
-  home.packages = [ (pkgs.callPackage ./weechat.nix {})];
+  home.packages = with pkgs; [
+    (weechat.override {
+      configure = { availablePlugins, ... }: {
+        scripts = with weechatScripts; [ weechat-otr wee-slack ];
+        plugins = [
+          availablePlugins.python
+          availablePlugins.perl
+          availablePlugins.lua
+        ];
+      };
+    })
+  ];
 }
