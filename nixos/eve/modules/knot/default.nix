9a3ee4b64b7a8254b3c39abc7bebee1ca85b94af
Jörg Thalheim <joerg@thalheim.io>
2020-02-07 15:41:06 +0000

    knot: enable dnssec & ratelimiting

--- nixos/eve/modules/knot/default.nix
+++ nixos/eve/modules/knot/default.nix
@@ -18,23 +18,47 @@ in {
       remote:
         - id: he_ip4
           address: 216.218.130.2
-        - id: he_ip6
-          address: 2001:470:100::2
+        # does not accept NOTIFY yet
+        #- id: he_ip6
+        #  address: 2001:470:100::2
 
       acl:
         - id: he_acl
           key: he1
           action: transfer
 
+      mod-rrl:
+        - id: default
+          rate-limit: 200   # Allow 200 resp/s for each flow
+          slip: 2           # Every other response slips
+
+      policy:
+        - id: rsa2k
+          algorithm: RSASHA256
+          ksk-size: 4096
+          zsk-size: 2048
+
+      template:
+        - id: default
+          semantic-checks: on
+          global-module: mod-rrl/default
+
+        - id: master
+          storage: /var/lib/knot/signed
+          semantic-checks: on
+          dnssec-signing: on
+          dnssec-policy: rsa2k
+          notify: [ he_ip4, he_ip6 ]
+          acl: [ he_acl ]
+          zonefile-load: difference
+
       zone:
-        - domain: thalheim.io
-          file: "${./thalheim.io.zone}"
-          notify: [he_ip4, he_ip6]
-          acl: [he_acl]
+        #- domain: thalheim.io
+        #  file: "${./thalheim.io.zone}"
+        #  template: master
         - domain: higgsboson.tk
           file: "${./higgsboson.tk.zone}"
-          notify: [he_ip4, he_ip6]
-          acl: [he_acl]
+          template: master
     '';
   };
   systemd.services.knot = {
