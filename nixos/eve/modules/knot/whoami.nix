354d066f062da6f2da7f3c3ad1ae660aad98151c
Jörg Thalheim <joerg@thalheim.io>
2020-02-07 13:36:05 +0000

    eve/knot/whoami: remove dual stack whoami

--- nixos/eve/modules/knot/whoami.nix
+++ nixos/eve/modules/knot/whoami.nix
@@ -13,20 +13,23 @@
         1 ; MINIMUM
       )
       $TTL 86400
-      @ NS ns1.thalheim.io.
+      @ NS ns1
+      ${lib.optionalString ipv4 ''
+        ns1 A ${config.networking.eve.ipv4.address}
+      ''}
+      ${lib.optionalString ipv6 ''
+        ns1 AAAA ${lib.head config.networking.eve.ipv6.addresses}
+      ''}
     '';
 in {
   services.knot = {
     extraConfig = ''
       zone:
-      - domain: whoami.thalheim.io
-        file: "${whoamiZone { domain = "whoami.thalheim.io"; ipv4 = true; ipv6 = true; }}"
-        module: mod-whoami
       - domain: whoami4.thalheim.io
-        file: "${whoamiZone { domain = "whoami4.thalheim.io"; ipv4 = true; ipv6 = false; }}"
+        file: "${whoamiZone { domain = "whoami4.thalheim.io"; ipv4 = true; }}"
         module: mod-whoami
       - domain: whoami6.thalheim.io
-        file: "${whoamiZone { domain = "whoami6.thalheim.io"; ipv4 = false; ipv6 = true; }}"
+        file: "${whoamiZone { domain = "whoami6.thalheim.io"; ipv6 = true; }}"
         module: mod-whoami
     '';
   };
