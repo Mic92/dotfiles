ffe95f62c12a3222edc4dbf616775980fa028de6
Jörg Thalheim <joerg@thalheim.io>
2020-02-11 19:35:29 +0000

    stubby: documentation on how to add pubkey fingerprint

--- nixos/turingmachine/modules/stubby.nix
+++ nixos/turingmachine/modules/stubby.nix
@@ -2,6 +2,8 @@
   services.stubby = {
     enable = true;
     upstreamServers = ''
+      # Compute the tls_pubkey_pinset:
+      # $ openssl rsa -in /var/lib/acme/thalheim.io/key.pem -outform der -pubout | openssl dgst -sha256 -binary | openssl enc -base64
       - address_data: 95.216.112.61
         tls_auth_name: "dns.thalheim.io"
         tls_pubkey_pinset:
