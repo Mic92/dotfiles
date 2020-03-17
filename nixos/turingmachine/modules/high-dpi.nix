a10654b99e30cdc2e14a19795bc36d0aeeb77fd4
Jörg Thalheim <joerg@thalheim.io>
2020-03-17 11:57:25 +0000

    high-dpi: set hidpi fonts in early boot & in systemd

--- nixos/turingmachine/modules/high-dpi.nix
+++ nixos/turingmachine/modules/high-dpi.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   console.font =
     "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  console.earlySetup = true;
+  boot.loader.systemd-boot.consoleMode = "max";
+
   services.xserver.dpi = 180;
   environment.variables = {
     GDK_SCALE = "2";
