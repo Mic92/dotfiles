bc1837999bbc43219bcc07efe5ec31a0da95e30f
Jörg Thalheim <joerg@thalheim.io>
2020-04-12 07:56:52 +0100

    bitwarden: only request password once, show retry count

--- nixpkgs-config/bitwarden.nix
+++ nixpkgs-config/bitwarden.nix
@@ -1,30 +1,40 @@
 { pkgs, lib, ... }:
 let
   path = lib.makeBinPath ([
-      pkgs.coreutils
-      pkgs.bitwarden-cli
-      pkgs.systemd
-      pkgs.gnused
+    pkgs.coreutils
+    pkgs.utillinux
+    pkgs.bitwarden-cli
+    pkgs.systemd
+    pkgs.gnused
   ]);
   askpass = "${pkgs.gnome3.seahorse}/libexec/seahorse/ssh-askpass";
   bw = pkgs.writeScriptBin "bw" ''
     #!${pkgs.runtimeShell}
     export PATH=${path};
     token_file=/run/user/$(id -u)/bw-token
+    lock_file=/run/user/$(id -u)/bw-token.lock
 
     export DISPLAY="$(systemctl --user show-environment | sed 's/^DISPLAY=\(.*\)/\1/; t; d')"
 
     if [[ -e "$token_file" ]]; then
       export BW_SESSION=$(<"$token_file")
     else
-      for i in $(seq 1 4); do
-        password=$(${askpass} "bitwarden")
-        export BW_SESSION=$(bw unlock --raw "$password")
-        if [[ "$BW_SESSION" != "" ]]; then
-          echo "$BW_SESSION" > "$token_file"
-          break
-        fi
+      exec 9>"$lock_file"
+      while ! flock -n 9; do
+        sleep 1
       done
+      if [[ -e "$token_file" ]]; then
+        export BW_SESSION=$(<"$token_file")
+      else
+        for i in $(seq 1 4); do
+          password=$(${askpass} "bitwarden (try $i/4)")
+          export BW_SESSION=$(bw unlock --raw "$password")
+          if [[ "$BW_SESSION" != "" ]]; then
+            echo "$BW_SESSION" > "$token_file"
+            break
+          fi
+        done
+      fi
     fi
 
     exec bw "$@"
