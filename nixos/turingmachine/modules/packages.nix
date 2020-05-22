6ae5a9c6fa17b46aee9f439c7ff71fcb654a066b
Jörg Thalheim <joerg@thalheim.io>
2020-05-22 11:27:50 +0100

    switch to stable zfs/kernel packages

--- nixos/turingmachine/modules/packages.nix
+++ nixos/turingmachine/modules/packages.nix
@@ -1,11 +1,6 @@
 { pkgs, config, ... }:
 
 {
-  boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
-    zfs.enableUnstable = true;
-  };
-
   imports = [ ../../modules/packages.nix ];
 
   environment.systemPackages = with pkgs; [ cntr ntfs3g ];
