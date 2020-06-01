6e4a20f9e423c2f30802883e09ad44464067b7d6
Jörg Thalheim <joerg@thalheim.io>
2020-06-01 22:44:26 +0100

    homeassistant/timer: use speech.text callback to return feedback

--- nixos/eve/modules/home-assistant/timer.nix
+++ nixos/eve/modules/home-assistant/timer.nix
@@ -9,43 +9,45 @@
     };
     timer.rhasspy = {};
     intent = {};
-    intent_script.GetTime = {
-      action = {
-        service = "rest_command.tts";
-        data_template.message = "It is {{ now().hour }}:{{ now().minute }}";
+    intent_script = {
+      GetTime.speech.text = "It is {{ now().hour }}:{{ now().minute }}";
+      SetTimer = {
+        speech.text = "Set a timer for {{ duration }} {{ unit }}";
+        action = [{
+          service = "timer.start";
+          entity_id = "timer.rhasspy";
+          data_template.duration = ''{% if unit == "seconds" %}
+              {{ duration }}
+            {% elif unit == "minutes" %}
+              {{ duration * 60 }}
+            {% elif unit == "hours" %}
+              {{ duration * 60 * 60 }}
+            {% endif %}
+          '';
+        }];
       };
-    };
-    intent_script.SetTimer = {
-      action = [{
-        service = "timer.start";
-        entity_id = "timer.rhasspy";
-        data_template.duration = ''{% if unit == "seconds" %}
-            {{ duration }}
-          {% elif unit == "minutes" %}
-            {{ duration * 60 }}
-          {% elif unit == "hours" %}
-            {{ duration * 60 * 60 }}
-          {% endif %}
-        '';
-      } {
-        service = "rest_command.tts";
-        data_template.message = "Set a timer for {{ duration }} {{ unit }}";
-      }];
+      CancelTimer = {
+        speech.text = ''Timer for {{ state_attr("timer.rhasspy", "duration") }} canceled'';
+        action = {
+          service = "timer.cancel";
+          entity_id = "timer.rhasspy";
+        };
+      };
     };
     automation = [{
       alias = "Timer is up notification";
-      trigger = {
-        platform = "event";
-        event_type = "timer.finished";
-        event_data.entity_id = "timer.rhasspy";
-      };
-      action = [{
-        service = "notify.pushover";
-        data_template.message = ''timer for {{ state_attr("timer.rhasspy", "duration") }} is up!'';
-      } {
-        service = "rest_command.tts";
-        data_template.message = ''Timer for {{ state_attr("timer.rhasspy", "duration") }} is up!'';
-      }];
+        trigger = {
+          platform = "event";
+          event_type = "timer.finished";
+          event_data.entity_id = "timer.rhasspy";
+        };
+        action = [{
+          service = "notify.pushover";
+          data_template.message = ''timer for {{ state_attr("timer.rhasspy", "duration") }} is up!'';
+        } {
+          service = "rest_command.tts";
+          data_template.message = ''Timer for {{ state_attr("timer.rhasspy", "duration") }} is up!'';
+        }];
     }];
   };
 }
