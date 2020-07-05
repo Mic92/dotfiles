0f5b09644ba495568ddf756ae2aae81f5c0aad25
Jörg Thalheim <joerg@thalheim.io>
2020-07-05 08:17:30 +0100

    hass: talkback intent

--- nixos/eve/modules/home-assistant/noops.nix
+++ nixos/eve/modules/home-assistant/noops.nix
@@ -2,5 +2,6 @@
   services.home-assistant.config.intent_script = {
     Noop.speech.text = "Fine, whatever...";
     FuckOff.speech.text = "Fuck yourself.";
+    TalkBack.speech.text = "You started it, arsehole.";
   };
 }
