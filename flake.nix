e9c8db94e40e1337a3bc1c97abf58c8f2b635c8c
Jörg Thalheim <joerg@thalheim.io>
2020-07-06 12:33:58 +0100

    change to sops-nix github repository

--- flake.nix
+++ flake.nix
@@ -3,15 +3,18 @@
   description = "NixOS configuration with flakes";
 
   inputs.nixpkgs.url = github:Mic92/nixpkgs/master;
-  inputs.sops-nix.url = "/home/joerg/git/sops-nix";
+  inputs.nur.url = github:nix-community/NUR;
+  inputs.sops-nix.url = github:Mic92/sops-nix;
+  # for development
+  #inputs.sops-nix.url = "/home/joerg/git/sops-nix";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixos-hardware.url = github:Mic92/nixos-hardware/master;
 
-  outputs = { self, nixpkgs, nixos-hardware, sops-nix }: {
+  outputs = { self, nixpkgs, nixos-hardware, sops-nix, nur }: {
     nixosConfigurations.turingmachine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        { system.configurationRevision = self.rev; }
+        { nixpkgs.overlays = [ nur.overlay ]; }
         nixos-hardware.nixosModules.dell.xps-13-9380
         sops-nix.nixosModules.sops
         ./nixos/turingmachine/configuration.nix
