4fe8bd4bc825a2ec57aef7537a185e5c7b6e6318
Jörg Thalheim <joerg@thalheim.io>
2020-08-05 23:24:27 +0100

    add repl.nix to load nixos configuration

---
+++ nixos/repl.nix
@@ -0,0 +1,6 @@
+# USAGE: nix repl ./repl.nix --argstr hostname <hostname>
+let
+  currentHostname = builtins.head (builtins.match "([a-zA-Z0-9]+)\n" (builtins.readFile "/etc/hostname"));
+in
+{ hostname ? currentHostname }:
+(builtins.getFlake (toString ./..)).nixosConfigurations.${hostname}
