0ac98dd15515bf95d3803e01ce1aee8c3c6eeeeb
Jörg Thalheim <joerg@thalheim.io>
2020-08-08 08:14:06 +0100

    add ec2 instance

---
+++ nixos/ec2/default.nix
@@ -0,0 +1,14 @@
+let
+  flake = builtins.getFlake (toString ./../..);
+in
+with import flake.inputs.nixpkgs {};
+mkShell {
+  sopsPGPKeys = [
+    "../secrets/keys/users/mic92.asc"
+  ];
+  nativeBuildInputs = [
+    bashInteractive
+    (terraform.withPlugins (p: [ p.aws ]))
+    (pkgs.callPackage flake.inputs.sops-nix {}).sops-pgp-hook
+  ];
+}
