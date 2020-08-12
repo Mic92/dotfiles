c76087859daa4866fa711c83c091dc459b48806a
Jörg Thalheim <joerg@thalheim.io>
2020-08-12 08:07:27 +0100

    turingmachine/nfs-dl: drop dependency on tinc

--- nixos/turingmachine/modules/nfs-dl.nix
+++ nixos/turingmachine/modules/nfs-dl.nix
@@ -13,7 +13,6 @@
       "x-systemd.automount"
       "x-systemd.device-timeout=1"
       "x-systemd.idle-timeout=1min"
-      "x-systemd.requires=sys-devices-virtual-net-tinc.retiolum.device"
     ];
   };
 }
