26a85452c77180f0f9929b61eff46753a61badf1
Jörg Thalheim <joerg@thalheim.io>
2020-08-19 19:37:20 +0100

    eva/openldap: switch to settings

--- nixos/eva/modules/openldap.nix
+++ nixos/eva/modules/openldap.nix
@@ -3,31 +3,53 @@
 {
   services.openldap = {
     enable = true;
-    suffix = "dc=eve";
-    rootdn = "cn=admin,dc=eve";
-    rootpwFile = config.sops.secrets.openldap-rootpw.path;
-    extraConfig = ''
-      access to attrs=userPassword
-        by self         write
-        by anonymous    auth
-        by *            none
-      access to attrs=loginShell
-        by self write
-        by * read
-      access to *
-        by * read
 
-      syncrepl rid=123
-        provider=ldap://eve.r:389
-        type=refreshOnly
-        interval=01:00:00:00
-        searchbase="dc=eve"
-        filter="*"
-        schemachecking=off
-        bindmethod=simple
-        binddn="cn=ldapsync,ou=system,ou=users,dc=eve"
-        include ${config.sops.secrets.ldapsync-password.path}
-    '';
+    defaultSchemas = null;
+    dataDir = null;
+    database = null;
+
+    settings.children."cn=schema".includes = [
+      "${pkgs.openldap}/etc/schema/core.ldif"
+      "${pkgs.openldap}/etc/schema/cosine.ldif"
+      "${pkgs.openldap}/etc/schema/inetorgperson.ldif"
+      "${pkgs.openldap}/etc/schema/nis.ldif"
+    ];
+
+    settings.children."olcDatabase={1}mdb" = {
+      attrs = {
+        objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
+        olcDatabase = "{1}mdb";
+        olcDbDirectory = "/var/db/openldap";
+        olcRootPW.path = config.sops.secrets.openldap-rootpw.path;
+        olcRootDN = "cn=admin,dc=eve";
+        olcSuffix = "dc=eve";
+        olcAccess = [
+          ''{0}to attrs=userPassword
+               by self write  by anonymous auth
+               by dn.base="cn=dovecot,dc=mail,dc=eve" read
+               by dn.base="cn=gitlab,ou=system,ou=users,dc=eve" read
+               by dn.base="cn=ldapsync,ou=system,ou=users,dc=eve"
+               read by * none''
+          ''{1}to attrs=loginShell  by self write  by * read''
+          ''{2}to dn.subtree="ou=system,ou=users,dc=eve"
+               by dn.base="cn=dovecot,dc=mail,dc=eve" read
+               by dn.subtree="ou=system,ou=users,dc=eve" read
+               by * none''
+          ''{3}to dn.subtree="ou=jabber,ou=users,dc=eve"  by dn.base="cn=prosody,ou=system,ou=users,dc=eve" write  by * read''
+          ''{4}to * by * read''
+        ];
+        # Current value (must be stored completly in file)
+        # provider=ldap://eve.r:389
+        # type=refreshOnly
+        # interval=01:00:00:00
+        # searchbase="dc=eve"
+        # schemachecking=off
+        # bindmethod=simple
+        # binddn="cn=ldapsync,ou=system,ou=users,dc=eve"
+        # credentials=<secret>
+        olcSyncRepl.path = config.sops.secrets.ldapsync-password.path;
+      };
+    };
   };
 
   sops.secrets.openldap-rootpw = {
