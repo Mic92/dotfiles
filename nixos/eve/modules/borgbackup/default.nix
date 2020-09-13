6991184ef518d185ecac7fad4c8fb000bc14f54b
Jörg Thalheim <joerg@thalheim.io>
2020-09-13 11:40:11 +0200

    eve/borgbackup: fix ssh keys

--- nixos/eve/modules/borgbackup/default.nix
+++ nixos/eve/modules/borgbackup/default.nix
@@ -7,12 +7,12 @@
 
   services.borgbackup.repos.uni = {
     path = "/data/backup/uni";
-    authorizedKeys = builtins.readFile ./uni-borgbackup.pub;
+    authorizedKeys = [ (builtins.readFile ./uni-borgbackup.pub) ];
   };
 
   services.borgbackup.repos.turingmachine = {
     path = "/data/backup/turingmachine";
-    authorizedKeys = builtins.readFile ./turingmachine-borgbackup.pub;
+    authorizedKeys = [ (builtins.readFile ./turingmachine-borgbackup.pub) ];
   };
 
   services.borgbackup.jobs.eve = {
