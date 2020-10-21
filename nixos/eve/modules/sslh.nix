0c36fde085eeb8d7dbe6830f2dc6345dd73b8c92
Jörg Thalheim <joerg@thalheim.io>
2020-10-21 21:19:09 +0100

    eve: fix sslh

--- nixos/eve/modules/sslh.nix
+++ nixos/eve/modules/sslh.nix
@@ -12,7 +12,7 @@ with lib;
       (
         { name: "ssh"; service: "ssh"; host: "localhost"; port: "22"; probe: "builtin"; },
         { name: "xmpp"; host: "localhost"; port: "5222"; probe: "builtin"; },
-        { name: "ssl"; host: "localhost"; port: "4443"; probe: "builtin"; },
+        { name: "tls"; host: "localhost"; port: "4443"; probe: "builtin"; },
         { name: "tinc"; host: "localhost"; port: "655"; probe: "builtin"; }
       );
     '';
