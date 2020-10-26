8e04547ba1f1371f6e18edc1519d5d41e1e5a0e1
Jörg Thalheim <joerg@thalheim.io>
2020-10-26 08:19:52 +0100

    nixos/kresd: switch to streamConfig option

--- nixos/eve/modules/kresd.nix
+++ nixos/eve/modules/kresd.nix
@@ -19,19 +19,17 @@
 
   services.nginx = {
     enableReload = true;
-    appendConfig = ''
-      stream {
-        upstream dns {
-          zone dns 64k;
-          server [::1]:53;
-        }
-        server {
-          listen 853 ssl;
-          ssl_certificate /var/lib/acme/thalheim.io/fullchain.pem;
-          ssl_certificate_key /var/lib/acme/thalheim.io/key.pem;
-          ssl_trusted_certificate /var/lib/acme/thalheim.io/fullchain.pem;
-          proxy_pass dns;
-        }
+    streamConfig = ''
+      upstream dns {
+        zone dns 64k;
+        server [::1]:53;
+      }
+      server {
+        listen 853 ssl;
+        ssl_certificate /var/lib/acme/thalheim.io/fullchain.pem;
+        ssl_certificate_key /var/lib/acme/thalheim.io/key.pem;
+        ssl_trusted_certificate /var/lib/acme/thalheim.io/fullchain.pem;
+        proxy_pass dns;
       }
     '';
     upstreams."doh" = {
