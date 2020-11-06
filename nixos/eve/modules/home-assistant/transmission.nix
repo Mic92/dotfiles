e8d08ff4629eeaf34d2c1ffc049e215465659b2d
Jörg Thalheim <joerg@thalheim.io>
2020-11-06 20:58:19 +0100

    notifications if torrent is complete

--- nixos/eve/modules/home-assistant/transmission.nix
+++ nixos/eve/modules/home-assistant/transmission.nix
@@ -1,7 +1,27 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.home-assistant.config.transmission = {
-    host = "yellow.r";
+  services.home-assistant.config = {
+    transmission.host = "yellow.r";
+    automation = [{
+      alias = "Completed Torrent";
+      trigger = {
+        platform = "event";
+        event_type = "transmission_downloaded_torrent";
+      };
+      action = {
+        service = "notify.irc_flix";
+        data = {
+          message = "torrent completed: {{trigger.event.data.name}}";
+        };
+      };
+    }];
+    notify = [{
+      name = "irc_flix";
+      platform = "command_line";
+      command = ''
+        ${pkgs.nur.repos.mic92.irc-announce}/bin/irc-announce irc.r 6667 transmission '#flix' "$(cat)"
+      '';
+    }];
   };
 }
