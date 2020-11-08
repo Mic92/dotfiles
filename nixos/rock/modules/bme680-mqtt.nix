a0ce5a1ad25398e6d7b3a508b271dcca969d4d4a
Jörg Thalheim <joerg@thalheim.io>
2020-11-08 21:29:54 +0100

    bme680: fix sensor read

--- nixos/rock/modules/bme680-mqtt.nix
+++ nixos/rock/modules/bme680-mqtt.nix
@@ -2,6 +2,6 @@
 {
    services.bme680-mqtt.enable = true;
    services.bme680-mqtt.mqtt.passwordFile = config.sops.secrets.mqtt-bme680.path;
-   services.bme680-mqtt.mqtt.url = "mqtts://bme680@thalheim.io@mqtt.thalheim.io";
+   services.bme680-mqtt.mqtt.url = "mqtts://bme680@thalheim.io@mqtt.thalheim.io:8883";
    sops.secrets.mqtt-bme680 = {};
 }
