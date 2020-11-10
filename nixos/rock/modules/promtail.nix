c7ca1eaae1a21ed5e0f594bfe1761465e1002316
Jörg Thalheim <joerg@thalheim.io>
2020-11-10 11:37:11 +0100

    promtail: make logs fancier

--- nixos/rock/modules/promtail.nix
+++ nixos/rock/modules/promtail.nix
@@ -17,11 +17,43 @@
 
       scrape_configs = [{
         job_name = "journal";
-        journal.max_age = "12h";
-        journal.labels.job = "systemd-journal";
+        journal = {
+          json = true;
+          max_age = "12h";
+          labels.job = "systemd-journal";
+        };
+        pipeline_stages = [{
+          json.expressions = {
+            transport = "_TRANSPORT";
+            unit = "_SYSTEMD_UNIT";
+            msg = "MESSAGE";
+          };
+        } {
+          # Set the unit (defaulting to the transport like audit and kernel)
+          template = {
+            source = "unit";
+            template = "{{if .unit}}{{.unit}}{{else}}{{.transport}}{{end}}";
+          };
+        } {
+          # Normalize session IDs (session-1234.scope -> session.scope) to limit number of label values
+          replace = {
+            source = "unit";
+            expression = "^(session-\\d+.scope)$";
+            replace = "session.scope";
+          };
+        } {
+          # Write the unit label
+          labels = { unit = "unit"; };
+        } {
+          # Write the proper message instead of JSON
+          output.source = "msg";
+        } {
+          # Drop useless messages
+          drop.expression = "xfs filesystem being remounted at [^ ]+ supports timestamps until 2038 \\(0x7fffffff\\)";
+        }];
         relabel_configs = [{
-          source_labels = [ "__journal__systemd_unit" ];
-          target_label = "unit";
+          source_labels = [ "__journal__hostname" ];
+          target_label = "nodename";
         }];
       }];
     };
