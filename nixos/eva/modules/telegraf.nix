5a319292335f7797a43583271fec1db99944478d
Jörg Thalheim <joerg@thalheim.io>
2020-11-12 12:48:23 +0100

    telegraf: add influxdb listener

--- nixos/eva/modules/telegraf.nix
+++ nixos/eva/modules/telegraf.nix
@@ -2,15 +2,37 @@
 
 {
   sops.secrets.telegraf.owner = config.systemd.services.telegraf.serviceConfig.User;
+  sops.secrets.telegraf-shared = {
+    owner = config.systemd.services.telegraf.serviceConfig.User;
+    sopsFile = ../../secrets/telegraf.yaml;
+  };
   systemd.services.telegraf.serviceConfig.SupplementaryGroups = [ "keys" ];
 
-  imports = [ ../../modules/telegraf.nix ];
+  imports = [
+    ../../modules/telegraf.nix
+  ];
+
+  services.nginx.virtualHosts."telegraf.thalheim.io" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".extraConfig =  ''
+      proxy_pass http://localhost:8186/;
+    '';
+  };
 
   services.telegraf = {
-    environmentFile = config.sops.secrets.telegraf.path;
+    environmentFile = [
+      config.sops.secrets.telegraf.path
+      config.sops.secrets.telegraf-shared.path
+    ];
     extraConfig = {
       agent.interval = "120s";
       inputs = {
+        influxdb_listener = [{
+          service_address = ":8186";
+          basic_username = "telegraf";
+          basic_password = "$INFLUXDB_PASSWORD";
+        }];
         ping = let
           urls = [
             "eve.r"
