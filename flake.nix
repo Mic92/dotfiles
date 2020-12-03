e9230077ad2233961543c0c59b1c7d4af71a3366
Jörg Thalheim <joerg@thalheim.io>
2020-12-03 12:56:35 +0100

    move outputs to external file

--- flake.nix
+++ flake.nix
@@ -43,70 +43,7 @@
     nix-ld.url = "github:Mic92/nix-ld";
     nix-ld.inputs.nixpkgs.follows = "nixpkgs";
     nix-ld.inputs.utils.follows = "flake-utils";
-    nix-ld.flake = true;
   };
 
-  outputs = { self
-            , nixpkgs
-            , nixpkgs-systemd
-            , nixos-hardware
-            , sops-nix
-            , nur
-            , home-manager
-            , retiolum
-            , flake-utils
-            , krops
-            , flake-registry
-            , bme680-mqtt
-            , nix-ld
-            , ... }:
-    (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      nurPkgs = import nur {
-        inherit pkgs;
-        nurpkgs = pkgs;
-      };
-     in {
-       devShell = pkgs.callPackage ./shell.nix {};
-       # deploy like this:
-       #  nix run ".#deploy.turingmachine"
-       #  nix run ".#deploy.eve"
-       apps.deploy = pkgs.callPackage ./nixos/krops.nix {
-         inherit (krops.packages.${system}) writeCommand;
-         lib = krops.lib;
-       };
-       apps.irc-announce = nurPkgs.repos.mic92.irc-announce;
-     })) // {
-
-
-     nixosConfigurations = import ./nixos/configurations.nix {
-       #nixpkgs = toString <nixpkgs>;
-       # for testing
-       #nixosSystem = import <nixpkgs/nixos/lib/eval-config.nix>;
-       inherit nixpkgs nixpkgs-systemd;
-       nixosSystem = nixpkgs.lib.nixosSystem;
-       inherit nur
-         home-manager
-         sops-nix
-         retiolum
-         nixos-hardware
-         flake-registry
-         bme680-mqtt
-         nix-ld;
-     };
-
-     hmConfigurations = import ./nixpkgs-config/homes.nix {
-       inherit self nixpkgs home-manager nur;
-     };
-
-     hydraJobs = {
-       configurations =
-         nixpkgs.lib.mapAttrs'
-           (name: config: nixpkgs.lib.nameValuePair name config.config.system.build.toplevel)
-           self.nixosConfigurations;
-       hmConfigurations = nixpkgs.lib.mapAttrs'
-         (name: config: nixpkgs.lib.nameValuePair name config.activation-script)
-         self.hmConfigurations;
-     };
-   };
+  outputs = { ... } @ args: import ./outputs.nix args;
 }
