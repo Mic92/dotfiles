1760d0e7414d8282e916299626b1defc9b0b093b
Jörg Thalheim <joerg@thalheim.io>
2020-12-15 04:00:40 +0100

    squid: avoid port collision with squid

--- nixos/eve/modules/squid.nix
+++ nixos/eve/modules/squid.nix
@@ -2,7 +2,7 @@
   services.squid = {
     enable = true;
     # We cannot disable the plain text port atm, but the firewall blocks it
-    proxyPort = 8888;
+    proxyPort = 8800;
     extraConfig = ''
       auth_param basic program ${pkgs.squid}/libexec/basic_ldap_auth -b "ou=users,dc=eve" -f "(&(objectClass=proxyUser)(mail=%s))" -D cn=squid,ou=system,ou=users,dc=eve -W ${config.sops.secrets.squid-ldap.path} -h 127.0.0.1
       acl ldapauth proxy_auth REQUIRED
