a46f12b6dc103260161faa54f44bb338afbdfd00
Jörg Thalheim <joerg@thalheim.io>
2020-12-20 07:12:37 +0100

    hass: add more noops

--- nixos/eve/modules/home-assistant/noops.nix
+++ nixos/eve/modules/home-assistant/noops.nix
@@ -32,5 +32,14 @@ in {
     Noop.speech.text = "${okay} {{ okay () }}";
     FuckOff.speech.text = "Fuck yourself.";
     NotAGirl.speech.text = "Not a girl";
+    Siri.speech.text = "Siri is a cunt";
+    PersonLocation.speech.text = '' {% if person == "Jork" %}
+      Joerg is {{ states.person.jorg_thalheim.state }} near {{ states.sensor.redmi_note_5_geocoded_location.attributes.Thoroughfare }} and {{ states.sensor.shannan_joerg_distance.state }} kilometer away from you.
+      His phone reports, he is {% set activity = states.sensor.redmi_note_5_detected_activity.state %} {% if activity == "still" %} stationary {% else %} {{ activity }} {% endif %}!
+    {% else %}
+      Shannan is {{ states.person.shannan_lekwati.state }} near {{ states.sensor.beatrice_geocoded_location.attributes.Thoroughfare }} and {{ states.sensor.shannan_joerg_distance.state }} kilometer away from you.
+      Her phone reports, she is {{ states.sensor.beatrice_activity.state }}!
+    {% endif %}
+   '';
   };
 }
