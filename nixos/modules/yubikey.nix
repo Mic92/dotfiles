cb2cd72b7e8cdb82e051fb0e98d4b4525f1fcb66
Jörg Thalheim <joerg@thalheim.io>
2021-01-24 22:05:23 +0100

    yubikey: don't enable pcscd

--- nixos/modules/yubikey.nix
+++ nixos/modules/yubikey.nix
@@ -6,7 +6,10 @@
     yubioath-desktop
   ];
 
-  services.pcscd.enable = true;
+  services.udev.extraRules = ''
+    # Yubikey 4/5 U2F+CCID
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1050", ATTR{idProduct}=="0406", ENV{ID_SECURITY_TOKEN}="1", GROUP="wheel"
+  '';
 
   services.udev.packages = with pkgs; [
     yubikey-personalization
