542d6a7b4d33e7e8ceb063d10552550db808edf9
Jörg Thalheim <joerg@thalheim.io>
2021-02-04 00:09:12 +0100

    sway: update for 2021

--- nixos/modules/sway.nix
+++ nixos/modules/sway.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }: {
   programs.sway = {
     enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
     extraPackages = with pkgs; [
       dex
       swaylock # lockscreen
@@ -9,13 +10,31 @@
       xwayland
       waybar # status bar
       (i3pystatus.override {
-        extraLibs = [ python3.pkgs.keyrings-alt ];
+        extraLibs = [ python3.pkgs.keyrings-alt python3.pkgs.paho-mqtt ];
       })
-      rofi
+      wofi
       gnome3.eog
       libnotify
       mako # notification daemon
       kanshi # autorandr
+      swappy
+      wdisplays
+      wl-clipboard
+      clipman
+
+      # autostart stuff
+      dex
+
+      pavucontrol
+      lightlocker
+      evince
+      libnotify
+      pamixer
+      mpc_cli
+      gnome3.networkmanagerapplet
+      gnome3.file-roller
+      gnome3.nautilus
+      firefox-wayland
     ];
     extraSessionCommands = ''
       export SDL_VIDEODRIVER=wayland
@@ -24,23 +43,43 @@
       export _JAVA_AWT_WM_NONREPARENTING=1
     '';
   };
-  programs.sway.wrapperFeatures.gtk = true;
-  users.users.joerg.extraGroups = [ "sway" ];
 
-   # Here we but a shell script into path, which lets us start sway.service (after importing the environment of the login shell).
-  environment.systemPackages = with pkgs; [(pkgs.writeTextFile {
-    name = "startsway";
-    destination = "/bin/startsway";
-    executable = true;
-    text = ''
-      #! ${pkgs.bash}/bin/bash
+  environment.systemPackages = with pkgs; [
+    # polkit agent
+    polkit_gnome
 
-      # first import environment variables from the login manager
-      systemctl --user import-environment PATH TMPDIR DISPLAY XDG_RUNTIME_DIR
-      # then start the service
-      exec systemctl --user start sway.service
-    '';
-  })];
+    # gtk3 themes
+    gtk-engine-murrine
+    gtk_engines
+    gsettings-desktop-schemas
+    lxappearance
+
+    # screen brightness
+    brightnessctl
+
+    # Here we but a shell script into path, which lets us start sway.service (after importing the environment of the login shell).
+    (pkgs.writeTextFile {
+      name = "startsway";
+      destination = "/bin/startsway";
+      executable = true;
+      text = ''
+        #! ${pkgs.bash}/bin/bash
+
+        # first import environment variables from the login manager
+        systemctl --user import-environment
+        # then start the service
+        exec systemctl --user start sway.service
+      '';
+    })
+  ];
+
+  # for polkit
+  environment.pathsToLink = [ "/libexec" ];
+
+  programs.qt5ct.enable = true;
+
+  # brightnessctl
+  users.users.joerg.extraGroups = [ "video" ];
 
   systemd.user.targets.sway-session = {
     description = "Sway compositor session";
@@ -67,30 +106,6 @@
       Restart = "on-failure";
       RestartSec = 1;
       TimeoutStopSec = 10;
-    };
-  };
-
-  services.redshift = {
-    enable = true;
-    package = pkgs.redshift-wlr;
-  };
-
-  location.provider = "geoclue2";
-
-  programs.waybar.enable = true;
-
-  systemd.user.services.kanshi = {
-    description = "Kanshi output autoconfig ";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      # kanshi doesn't have an option to specifiy config file yet, so it looks
-      # at .config/kanshi/config
-      ExecStart = ''
-        ${pkgs.kanshi}/bin/kanshi
-      '';
-      RestartSec = 5;
-      Restart = "always";
     };
   };
 }
