0c75a328535003e65d231c0d909045cf4b35e8a2
Jörg Thalheim <joerg@thalheim.io>
2021-02-10 15:09:43 +0100

    hass/noops: only report activity if known

--- nixos/eve/modules/home-assistant/noops.nix
+++ nixos/eve/modules/home-assistant/noops.nix
@@ -39,7 +39,9 @@ in {
       His phone reports, he is {% set activity = states.sensor.redmi_note_5_detected_activity.state %} {% if activity == "still" %} stationary {% else %} {{ activity }} {% endif %}!
     {% else %}
       Shannan is {{ states.person.shannan_lekwati.state }} near {{ states.sensor.beatrice_geocoded_location.attributes.Thoroughfare }} and {{ states.sensor.shannan_joerg_distance.state }} kilometer away from you.
+      {% if states.sensor.beatrice_activity.state != "Unknown" %}
       Her phone reports, she is {{ states.sensor.beatrice_activity.state }}!
+      {% endif %}
     {% endif %}
    '';
   };
