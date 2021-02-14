7378beb7d5bc694d9237d30171652dee57eb4d5a
Jörg Thalheim <joerg@thalheim.io>
2021-02-14 20:04:24 +0100

    eve/goatcounter: fix database access and group

--- nixos/eve/modules/goatcounter.nix
+++ nixos/eve/modules/goatcounter.nix
@@ -9,14 +9,28 @@
       EnvironmentFile = [ config.sops.secrets.goatcounter-smtp-password.path ];
       ExecStart = ''
         ${pkgs.nur.repos.mic92.goatcounter}/bin/goatcounter \
+          serve \
           -automigrate \
           -listen localhost:3003 \
           -tls none \
-          -db postgres://dbname=goatcounter \
+          -db postgresql://?host=/run/postgresql&dbname=goatcounter&sslmode=disable \
           -smtp smtp://goatcounter@thalheim.io:''${SMTP_PASSWORD}@mail.thalheim.io
       '';
       User = "goatcounter";
     };
+  };
+          #-db postgresql://?host=/run/postgresql&dbname=goatcounter&sslmode=disable \
+
+  services.nginx.virtualHosts."goatcounter.thalheim.io" = {
+    useACMEHost = "thalheim.io";
+    forceSSL = true;
+    locations."/".extraConfig = ''
+      proxy_pass http://localhost:3003;
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+    '';
   };
 
   services.postgresql.ensureDatabases = [ "goatcounter" ];
@@ -31,4 +45,6 @@
     isSystemUser = true;
     group = "goatcounter";
   };
+
+  users.groups.goatcounter = {};
 }
