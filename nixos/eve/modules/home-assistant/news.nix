70d4369fac94177aa7c366f8c655d2d42a2688ba
Jörg Thalheim <joerg@thalheim.io>
2021-03-05 08:57:09 +0100

    hass/news: make curl quiet

--- nixos/eve/modules/home-assistant/news.nix
+++ nixos/eve/modules/home-assistant/news.nix
@@ -6,14 +6,14 @@
       platform = "command_line";
       scan_interval = 60 * 60 * 24;
       command =
-        ''${pkgs.curl}/bin/curl https://podcasts.files.bbci.co.uk/p02nrtyg.rss | grep -m1 -oPm1 '(?<=url=")https://[^"]+.mp3' | shuf -n1 '';
+        ''${pkgs.curl}/bin/curl -sL https://podcasts.files.bbci.co.uk/p02nrtyg.rss | grep -m1 -oPm1 '(?<=url=")https://[^"]+.mp3' | shuf -n1 '';
     }
       {
         name = "BBC World News";
         platform = "command_line";
         scan_interval = 60 * 60;
         command =
-          ''${pkgs.curl}/bin/curl https://www.bbc.co.uk/programmes/p002vsmz/episodes/player | grep -m1 -o -E 'https://www.bbc.co.uk/programmes/w[^"]+' '';
+          ''${pkgs.curl}/bin/curl -sL https://www.bbc.co.uk/programmes/p002vsmz/episodes/player | grep -m1 -o -E 'https://www.bbc.co.uk/programmes/w[^"]+' '';
       }];
   };
 }
