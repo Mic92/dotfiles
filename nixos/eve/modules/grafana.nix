f1caa144fbc6426da71498380948a3b6c1187a6e
Jörg Thalheim <joerg@thalheim.io>
2021-04-04 10:50:12 +0200

    grafana: switch to upstream password ldap option

--- nixos/eve/modules/grafana.nix
+++ nixos/eve/modules/grafana.nix
@@ -1,5 +1,23 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }: let
+  ldap = pkgs.writeTextFile {
+    name = "ldap.toml";
+    text = ''
+      [[servers]]
+      host = "127.0.0.1"
+      port = 389
+      bind_dn = "cn=grafana,ou=system,ou=users,dc=eve"
+      bind_password = "''${LDAP_ADMIN_PASSWORD}"
+      search_filter = "(&(objectClass=grafana)(|(mail=%s)(uid=%s)))"
+      search_base_dns = ["ou=users,dc=eve"]
 
+      [servers.attributes]
+      name = "givenName"
+      surname = "sn"
+      username = "uid"
+      email =  "mail"
+    '';
+  };
+in {
   services.grafana = {
     enable = true;
     domain = "grafana.thalheim.io";
@@ -8,12 +26,13 @@
     extraOptions = {
       SERVER_ENFORCE_DOMAIN = "true";
       AUTH_LDAP_ENABLED = "true";
-      AUTH_LDAP_CONFIG_FILE = "/run/grafana/ldap.toml";
+      AUTH_LDAP_CONFIG_FILE = ldap;
 
       AUTH_ANONYMOUS_ENABLED = "true";
       AUTH_ANONYMOUS_ORG_NAME = "Main Org.";
       AUTH_ANONYMOUS_ORG_ROLE = "Viewer";
     };
+    ldap.bindPasswordFile = config.sops.secrets.grafana-ldap-password.path;
     smtp = {
       enable = true;
       host = "mail.thalheim.io:587";
@@ -33,44 +52,7 @@
   };
 
   users.users.grafana.extraGroups = [ "keys" ];
-  systemd.services.grafana = {
-    serviceConfig = {
-      SupplementaryGroups = [ "keys" ];
-      RuntimeDirectory = [ "grafana" ];
-    };
-    preStart =
-      let
-        ldap = pkgs.writeTextFile {
-          name = "ldap.toml";
-          text = ''
-            [[servers]]
-            host = "127.0.0.1"
-            port = 389
-            bind_dn = "cn=grafana,ou=system,ou=users,dc=eve"
-            bind_password = "@bindPassword@"
-            search_filter = "(&(objectClass=grafana)(|(mail=%s)(uid=%s)))"
-            search_base_dns = ["ou=users,dc=eve"]
-
-            [servers.attributes]
-            name = "givenName"
-            surname = "sn"
-            username = "uid"
-            email =  "mail"
-          '';
-        };
-      in
-      ''
-        umask 077
-        sed -e "s/@bindPassword@/$(cat ${config.sops.secrets.grafana-ldap-password.path})/" ${ldap} > /run/grafana/ldap.toml
-
-        for i in `seq 1 10`; do
-          if pg_isready; then
-            break
-          fi
-          sleep 1
-        done
-      '';
-  };
+  systemd.services.grafana.serviceConfig.SupplementaryGroups = [ "keys" ];
 
   services.nginx = {
     virtualHosts."grafana.thalheim.io" = {
