2fc5f0eb3e1c7e058ba7a40600c6ea5873dbef5e
Jörg Thalheim <joerg@thalheim.io>
2021-04-28 08:06:43 +0200

    lambda-shell-as-a-service: add devkid

--- nixos/eve/modules/lambda-shaas.nix
+++ nixos/eve/modules/lambda-shaas.nix
@@ -13,12 +13,15 @@ in
   users.extraUsers.lambda = {
     isSystemUser = true;
     shell = "/run/current-system/sw/bin/bash";
-    openssh.authorizedKeys.keys = map (key:
+    openssh.authorizedKeys.keys = map
+      (key:
         ''command="${lambda-shell}",no-pty,no-agent-forwarding,no-port-forwarding,no-X11-forwarding,no-user-rc ${key}''
-      ) [
-        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine''
+      )
+      ([
         # Peter Okelmann
         ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDITBcN9iw5Fn7yyfgiWFet3QWDoMcUNtzLi+PNoYS7jksvcKZy5pLOjE6wCpkbYx+Tcb4MyvoWPXvwdo5FfL4XdhZRO+JlZ66p/rGssq/wEr2BBUwohP7o39JLtiyXGXSsK6MO2aceOFLQr4KAdaeD8ST0XumGcV6bGqIbjFsK5FCxFhO8NkCFtavBjDwKUm3uyOnVCWMp12abUphzxrVtWhcsnw5GapohATP03mCNxmrn/L7x393HutxgjyduScX7++MjwVE6J7wCnztPUtJbh9jYemr/K9fBMBbLhQagOjrlQYGU5frgmLrPCRZusyg5HjWx6gJIxs/DskfgmW+V''
-    ];
+      ]
+      ++ config.users.extraUsers.joerg.openssh.authorizedKeys.keys
+      ++ config.users.extraUsers.devkid.openssh.authorizedKeys.keys);
   };
 }
