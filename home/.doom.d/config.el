ce37408afd6ca2c3ba46053f959f9076b9f13ffc
Jörg Thalheim <joerg@thalheim.io>
2021-05-06 07:30:14 +0200

    doom: dracula

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-solarized-light)
+(setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
