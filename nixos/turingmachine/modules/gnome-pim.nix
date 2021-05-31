8578a7a30828f3b8ff7a120eda7a19753fbc46f3
Jörg Thalheim <joerg@thalheim.io>
2021-05-31 07:08:21 +0200

    turingmachine: fix gnome3 warnings

--- nixos/turingmachine/modules/gnome-pim.nix
+++ nixos/turingmachine/modules/gnome-pim.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
   programs.dconf.enable = true;
-  services.gnome3.evolution-data-server.enable = true;
-  services.gnome3.gnome-online-accounts.enable = true;
-  services.gnome3.gnome-keyring.enable = true;
+  services.gnome.evolution-data-server.enable = true;
+  services.gnome.gnome-online-accounts.enable = true;
+  services.gnome.gnome-keyring.enable = true;
   environment.systemPackages = [
     pkgs.gnome3.gnome-calendar
     pkgs.gnome3.gnome-todo
