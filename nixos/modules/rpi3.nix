79ed5704609a960dd246d25360d9919f3e2bd2f6
Jörg Thalheim <joerg@thalheim.io>
2021-06-22 20:58:44 +0200

    rpi3 switch kernel

--- nixos/modules/rpi3.nix
+++ nixos/modules/rpi3.nix
@@ -10,7 +10,7 @@ in
 
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_rpi3;
   documentation.enable = false;
 
   boot.kernelParams = [ "cma=32M" ];
