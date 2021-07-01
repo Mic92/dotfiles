02d55c39160189d19de961ccfb2f8cd5c1032e9c
Jörg Thalheim <joerg@thalheim.io>
2021-07-01 08:36:24 +0000

    home/eve: remove signal, add weechat-matrix

--- nixpkgs-config/eve.nix
+++ nixpkgs-config/eve.nix
@@ -6,14 +6,13 @@
   ];
   home.packages = [
     pkgs.profanity
-    pkgs.nur.repos.mic92.signald
     (pkgs.weechat.override {
       configure = { availablePlugins, ... }: {
         scripts = with pkgs.weechatScripts; [
           weechat-otr
           wee-slack
           multiline
-          pkgs.nur.repos.mic92.weechat-signal
+          weechat-matrix
         ];
         plugins = [
           availablePlugins.python
