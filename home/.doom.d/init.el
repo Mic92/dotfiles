fdb3a097d95f27471dac69b26ad811888b70252c
Jörg Thalheim <joerg@thalheim.io>
2021-07-08 00:22:24 +0200

    doom: switch to mspyls

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -148,7 +148,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright)   ; beautiful is better than ugly
+       (python +lsp)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
