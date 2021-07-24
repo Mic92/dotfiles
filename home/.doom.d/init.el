e0d2f9e2d1676750f3f791b6705139569feb347f
Jörg Thalheim <joerg@thalheim.io>
2021-07-24 21:34:49 +0200

    doom: disable org-mode

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -144,7 +144,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       ;;org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
