62a93ee1184a5ebf38af0ad16bb4022ad32e4b33
Jörg Thalheim <joerg@thalheim.io>
2021-07-30 14:06:58 +0200

    nfs: expand settins

--- nixos/turingmachine/modules/nfs.nix
+++ nixos/turingmachine/modules/nfs.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ lib, ... }:
 {
   services.nfs.server.enable = false;
   # nix-shell -p nfs-utils 'mount.nfs 129.215.90.50:/home /home -o nolock'
   services.nfs.server.exports = ''
-    /home                 129.215.90.4(rw,fsid=root,nohide,insecure,no_subtree_check)
+    /home/joerg/web ${lib.concatMapStringsSep " " (host: ''${host}(rw,nohide,insecure,no_subtree_check,no_root_squash)'') ["192.168.32.51"]}
   '';
 }
 
