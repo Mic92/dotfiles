b45ee48eb5ae82ba30a4f3e06e6a2b4a18e73031
Jörg Thalheim <joerg@thalheim.io>
2021-08-02 08:34:38 +0000

    disable ligatures

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -34,7 +34,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures         ; ligatures and symbols to make your code pretty again
+       ;;(ligatures +fira)   ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
