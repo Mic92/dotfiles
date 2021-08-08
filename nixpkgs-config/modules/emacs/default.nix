c0b33abec6824636c9577595f10a75c84ae37ee8
Jörg Thalheim <joerg@thalheim.io>
2021-08-08 19:41:52 +0000

    extend doom upgrades

--- nixpkgs-config/modules/emacs/default.nix
+++ nixpkgs-config/modules/emacs/default.nix
@@ -37,7 +37,8 @@ let
     if [ $(git -C $HOME/.emacs.d rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
       git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
       git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
-      $HOME/.emacs.d/bin/doom sync -u &
+      $HOME/.emacs.d/bin/doom sync || true
+      yes 4 | $HOME/.emacs.d/bin/doom sync -u &
     fi
     exec ${myemacs}/bin/emacs --daemon
   '';
