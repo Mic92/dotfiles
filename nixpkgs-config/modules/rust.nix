60595eaf78ae8eb33528ce9d9a56d6c4ec057520
Jörg Thalheim <joerg@thalheim.io>
2021-08-13 10:03:14 +0200

    home-manager/rust: drop dependency on firefox

--- nixpkgs-config/modules/rust.nix
+++ nixpkgs-config/modules/rust.nix
@@ -6,7 +6,7 @@
     rust-analyzer
     (writeScriptBin "rust-doc" ''
       #! ${stdenv.shell} -e
-      exec "${firefox}" "${rustc.doc}/share/doc/rust/html/index.html"
+      exec firefox "${rustc.doc}/share/doc/rust/html/index.html"
     '')
     rustracer
   ];
