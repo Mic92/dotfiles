9477bf199ba21b373c9d145f88ce9d71211cc813
Jörg Thalheim <joerg@thalheim.io>
2021-08-17 05:47:28 +0000

    update kexec image

--- nixos/images/hetzner/kexec.nix
+++ nixos/images/hetzner/kexec.nix
@@ -3,15 +3,14 @@
 # $ sgdisk  -n 1:0:+1M -n 2:0:+2G -N 3 -t 1:ef02 -t 2:8304 -t 3:8304 /dev/sda
 # SSD2: 1MB Grub, rest zfs
 # $ sgdisk  -n 1:0:+1M -N 2 -t 1:ef02 -t 2:8304 /dev/sda
-# $ nix-build '<nixpkgs/nixos>' -A config.system.build.kexec_tarball -I nixos-config=./kexec.nix nixcfg=./hetzner-kexec.nix
+# $ nix-build '<nixpkgs/nixos>' -A config.system.build.kexec_bundle -I nixos-config=../kexec.nix -I nixcfg=kexec.nix
 # $ mkfs.vfat -b32 -n bios-boot /dev/sda1
 # $ mkfs.vfat -b32 -n bios-boot /dev/sdb1
 # $ mkfs.ext4 -L linux-boot /dev/sda2
 # $ mount /dev/sda2 /mnt
 ## Start nixos installer via kexec
-# $ rsync -e ssh -a result/tarball/nixos-system-x86_64-linux.tar.xz root@95.216.112.61:/mnt
-# rescue> tar -C / -xf nixos-system-x86_64-linux.tar.xz
-# rescue> /kexec_nixos
+# $ rsync -e ssh -a result root@95.216.112.61:/mnt
+# rescue> /mnt/result
 
 ## Create encrypted zfs
 # kexec> zpool create -o ashift=12 -o altroot=/mnt -O acltype=posixacl -O xattr=sa -O compression=lz4 zroot /dev/sda3
--- nixos/images/kexec.nix
+++ nixos/images/kexec.nix
@@ -2,13 +2,14 @@
 { lib, ... }:
 let
   nixos-generators = builtins.fetchGit {
-    url = https://github.com/krebs/nixos-generators;
-    rev = "4edb2d6d74134223c55db1a2ad8c7c3618ca37fe"; #master on 2019-08-26
+    url = "https://github.com/krebs/nixos-generators";
+    rev = "022ef440af8dc237ab1f59fa363cb1e25783ec3e"; #master on 29.06.2021
   };
 in
 {
   imports = [
-    "${nixos-generators}/lib/kexec.nix"
+    "${nixos-generators}/formats/kexec-bundle.nix"
+    "${nixos-generators}/format-module.nix"
     ./base-config.nix
   ];
 }
