50a5cc9ce321530351449cebf0e90ce09cf08e86
Jörg Thalheim <joerg@thalheim.io>
2021-08-26 14:18:47 +0000

    disable doom-quit

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -29,7 +29,7 @@
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
