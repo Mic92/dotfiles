3ebe88a6e0cc8a7958ef49dd60b0b205858827fa
Jörg Thalheim <joerg@thalheim.io>
2021-08-26 17:33:31 +0200

    kresd: workaround for c3d2.de

--- nixos/eve/modules/kresd.nix
+++ nixos/eve/modules/kresd.nix
@@ -19,6 +19,9 @@
       net.tls("/var/lib/acme/dns.thalheim.io/fullchain.pem", "/var/lib/acme/dns.thalheim.io/key.pem")
       modules = { 'hints > iterate' }
       hints.add_hosts('${pkgs.retiolum}/etc.hosts')
+
+      # use ns.spaceboys.net as one dns resolver is borked
+      policy.add(policy.suffix(policy.STUB('95.217.229.209'), {todname('chat.c3d2.de')}))
     '';
   };
 
