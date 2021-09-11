c3449c4f7562857c54e99a5bd4bb3d14219ac68b
Jörg Thalheim <joerg@thalheim.io>
2021-09-11 10:51:51 +0200

    remove drone from shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,9 @@
-{ drone-cli
-, mkShell
-, sops-import-keys-hook
-}:
+{ mkShell, sops-import-keys-hook }:
 
 mkShell {
-  DRONE_SERVER = "https://drone.thalheim.io";
   sopsPGPKeyDirs = [ "./nixos/secrets/keys" ];
   sopsCreateGPGHome = true;
   nativeBuildInputs = [
     sops-import-keys-hook
-    drone-cli
   ];
 }
