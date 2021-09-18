8e908476a3340b5b7487afa177615385203a3370
Jörg Thalheim <joerg@thalheim.io>
2021-09-18 18:51:28 +0200

    add group for hass-agent

--- nixos/modules/hass-agent.nix
+++ nixos/modules/hass-agent.nix
@@ -2,6 +2,7 @@
 {
   users.extraUsers.hass-agent = {
     isSystemUser = true;
+    group = "hass-agent";
     shell = "/run/current-system/sw/bin/bash";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6vG7qFCKcdlB+0PdLc2IY7dBmD26NcSEUVwaoqLFNB"
@@ -13,6 +14,9 @@
       '')
     ];
   };
+
+  users.groups.hass-agent = {};
+
   security.sudo.extraRules = [{
     users = [ "hass-agent" ];
     commands = [{
