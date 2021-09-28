563ca7bd26be82c08c83d46c1f241add7609690c
Jörg Thalheim <joerg@thalheim.io>
2021-09-28 22:36:49 +0200

    add x86_64 builder

--- nixos/eve/modules/remote-builder.nix
+++ nixos/eve/modules/remote-builder.nix
@@ -12,7 +12,26 @@
       "kvm"
       "nixos-tests"
     ];
+  } {
+    hostName = "nardole.r";
+    maxJobs = 96;
+    sshKey = config.sops.secrets.tum-builder.path;
+    sshUser = "nix";
+    system = "x86_64-linux";
+    supportedFeatures = [
+      "big-parallel"
+      "kvm"
+      "nixos-tests"
+    ];
   }];
 
-  sops.secrets.ssh-aarch64-builder = { };
+  programs.ssh.knownHosts = {
+    "aarch64.nixos.community" = {
+      hostNames = ["aarch64.nixos.community"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds";
+    };
+  };
+
+  sops.secrets.tum-builder.owner = "hydra-queue-runner";
+  sops.secrets.ssh-aarch64-builder.owner = "hydra-queue-runner";
 }
