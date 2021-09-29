2b69ee563bac38e4e0790b576da01eeb8a14d120
Jörg Thalheim <joerg@thalheim.io>
2021-09-29 21:39:44 +0200

    hydra: add doctor-cluster-config

--- nixos/eve/modules/hydra/jobs.tf
+++ nixos/eve/modules/hydra/jobs.tf
@@ -37,3 +37,29 @@ resource "hydra_jobset" "dotfiles-master" {
 
   email_notifications = true
 }
+
+resource "hydra_project" "doctor-cluster-config" {
+  name         = "doctor-cluster-config"
+  display_name = "Doctor-cluster"
+  description  = " NixOS configuration for TUM cluster"
+  homepage     = "https://github.com/Mic92/doctor-cluster-config"
+  owner        = "admin"
+  enabled      = true
+  visible      = true
+}
+
+resource "hydra_jobset" "doctor-cluster-config-master" {
+  project     = hydra_project.doctor-cluster-config.name
+  state       = "enabled"
+  visible     = true
+  name        = "master"
+  type        = "flake"
+  description = "master branch"
+  flake_uri   = "github:Mic92/doctor-cluster-config"
+
+  check_interval    = 60
+  scheduling_shares = 3000
+  keep_evaluations  = 3
+
+  email_notifications = true
+}
