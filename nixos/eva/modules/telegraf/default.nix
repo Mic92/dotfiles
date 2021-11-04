99b2e7bcb0ba42935d42b20edecd334129f0738d
Jörg Thalheim <joerg@thalheim.io>
2021-11-04 08:06:49 +0100

    eva/telegraf: remove influxdb endpoint

--- nixos/eva/modules/telegraf/default.nix
+++ nixos/eva/modules/telegraf/default.nix
@@ -8,17 +8,4 @@
     ./krebs.nix
     ./nix-community.nix
   ];
-
-  services.nginx.virtualHosts."telegraf.thalheim.io" = {
-    forceSSL = true;
-    enableACME = true;
-    locations."/".extraConfig = ''
-      proxy_pass http://localhost:8186/;
-    '';
-  };
-
-  sops.secrets.telegraf.owner = config.systemd.services.telegraf.serviceConfig.User;
-  services.telegraf.environmentFiles = [
-    config.sops.secrets.telegraf.path
-  ];
 }
