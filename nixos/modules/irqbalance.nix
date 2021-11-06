e76606ed88509930973e49360e39a925739e1ce4
Jörg Thalheim <joerg@thalheim.io>
2021-11-06 11:10:33 +0100

    irqbalance: no laptops

--- nixos/modules/irqbalance.nix
+++ nixos/modules/irqbalance.nix
@@ -1,6 +1,8 @@
-{ config, lib, ... }: let
+{ config, lib, ... }:
+let
   isVM = lib.any (mod: mod == "xen-blkfront" || mod == "virtio_console") config.boot.initrd.kernelModules;
 in {
   # Pointless in vms or containers and better managed by the host.
-  services.irqbalance.enable = !(config.boot.isContainer || isVM);
+  # Also don't enable on laptop's as it might increase energy consumption.
+  services.irqbalance.enable = !(config.boot.isContainer || isVM || config.systemd.services.tlp.enable);
 }
