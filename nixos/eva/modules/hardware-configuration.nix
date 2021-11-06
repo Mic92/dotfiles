3d2cac55dc45f33a11dacdf47ada0a5ec66b0c38
Jörg Thalheim <joerg@thalheim.io>
2021-11-06 10:17:13 +0100

    eva: remove vm hardware settings

--- nixos/eva/modules/hardware-configuration.nix
+++ nixos/eva/modules/hardware-configuration.nix
@@ -1,14 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/virtualisation/lxc-container.nix")
   ];
-
-  boot.loader.grub.devices = [ "/dev/sda" ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "xhci_pci" "sd_mod" "sr_mod" ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/84053adc-49bc-4e02-8a19-3838bf3a43fd";
-    fsType = "ext4";
-  };
 }
