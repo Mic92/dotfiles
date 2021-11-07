ce081d5071beda2cbf566488642a06807e33fa30
Jörg Thalheim <joerg@thalheim.io>
2021-11-07 09:30:44 +0100

    eva: update ListenAddress

--- nixos/eva/modules/sshd.nix
+++ nixos/eva/modules/sshd.nix
@@ -1,8 +1,9 @@
 {
   imports = [ ../../modules/sshd.nix ];
-  services.openssh.extraConfig = "HostCertificate ${./eva-cert.pub}";
-  services.openssh.listenAddresses = [
-    { addr = "10.243.29.185"; port = 22; }
-    { addr = "[42:0:3c46:8a42:2b1:5ef8:7562:676a]"; port = 22; }
-  ];
+  services.openssh.extraConfig = ''
+    HostCertificate ${./eva-cert.pub}
+    ListenAddress 10.243.29.185
+    ListenAddress [42:0:3c46:8a42:2b1:5ef8:7562:676a]
+  '';
+  services.openssh.startWhenNeeded = false;
 }
