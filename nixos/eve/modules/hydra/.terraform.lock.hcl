ab5d093f1825cd5b86e98d432c3184326d784b45
Jörg Thalheim <joerg@thalheim.io>
2021-11-07 09:41:04 +0100

    hydra: drop pr inputs

--- nixos/eve/modules/hydra/.terraform.lock.hcl
+++ nixos/eve/modules/hydra/.terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "registry.terraform.io/determinatesystems/hydra" {
   version     = "0.1.2"
   constraints = "~> 0.1"
   hashes = [
-    "h1:eoWSm8A90mcT8putcWUErTKyPLT6a+A0z1ABabDTHtQ=",
+    "h1:HXFjNHzWbRXON9LvW7Z0WHxx6EZtIlQXwN2o1gMyJEs=",
   ]
 }
--- nixos/eve/modules/hydra/jobs.tf
+++ nixos/eve/modules/hydra/jobs.tf
@@ -38,40 +38,6 @@ resource "hydra_jobset" "dotfiles-master" {
   email_notifications = true
 }
 
-resource "hydra_jobset" "dotfiles-prs" {
-  project     = hydra_project.dotfiles.name
-  state       = "enabled"
-  visible     = true
-  name        = "prs"
-  type        = "legacy"
-  description = "pull requests for dotfiles"
-
-  check_interval    = 60
-  scheduling_shares = 3000
-  keep_evaluations  = 3
-
-  email_notifications = true
-
-  nix_expression {
-    file  = "ci.nix"
-    input = "dotfiles"
-  }
-
-  input {
-    name = "dotfiles"
-    type = "git"
-    value = "https://github.com/Mic92/dotfiles.git"
-    notify_committers = false
-  }
-
-  input {
-    name = "pr"
-    value = "Mic92 dotfiles"
-    type = "githubpulls"
-    notify_committers = false
-  }
-}
-
 resource "hydra_project" "doctor-cluster-config" {
   name         = "doctor-cluster-config"
   display_name = "Doctor-cluster"
@@ -96,55 +62,4 @@ resource "hydra_jobset" "doctor-cluster-config-master" {
   keep_evaluations  = 3
 
   email_notifications = true
-}
-
-resource "hydra_project" "sops-nix" {
-  name         = "sops-nix"
-  display_name = "sops-nix"
-  description  = "Atomic secret provisioning for NixOS based on sops"
-  homepage     = "https://github.com/Mic92/sops-nix"
-  owner        = "admin"
-  enabled      = true
-  visible      = true
-}
-
-resource "hydra_jobset" "sops-nix-master" {
-  project     = hydra_project.sops-nix.name
-  state       = "enabled"
-  visible     = true
-  type        = "legacy"
-  name        = "master"
-  description = "master branch"
-
-  check_interval    = 60
-  scheduling_shares = 3000
-  keep_evaluations  = 3
-
-  email_notifications = true
-
-  nix_expression {
-    file  = "default.nix"
-    input = "src"
-  }
-
-  input {
-    name = "src"
-    type = "git"
-    value = "https://github.com/Mic92/sops-nix"
-    notify_committers = false
-  }
-
-  input {
-    name = "nixpkgs"
-    type = "git"
-    value = "https://github.com/NixOS/nixpkgs.git nixpkgs-unstable"
-    notify_committers = false
-  }
-
-  input {
-    name = "pr"
-    value = "Mic92 sops-nix"
-    type = "githubpulls"
-    notify_committers = false
-  }
 }
