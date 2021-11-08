665ab41be0087dfebf18f6be86c2b1595863c053
Jörg Thalheim <joerg@thalheim.io>
2021-11-08 13:41:12 +0100

    zfs: disable io scheduler

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -4,4 +4,8 @@
     autoSnapshot.monthly = 2;
     autoScrub.enable = true;
   };
+  # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when i nix build something.
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  '';
 }
