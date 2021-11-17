e677314673fc42f4869222d54a481ac7f1ccaecb
Jörg Thalheim <joerg@thalheim.io>
2021-11-17 20:31:45 +0100

    hass: reformat

--- nixos/eve/modules/home-assistant/default.nix
+++ nixos/eve/modules/home-assistant/default.nix
@@ -116,23 +116,21 @@ in
       }];
     };
 
-  services.nginx = {
-    virtualHosts."hass.thalheim.io" = {
-      useACMEHost = "thalheim.io";
-      forceSSL = true;
-      extraConfig = ''
-        proxy_buffering off;
-      '';
-      locations."/".extraConfig = ''
-        proxy_pass http://127.0.0.1:8123;
-        proxy_set_header Host $host;
-        proxy_redirect http:// https://;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
-      '';
-    };
+  services.nginx.virtualHosts."hass.thalheim.io" = {
+    useACMEHost = "thalheim.io";
+    forceSSL = true;
+    extraConfig = ''
+      proxy_buffering off;
+    '';
+    locations."/".extraConfig = ''
+      proxy_pass http://127.0.0.1:8123;
+      proxy_set_header Host $host;
+      proxy_redirect http:// https://;
+      proxy_http_version 1.1;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection $connection_upgrade;
+    '';
   };
 
   sops.secrets."home-assistant-secrets.yaml" = {
