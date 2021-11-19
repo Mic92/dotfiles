d5ec027bf1c3e7979b8d05ad9446b8e561708fc9
Jörg Thalheim <joerg@thalheim.io>
2021-11-19 12:16:54 +0100

    retiolum: switch to master branch

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     bme680-mqtt.inputs.utils.follows = "flake-utils";
     bme680-mqtt.inputs.nixpkgs.follows = "nixpkgs";
 
-    retiolum.url = "git+https://git.thalheim.io/Mic92/retiolum?ref=restarts";
+    retiolum.url = "git+https://git.thalheim.io/Mic92/retiolum";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
