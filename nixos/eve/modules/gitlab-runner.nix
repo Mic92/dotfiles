e7290789011260f89be65b6e9af27c6f16d481ca
Jörg Thalheim <joerg@thalheim.io>
2021-11-20 09:51:48 +0100

    run gitlab with confinment

--- nixos/eve/modules/gitlab-runner.nix
+++ nixos/eve/modules/gitlab-runner.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, lib, ... }: {
   sops.secrets.gitlab-runner-registration.owner = "gitlab-runner";
 
   services.gitlab-runner = {
@@ -22,9 +22,35 @@
   };
 
   systemd.services.gitlab-runner = {
+    confinement.enable = true;
+    confinement.packages = config.services.gitlab-runner.extraPackages;
     serviceConfig = {
       User = "gitlab-runner";
       Group = "gitlab-runner";
+      DynamicUser = lib.mkForce false;
+      Environment = [
+        "NIX_REMOTE=daemon"
+        "PAGER=cat"
+      ];
+      BindPaths = [
+        "/nix/var/nix/daemon-socket/socket"
+        "/run/nscd/socket"
+        "/var/lib/drone"
+      ];
+      BindReadOnlyPaths = [
+        "/etc/passwd:/etc/passwd"
+        "/etc/group:/etc/group"
+        "/nix/var/nix/profiles/system/etc/nix:/etc/nix"
+        "${config.environment.etc."ssl/certs/ca-certificates.crt".source}:/etc/ssl/certs/ca-certificates.crt"
+        "${config.environment.etc."ssh/ssh_known_hosts".source}:/etc/ssh/ssh_known_hosts"
+        "${builtins.toFile "ssh_config" ''
+          Host eve.thalheim.io
+            ForwardAgent yes
+        ''}:/etc/ssh/ssh_config"
+        "/etc/machine-id"
+        # channels are dynamic paths in the nix store, therefore we need to bind mount the whole thing
+        "/nix/"
+      ];
     };
   };
 
