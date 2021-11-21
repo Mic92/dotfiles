48f812a62042d48a61120b33a99d9fa515253acd
Jörg Thalheim <joerg@thalheim.io>
2021-11-21 13:23:56 +0100

    always sync -u doom after upgrade

--- nixpkgs-config/modules/emacs/default.nix
+++ nixpkgs-config/modules/emacs/default.nix
@@ -36,8 +36,7 @@ let
     if [ $(git -C $HOME/.emacs.d rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
       git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
       git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
-      $HOME/.emacs.d/bin/doom sync || true
-      YES=1 FORCE=1 $HOME/.emacs.d/bin/doom sync -u &
+      YES=1 FORCE=1 $HOME/.emacs.d/bin/doom sync -u || true
     fi
     exec ${myemacs}/bin/emacs --daemon
   '';
