8f2ab535b9f285e51c75f1032b71d5dfa7745915
Jörg Thalheim <joerg@thalheim.io>
2021-11-24 13:21:24 +0100

    move backend config to terraform

--- nixos/eve/modules/gitlab/repo-mirror.tf
+++ nixos/eve/modules/gitlab/repo-mirror.tf
@@ -13,7 +13,15 @@ terraform {
       version = "~> 0.5"
     }
   }
-  backend "http" {}
+  backend "http" {
+    address = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab"
+    lock_address = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab/lock"
+    username = "Mic92"
+    lock_method = "POST"
+    unlock_method = "DELETE"
+    retry_wait_min = "5"
+  }
 }
 
 data "sops_file" "secrets" {
