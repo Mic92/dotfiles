a412e55987a8a99f207d743196d72845828de43e
Jörg Thalheim <joerg@thalheim.io>
2021-11-25 12:35:40 +0100

    emacs: enable terraform

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -102,7 +102,7 @@
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
-       ;;terraform         ; infrastructure as code
+       terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
