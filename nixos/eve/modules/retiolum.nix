6fcad16a4973cbd62c5d7ad51adb050d4cab8f01
Jörg Thalheim <joerg@thalheim.io>
2021-11-26 10:38:39 +0100

    eve: drop ipv4 tinc

--- nixos/eve/modules/retiolum.nix
+++ nixos/eve/modules/retiolum.nix
@@ -1,11 +1,5 @@
 {
-  systemd.network.networks."retiolum".extraConfig = ''
-    [Network]
-    Address=fd42:4492:6a6d:500:8526:2adf:7451:8bbb/64
-  '';
-
   networking.retiolum = {
-    ipv4 = "10.243.29.174";
     ipv6 = "42:0:3c46:70c7:8526:2adf:7451:8bbb";
   };
 }
