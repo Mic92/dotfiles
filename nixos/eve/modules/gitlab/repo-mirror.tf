acf50b61e638a41a4f9496bdd7adc50bae0c18de
Jörg Thalheim <joerg@thalheim.io>
2021-11-29 10:12:22 +0100

    sops-nix: add own ci

--- nixos/eve/modules/gitlab/repo-mirror.tf
+++ nixos/eve/modules/gitlab/repo-mirror.tf
@@ -44,6 +44,7 @@ resource "gitlab_project" "repos" {
   mirror_overwrites_diverged_branches = true
   shared_runners_enabled              = false
   ci_config_path = lookup({
+    "Mic92/sops-nix" : ".gitlab-ci.yml@Mic92/sops-nix",
     }, each.key,
     # sane default
   ".gitlab-ci.yml@Mic92/dotfiles")
