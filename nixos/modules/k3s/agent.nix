26435f5c772bb534d5591430f44bf0e29b2481b8
Jörg Thalheim <joerg@thalheim.io>
2021-12-29 09:40:19 +0100

    k3s: fix cluster url

--- nixos/modules/k3s/agent.nix
+++ nixos/modules/k3s/agent.nix
@@ -5,5 +5,5 @@
   sops.secrets.k3s-server-token = {};
   services.k3s.role = "agent";
   services.k3s.tokenFile = config.sops.secrets.k3s-server-token.path;
-  services.k3s.serverAddr = "https://node0.NixOS-cluster2.Serverless-tum.emulab.net:6443";
+  services.k3s.serverAddr = "https://node0.nixos-cluster.Serverless-tum.emulab.net:6443";
 }
