8f8c9da1bad91bf4f463428975d4192006612cd0
Jörg Thalheim <joerg@thalheim.io>
2022-01-01 14:15:41 +0100

    add more cert checks

--- nixos/eva/modules/telegraf/private.nix
+++ nixos/eva/modules/telegraf/private.nix
@@ -287,6 +287,10 @@ in
           # nginx
           "https://devkid.net:443"
           "https://thalheim.io:443"
+          "https://tts.r:443"
+          "https://loki.r:443"
+          "https://prometheus.r:443"
+          "https://alertmanager.r:443"
           # squid
           "https://devkid.net:8889"
           # dovecot
