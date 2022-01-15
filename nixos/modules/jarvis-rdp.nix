e43aed87805f4740fa3d454a079941fa2de205fb
Jörg Thalheim <joerg@thalheim.io>
2022-01-15 21:19:50 +0100

    jarvis: add desktop entry

--- nixos/modules/jarvis-rdp.nix
+++ nixos/modules/jarvis-rdp.nix
@@ -5,8 +5,22 @@
     mode = "0440";
   };
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "jarvis-desktop" ''
+    (pkgs.runCommand "jarvis" {} ''
+      mkdir -p $out/bin $out/share/applications
+
+      cat > $out/bin/jarvis <<EOF
+      #!${pkgs.runtimeShell} -e
       exec ${pkgs.freerdp}/bin/wlfreerdp /ipv6 /v:jarvis.r:3389 /u:joerg /p:$(<${config.sops.secrets.jarvis-rdp-password.path}) /cert:tofu "$@"
+      EOF
+      chmod +x $out/bin/jarvis
+
+      cat > $out/share/applications/jarvis.desktop <<EOF
+      [Desktop Entry]
+      StartupNotify=true
+      Type=Application
+      Name=Jarvis
+      Exec=jarvis
+      EOF
     '')
   ];
 }
