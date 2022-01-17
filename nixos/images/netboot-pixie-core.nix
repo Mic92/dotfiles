ce4a8893636c89f64f344da018c158e6b8fad8b4
Jörg Thalheim <joerg@thalheim.io>
2022-01-17 15:21:24 +0100

    netboot: also include kernel cmdline

--- nixos/images/netboot-pixie-core.nix
+++ nixos/images/netboot-pixie-core.nix
@@ -9,10 +9,11 @@
 # $ sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 # $ sudo nix run '.#netboot-pixie-core'
 pkgs.writeShellScriptBin "netboot-pixie-core" ''
-  set -euo pipefail
-  init=$(${pkgs.gnugrep}/bin/grep -ohP 'init=\S+' ${netboot}/netboot.ipxe)
+  set -euo pipefail -x
+  cmdline=$(${pkgs.gnugrep}/bin/grep -ohP 'init=.+' ${netboot}/netboot.ipxe)
+  cmdline=$(echo "''${cmdline//initrd=initrd /}" )
   ${pkgs.pixiecore}/bin/pixiecore \
     boot ${netboot}/bzImage ${netboot}/initrd \
-    --cmdline "$init loglevel=4" \
+    --cmdline "$cmdline" \
     --debug --dhcp-no-bind --port 64172 --status-port 64172
 ''
