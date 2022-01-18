b5bd0495c4f5636e7bc218dd2e8d9e4f48e8b600
Jörg Thalheim <joerg@thalheim.io>
2022-01-18 22:54:57 +0100

    hass: drop PlayPodcast

--- nixos/eve/modules/home-assistant/android.nix
+++ nixos/eve/modules/home-assistant/android.nix
@@ -2,18 +2,6 @@
 
 {
   services.home-assistant.config.intent_script = {
-    PlayPodcast = {
-      speech.text = "Start podcast";
-      action = {
-        service = "notify.pushover";
-        data = {
-          title = "Tasker";
-          message = "Play podcast";
-          target = "android";
-        };
-      };
-    };
-
     FindAndroid = {
       speech.text = "Send notification";
       action = {
