59ed70334e976bc518efb76efa6f0ff543355bfc
Jörg Thalheim <joerg@thalheim.io>
2022-01-23 18:12:09 +0100

    fix ssh keys for users module

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,8 +1,8 @@
 let
-  key = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOBXTForyB6oNMK5bbGpALKU4lPsKRGxNLhrE/PnHHq7 shannan@bernie
-  '';
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOBXTForyB6oNMK5bbGpALKU4lPsKRGxNLhrE/PnHHq7 shannan@bernie"
+  ];
 in
 {
   users.extraUsers = {
@@ -12,13 +12,13 @@ in
       extraGroups = [ "audio" "wheel" "docker" "plugdev" "vboxusers" "adbusers" "input" "kvm" "wireshark" ];
       shell = "/run/current-system/sw/bin/zsh";
       uid = 1000;
-      openssh.authorizedKeys.keys = [ key ];
+      openssh.authorizedKeys.keys = keys;
     };
 
-    root.openssh.authorizedKeys.keys = [ key ];
+    root.openssh.authorizedKeys.keys = keys;
   };
 
-  boot.initrd.network.ssh.authorizedKeys = [ key ];
+  boot.initrd.network.ssh.authorizedKeys = keys;
 
   security.sudo.wheelNeedsPassword = false;
 
