845e2725d27be6c1f2de23b83293d20df2d8a759
Jörg Thalheim <joerg@thalheim.io>
2022-02-03 15:53:37 +0100

    add blog.devkid.net

--- nixos/eve/modules/nginx/devkid.net.nix
+++ nixos/eve/modules/nginx/devkid.net.nix
@@ -6,6 +6,12 @@
       root = "/var/www/devkid.net";
     };
 
+    virtualHosts."blog.devkid.net" = {
+      useACMEHost = "thalheim.io";
+      forceSSL = true;
+      root = "/var/www/devkid.net";
+    };
+
     virtualHosts."www.devkid.net" = {
       useACMEHost = "thalheim.io";
       forceSSL = true;
