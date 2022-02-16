831a06a4dc34602fd0a0c04715b34c69beba87b7
Jörg Thalheim <joerg@thalheim.io>
2022-02-16 22:28:24 +0100

    add navidrome

--- nixos/eve/modules/navidrome.nix
+++ nixos/eve/modules/navidrome.nix
@@ -1,6 +1,24 @@
-{
+{ config, ... }: {
   services.navidrome.enable = true;
   services.navidrome.settings.MusicFolder = "/data/torrent/download";
+  services.navidrome.settings.ReverseProxyWhitelist = "127.0.0.1/32";
+
+  security.acme.certs."navidrome.r".server = config.retiolum.ca.acmeURL;
+
+  services.nginx.virtualHosts."navidrome.r" = {
+    enableACME = true;
+    addSSL = true;
+    locations."/".extraConfig = ''
+      proxy_pass http://localhost:4533;
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+      proxy_set_header X-Forwarded-Protocol $scheme;
+      proxy_set_header X-Forwarded-Host $http_host;
+      proxy_set_header Remote-User krebs;
+    '';
+  };
 
   services.nginx.virtualHosts."navidrome.thalheim.io" = {
     useACMEHost = "thalheim.io";
@@ -13,6 +31,7 @@
       proxy_set_header X-Forwarded-Proto $scheme;
       proxy_set_header X-Forwarded-Protocol $scheme;
       proxy_set_header X-Forwarded-Host $http_host;
+      proxy_set_header Remote-User "";
     '';
   };
 }
