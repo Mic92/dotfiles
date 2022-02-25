4aa8dec7258b936f77ed7b5df84d05f453f24ea8
Jörg Thalheim <joerg@thalheim.io>
2022-02-25 14:33:48 +0100

    hass/light: add temmplate sensor to toggle light

--- nixos/eve/modules/home-assistant/light.nix
+++ nixos/eve/modules/home-assistant/light.nix
@@ -1,7 +1,21 @@
 { pkgs, ... }: {
   services.home-assistant.config = {
+    light = {
+      platform = "template";
+      lights.bedroom = {
+        friendly_name = "Sofa light";
+        turn_on.service = "shell_command.turn_light_on";
+        turn_off.service = "shell_command.turn_light_off";
+      };
+    };
     shell_command.toggle_light = ''
       ${pkgs.openssh}/bin/ssh -o "StrictHostKeyChecking no" light@jarvis.r hue-ble-ctl toggle E8:D4:5B:99:49:10
+    '';
+    shell_command.turn_light_on = ''
+      ${pkgs.openssh}/bin/ssh -o "StrictHostKeyChecking no" light@jarvis.r hue-ble-ctl switch_on E8:D4:5B:99:49:10
+    '';
+    shell_command.turn_light_off = ''
+      ${pkgs.openssh}/bin/ssh -o "StrictHostKeyChecking no" light@jarvis.r hue-ble-ctl switch_off E8:D4:5B:99:49:10
     '';
     intent_script.ToggleLight = {
       speech.text = "Toggled light.";
