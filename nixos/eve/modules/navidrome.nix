1dbb18fb7eab0ba4da13467bdf0e8875b60185ae
Jörg Thalheim <joerg@thalheim.io>
2022-03-11 14:07:09 +0100

    navidrome: add alias for mukke.krebsco.de

--- nixos/eve/modules/navidrome.nix
+++ nixos/eve/modules/navidrome.nix
@@ -21,6 +21,21 @@
     '';
   };
 
+  services.nginx.virtualHosts."mukke.krebsco.de" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/".extraConfig = ''
+      proxy_pass http://localhost:4533;
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+      proxy_set_header X-Forwarded-Protocol $scheme;
+      proxy_set_header X-Forwarded-Host $http_host;
+      proxy_set_header Remote-User "";
+    '';
+  };
+
   services.nginx.virtualHosts."navidrome.thalheim.io" = {
     useACMEHost = "thalheim.io";
     forceSSL = true;
