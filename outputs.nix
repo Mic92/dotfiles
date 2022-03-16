0e8792ec010b956872a626206d84d7449e9cdd8c
Jörg Thalheim <joerg@thalheim.io>
2022-03-16 19:28:44 +0100

    add lxc template for joe

--- outputs.nix
+++ outputs.nix
@@ -75,6 +75,22 @@ in {
       format = "kexec";
     };
 
+    joeserver-template = nixos-generators.nixosGenerate {
+      inherit pkgs;
+      modules = [{
+        services.openssh = {
+          enable = true;
+          passwordAuthentication = false;
+          useDns = false;
+        };
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine"
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW+YfsFtRz1h/0ubcKU+LyGfxH505yUkbWa5VtRFNWF2fjTAYGj6o5M4dt+fv1h370HXvvOBtt8sIlWQgMsD10+9mvjdXWhTcpnYPx4yWuyEERE1/1BhItrog6XJKAedbCDpQQ+POoewouiHWVAUfFByPj5RXuE8zKUeIEkGev/QKrKTLnTcS8zFs/yrokf1qYYR571B3U8IPDjpV/Y1GieG3MSNaefIMCwAAup1gPkUA0XZ4A1L7NdEiUEHlceKVu9eYiWUM+wDRunBXnLHubeGyP8KmBA7PNKgml3WWRNTZjqNQk4u9Bl+Qea5eCkD8KI257EqgXYXy0QBWNyF8X j03@l302"
+        ];
+      }];
+      format = "lxc";
+    };
+
     kexec-aarch64 = nixos-generators.nixosGenerate {
       pkgs = nixpkgs.legacyPackages.aarch64-linux;
       modules = [
