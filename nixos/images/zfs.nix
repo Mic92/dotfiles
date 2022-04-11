1e077afd87b1acbfe647fb73639345a121bc42fa
Jörg Thalheim <joerg@thalheim.io>
2022-04-11 19:05:24 +0200

    installer/zfs: add mbuffer

--- nixos/images/zfs.nix
+++ nixos/images/zfs.nix
@@ -1,6 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   boot.zfs.enableUnstable = true;
   boot.zfs.requestEncryptionCredentials = true;
   boot.supportedFilesystems = ["zfs"];
   networking.hostId = "ac174b52";
+  environment.systemPackages = [
+    pkgs.mbuffer # for sending
+  ];
 }
