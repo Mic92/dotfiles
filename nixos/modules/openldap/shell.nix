5e7dc0286e31064e9d9468d2314d310adb475648
Jörg Thalheim <joerg@thalheim.io>
2022-04-24 16:16:30 +0200

    openldap/shell: drop 20.03 pinning

--- nixos/modules/openldap/shell.nix
+++ nixos/modules/openldap/shell.nix
@@ -1,6 +1,6 @@
-{pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-20.03.tar.gz") {}}:
+#{pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {}}:
+{ pkgs ? import <nixpkgs> {}}:
 pkgs.stdenvNoCC.mkDerivation {
   name = "env";
-  # jdk is broken in unstable
   nativeBuildInputs = [pkgs.apache-directory-studio];
 }
