dc0c38b0b828eb68429d2b86cb7709cbd855a054
Jörg Thalheim <joerg@thalheim.io>
2022-05-08 14:38:46 +0200

    nix-uci: avoid rebuilds

--- openwrt/nix/nix-uci.nix
+++ openwrt/nix/nix-uci.nix
@@ -1,8 +1,13 @@
-{ buildPythonApplication, sops, lib }:
+{ buildPythonApplication, sops, lib, runCommand }:
 buildPythonApplication {
   pname = "nix-uci";
   version = "0.0.1";
-  src = ../.;
+  src = runCommand "src" {} ''
+    mkdir $out
+    cp -r ${../nix_uci} $out/nix_uci
+    install ${../setup.cfg} $out/setup.cfg
+    install ${../setup.py} $out/setup.py
+  '';
 
   meta = with lib; {
     description = "Write openwrt's UCI configuration using nixos modules";
