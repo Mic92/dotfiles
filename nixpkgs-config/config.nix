d4215d3f38e85c8b4db546d80091a7c08dba7de9
Jörg Thalheim <joerg@thalheim.io>
2022-05-08 11:41:55 +0200

    home-manager: allow unfree packages again

--- nixpkgs-config/config.nix
+++ nixpkgs-config/config.nix
@@ -3,6 +3,9 @@
   nurFun ? (import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz")),
 }: {
   allowUnfree = true;
+  # https://github.com/nix-community/home-manager/issues/2942
+  allowUnfreePredicate = (pkg: true);
+
   pulseaudio = true;
   #allowUnsupportedSystem = true;
   oraclejdk.accept_license = true;
