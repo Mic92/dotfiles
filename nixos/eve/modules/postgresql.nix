a8f2e8a19187d1469f83f97eb696253dd26c740c
Jörg Thalheim <joerg@thalheim.io>
2022-05-29 10:49:30 +0200

    postgresql: increase max connections

--- nixos/eve/modules/postgresql.nix
+++ nixos/eve/modules/postgresql.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
   services.postgresql.enable = true;
   services.postgresql.package = pkgs.postgresql_14;
+  services.postgresql.settings = {
+    max_connections = "300";
+    shared_buffers = "80MB";
+  };
   services.postgresqlBackup.enable = true;
 }
