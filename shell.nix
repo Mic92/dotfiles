33dc2ebd0e3dc38a928cfc43712c93f89cbf1fc8
Jörg Thalheim <joerg@thalheim.io>
2022-05-30 19:26:18 +0200

    use leaner mkShellNoCC

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
   perSystem = { inputs', pkgs, ... }: {
     # Definitions like this are entirely equivalent to the ones
     # you may have directly in flake.nix.
-    devShells.default = pkgs.mkShell {
+    devShells.default = pkgs.mkShellNoCC {
       sopsPGPKeyDirs = ["./nixos/secrets/keys"];
       sopsCreateGPGHome = true;
       nativeBuildInputs = [
