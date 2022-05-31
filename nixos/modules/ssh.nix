a80602893a9451ad80c14e01ccbb8fb50dcbc8d2
Jörg Thalheim <joerg@thalheim.io>
2022-05-31 21:43:56 +0200

    add login.dse.in.tum.de to known hosts

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -18,4 +18,8 @@
     Host *.dse.in.tum.de !login.dse.in.tum.de
       ProxyJump tunnel@login.dse.in.tum.de
  '';
+  programs.ssh.knownHosts."login.dse.in.tum.de" = {
+    hostNames = ["login.dse.in.tum.de"];
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOdlUylM9WIFfIYZDK8rjVYQzX+RYwIlLgsEh4j0pNx6";
+  };
 }
