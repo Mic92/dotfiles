31c49fcd013a720ffa432be653cd7a351993980d
Jörg Thalheim <joerg@thalheim.io>
2022-06-03 12:10:45 +0200

    eve: move back to vpn for ryan builder

--- nixos/eve/modules/remote-builder.nix
+++ nixos/eve/modules/remote-builder.nix
@@ -18,8 +18,9 @@
         "nixos-test"
       ];
     }
+    # direct connection sometimes break, too many connections?
     {
-      hostName = "ryan.dse.in.tum.de";
+      hostName = "ryan.r";
       maxJobs = 128;
       sshKey = config.sops.secrets.tum-builder.path;
       sshUser = "ssh-ng://nix";
@@ -31,12 +32,23 @@
       ];
     }
   ];
-
+  #programs.ssh.extraConfig = ''
+  #  Host ryan-builder
+  #    ProxyJump tunnel@login-builder
+  #    IdentityFile ${config.sops.secrets.tum-builder.path}
+  #  Host login-builder
+  #    HostName login.dse.in.tum.de
+  #    IdentityFile ${config.sops.secrets.tum-builder.path}
+  #'';
   programs.ssh.knownHosts = {
     "aarch64.nixos.community" = {
       hostNames = ["aarch64.nixos.community"];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUTz5i9u5H2FHNAmZJyoJfIGyUm/HfGhfwnc142L3ds";
     };
+    #"login-builder" = {
+    #  hostNames = ["login.dse.in.tum.de"];
+    #  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOdlUylM9WIFfIYZDK8rjVYQzX+RYwIlLgsEh4j0pNx6";
+    #};
   };
   sops.secrets.tum-builder = {};
   sops.secrets.ssh-aarch64-builder = {};
