d4e40de287eba6bc275bd27a7ef7e79dd93f82ad
Jörg Thalheim <joerg@thalheim.io>
2022-06-26 18:13:27 +0200

    attempt to switch to dynamic trigger

--- nixos/eve/modules/buildbot/master.py
+++ nixos/eve/modules/buildbot/master.py
@@ -4,9 +4,10 @@ import os
 import sys
 import json
 
-from buildbot.plugins import worker, changes, util, steps, schedulers, reporters
+from buildbot.plugins import worker, util, steps, schedulers, reporters
+from buildbot.steps.trigger import Trigger
 from buildbot.process import buildstep, logobserver
-from buildbot.process.properties import Interpolate
+from buildbot.process.properties import Properties, Interpolate
 from typing import Generator
 from pathlib import Path
 from typing import Any, Dict, List
@@ -19,6 +20,77 @@ def read_secret_file(secret_name: str) -> str:
         print(f"directory not set", file=sys.stderr)
         sys.exit(1)
     return Path(directory).joinpath(secret_name).read_text()
+
+
+class BuildTrigger(Trigger):
+    def __init__(self, scheduler: str, jobs: List[Dict[str, str]], **kwargs):
+        if "name" not in kwargs:
+            kwargs["name"] = "trigger"
+        self.jobs = jobs
+        self.config = None
+        steps.trigger.Trigger.__init__(
+            self,
+            waitForFinish=True,
+            schedulerNames=[scheduler],
+            haltOnFailure=True,
+            flunkOnFailure=True,
+            sourceStamps=[],
+            alwaysUseLatest=False,
+            updateSourceStamp=False,
+            **kwargs,
+        )
+
+    def createTriggerProperties(self, props):
+        return props
+
+    def getSchedulersAndProperties(self):
+        sch = self.schedulerNames[0]
+        triggered_schedulers = []
+        for job in self.jobs:
+            attr = job["attr"]
+            drv_path = job["drvPath"]
+            props = Properties()
+            props.setProperty("virtual_builder_name", attr, "spawner")
+            props.setProperty("virtual_builder_tags", "", "spawner")
+            props.setProperty("attr", attr, "spawner")
+            props.setProperty("drv_path", drv_path, "spawner")
+            triggered_schedulers.append((sch, props))
+        return triggered_schedulers
+        # for
+        # sch = self.schedulerNames[0]
+        # reason_excluded_env = self.config.global_env.keys()
+
+        # triggered_schedulers = []
+        # for env in self.config.matrix:
+        #    props_to_set = Properties()
+        #    props_to_set.setProperty("TRAVIS_PULL_REQUEST",
+        #                             self.getProperty("TRAVIS_PULL_REQUEST"),
+        #                             "inherit")
+        #    flat_env = {}
+        #    for k, v in env.items():
+        #        if k == "env":
+        #            props_to_set.update(v, ".travis.yml")
+        #            flat_env.update(v)
+        #        else:
+        #            props_to_set.setProperty(k, v, ".travis.yml")
+        #            flat_env[k] = v
+        #    tags = self.build.builder.config.tags
+        #    for tag in ["trunk", "try"]:
+        #        if tag in tags:
+        #            tags.remove(tag)
+        #    label_tags = sorted(
+        #        str(self.config.label_mapping.get(k, k)) + ':' +
+        #        str(self.config.label_mapping.get(v, v))
+        #        for k, v in flat_env.items() if k not in reason_excluded_env)
+        #    props_to_set.setProperty("virtual_builder_name", u" ".join(tags + label_tags),
+        #                             "spawner")
+        #    props_to_set.setProperty("virtual_builder_tags", tags + label_tags,
+        #                             "spawner")
+        #    props_to_set.setProperty("matrix_label", u"/".join(label_tags),
+        #                             "spawner")
+
+        #    triggered_schedulers.append((sch, props_to_set))
+        # return triggered_schedulers
 
 
 class GenerateStagesCommand(buildstep.ShellMixin, steps.BuildStep):
@@ -39,23 +111,26 @@ class GenerateStagesCommand(buildstep.ShellMixin, steps.BuildStep):
         if result == util.SUCCESS:
             # create a ShellCommand for each stage and add them to the build
             jobs = []
+
             for line in self.observer.getStdout().split("\n"):
                 if line != "":
                     jobs.append(json.loads(line))
-            build_steps = []
-            for job in jobs:
-                build_steps.append(
-                    steps.ShellCommand(
-                        name=job["attr"],
-                        command=f"nix build --out-link 'result-{job['attr']}' -L '{job['drvPath']}'",
-                    )
-                )
-            self.build.addStepsAfterCurrentStep(build_steps)
+            # build_steps = []
+            # for job in jobs:
+            #    build_steps.append(
+            #        BuildTrigger(
+            #            name=job["attr"],
+            #            command=f"nix build --out-link 'result-{job['attr']}' -L '{job['drvPath']}'",
+            #        )
+            #    )
+            #self.build.addStepsAfterCurrentStep(
+            #    [BuildTrigger(scheduler="nix-build", name="nix-build", jobs=jobs)]
+            #)
 
         return result
 
 
-def nix_builder_config(worker_config: List[Dict[str, str]]) -> util.BuilderConfig:
+def nix_eval_config(worker_names: List[str]) -> util.BuilderConfig:
     factory = util.BuildFactory()
     # check out the source
     factory.addStep(
@@ -67,41 +142,68 @@ def nix_builder_config(worker_config: List[Dict[str, str]]) -> util.BuilderConfi
     factory.addStep(
         GenerateStagesCommand(
             env={},
-            name="Generate build stages",
+            name="Eval flake",
             command=[
                 "nix",
                 "run",
                 "github:nix-community/nix-eval-jobs",
                 "--",
-                "--workers", "8",
+                "--workers",
+                "8",
                 "--gc-roots-dir",
                 "/var/lib/buildbot-worker/gcroot",
                 "--flake",
-                ".#hydraJobs"
+                ".#hydraJobs",
             ],
             haltOnFailure=True,
         )
     )
 
-    worker_names = [item["name"] for item in worker_config]
-    return util.BuilderConfig(name="flake", workernames=worker_names, factory=factory)
+    return util.BuilderConfig(
+        name="nix_eval", workernames=worker_names, factory=factory
+    )
+
+
+def nix_build_config(worker_names: List[str]) -> util.BuilderConfig:
+    factory = util.BuildFactory()
+    factory.addStep(
+        steps.ShellCommand(
+            env={},
+            name="Build flake attr",
+            command=[
+                "nix",
+                "build",
+                "-L",
+                "--out-link",
+                util.Interpolate("result-%(prop:attr)s"),
+                util.Property("drv_path"),
+            ],
+        )
+    )
+    return util.BuilderConfig(
+        name="nix_build",
+        workernames=worker_names,
+        properties=[],
+        collapseRequests=False,
+        env={},
+        factory=factory,
+    )
 
 
 def build_config() -> dict[str, Any]:
     c = {}
     c["buildbotNetUsageData"] = None
 
-    # pr_filter = util.ChangeFilter(
-    #    filter_fn=lambda change: change.properties["event"] == "pull_request"
-    # )
-
     c["schedulers"] = [
         schedulers.AnyBranchScheduler(
-            # name="all", change_filter=pr_filter, builderNames=["runtests"]
             name="all",
-            builderNames=["flake"],
+            builderNames=["nix_eval"],
         ),
-        schedulers.ForceScheduler(name="force", builderNames=["flake"]),
+        #schedulers.Triggerable(
+        #    name="nix-build",
+        #    builderNames=["nix-build"],
+        #),
+        schedulers.ForceScheduler(name="force", builderNames=["nix_eval"]),
     ]
 
     github_api_token = read_secret_file("github-token")
@@ -116,7 +218,9 @@ def build_config() -> dict[str, Any]:
     worker_config = json.loads(read_secret_file("github-workers"))
 
     c["workers"] = [worker.Worker(item["name"], item["pass"]) for item in worker_config]
-    c["builders"] = [nix_builder_config(worker_config)]
+    worker_names = [item["name"] for item in worker_config]
+    #c["builders"] = [nix_eval_config(worker_names), nix_build_config(worker_names)]
+    c["builders"] = [nix_eval_config(worker_names)]
 
     github_admins = os.environ.get("GITHUB_ADMINS", "").split(",")
 
