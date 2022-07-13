5e9325da808cf9ee2fb8805c5d2aa55b3ba99d76
Jörg Thalheim <joerg@thalheim.io>
2022-07-13 08:22:11 +0200

    headscale: update image

--- nixos/images/headscale.nix
+++ nixos/images/headscale.nix
@@ -10,7 +10,41 @@
 }: let
   kresd = knot-resolver.override {extraFeatures = true;};
   configFile = writeText "headscale.yaml" (builtins.toJSON {
-    db_type = "sqlite";
+    db_type = "sqlite3";
+    db_path = "/var/lib/headscale/db.sqlite";
+    server_url = "http://headscale.thalheim.io";
+    listen_addr = "0.0.0.0:8080";
+    grpc_listen_addr = "0.0.0.0:50443";
+    tls_client_auth_mode = "relaxed";
+    private_key_path = "/var/lib/headscale/private.key";
+
+    derp = {
+      server = {
+        enabled = true;
+        region_id = 999;
+        region_code = "headscale";
+        region_name = "Headscale Embedded DERP";
+        # udp
+        stun_listen_addr = "0.0.0.0:3478";
+      };
+      urls = [
+        "https://controlplane.tailscale.com/derpmap/default"
+      ];
+      auto_update_enabled = "true";
+      update_frequency = "24h";
+      disable_check_updates = false;
+      logtail.enabled = false;
+    };
+    #tls_letsencrypt_challenge_type = "TLS-ALPN-01";
+    #tls_letsencrypt_listen: ":http"
+    #tls_letsencrypt_cache_dir = "/var/lib/headscale/cache";
+    ip_prefixes = [
+      "fd7a:115c:a1e0::/48"
+      "100.64.0.0/10"
+    ];
+    ephemeral_node_inactivity_timeout = "600m";
+    tls_letsencrypt_hostname = "headscale.thalheim.io";
+    acme_email = "joerg@thalheim.io";
   });
 in
   nix2container.buildImage {
@@ -37,10 +71,10 @@ in
         "/var/lib/headscale" = {};
       };
     };
-    isolatedDeps = [
+    layers = [
       (nix2container.buildLayer {
         deps = [
-          kresd
+          headscale
           busybox
           tcpdump
         ];
