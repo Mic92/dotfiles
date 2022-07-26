e259ac2ed2644c4336a3df516012cb98a7f5df92
Jörg Thalheim <joerg@thalheim.io>
2022-07-26 09:52:37 +0200

    bypass cachix for prefetching updates

--- nixos/modules/update-prefetch.nix
+++ nixos/modules/update-prefetch.nix
@@ -3,17 +3,21 @@
   systemd.services.update-prefetch = {
     startAt = "hourly";
     script = ''
-      export PATH=${lib.makeBinPath (with pkgs; [config.nix.package pkgs.curl pkgs.iproute2 pkgs.nettools pkgs.git])}
+      export PATH=${lib.makeBinPath (with pkgs; [config.nix.package pkgs.curl pkgs.iproute2 pkgs.nettools pkgs.git pkgs.openssh])}
       # skip service if do not have a default route
       if ! ip r g 8.8.8.8; then
         exit
       fi
       HOST=$(</proc/sys/kernel/hostname)
-      nix-store --add-root /run/next-system -r "$(curl -L buildbot.thalheim.io/nix-outputs/nixos-$HOST)"
+      store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/nixos-$HOST)"
+      nix copy --no-check-sigs --from ssh-ng://binary-cache "$store_path"
+      nix-store --add-root /run/next-system -r "$store_path"
 
       if [[ -f /home/joerg/.homesick/repos/dotfiles/flake.nix ]]; then
         profile=$(nix run "/home/joerg/.homesick/repos/dotfiles/#hm" -- profile)
-        nix-store --add-root /run/next-home -r "$(curl -L buildbot.thalheim.io/nix-outputs/home-manager-$profile)"
+        store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/home-manager-$profile)"
+        nix copy --no-check-sigs --from ssh-ng://binary-cache "$store_path"
+        nix-store --add-root /run/next-home -r "$store_path"
       fi
     '';
     serviceConfig = {
@@ -21,4 +25,12 @@
       IOSchedulingClass = "idle";
     };
   };
+
+  programs.ssh.extraConfig = ''
+    Host binary-cache
+      User nix-ssh
+      HostName eve.thalheim.io
+      IdentityFile ${config.sops.secrets.binary-cache-key.path}
+  '';
+  sops.secrets.binary-cache-key.sopsFile = ../secrets/secrets.yaml;
 }
