8887ca3b01edecd83c2e95f01e42885ce26f76c5
Jörg Thalheim <joerg@thalheim.io>
2022-08-01 17:40:36 +0200

    terraform: add buildbot webhook

--- terraform/gitlab/repo-mirror.tf
+++ terraform/gitlab/repo-mirror.tf
@@ -17,28 +17,6 @@ resource "gitlab_project" "repos" {
   visibility_level = "public"
 }
 
-resource "gitlab_project_variable" "cachix-key" {
-  for_each = toset([
-    gitlab_project.repos["Mic92/dotfiles"].id,
-  ])
-  project   = each.key
-  key       = "CACHIX_SIGNING_KEY"
-  value     = data.sops_file.secrets.data["CACHIX_SIGNING_KEY"]
-  protected = true
-  masked    = true
-}
-
-resource "gitlab_project_variable" "github-token" {
-  for_each = toset([
-    gitlab_project.repos["Mic92/dotfiles"].id,
-  ])
-  project   = each.key
-  key       = "GITHUB_TOKEN"
-  value     = data.sops_file.secrets.data["GITHUB_TOKEN"]
-  protected = true
-  masked    = true
-}
-
 resource "gitlab_service_github" "github" {
   for_each       = toset(data.github_repositories.my-repos.full_names)
   project        = gitlab_project.repos[each.key].id
@@ -50,8 +28,25 @@ resource "github_repository_webhook" "gitlab" {
   for_each   = toset(data.github_repositories.my-non-archived-repos.names)
   repository = each.key
 
-  configuration {
+   configuration {
     url          = "https://gitlab.com/api/v4/projects/Mic92%2F${each.key}/mirror/pull?private_token=${data.sops_file.secrets.data["GITLAB_TOKEN"]}"
+     content_type = "form"
+     insecure_ssl = false
+   }
+
+   active = true
+
+   events = ["push", "pull_request"]
+}
+
+
+resource "github_repository_webhook" "buildbot" {
+  repository = "dotfiles"
+
+  configuration {
+    url          = "https://buildbot.thalheim.io/change_hook/github"
+    # needs to be kept in sync with eve's secrets
+    secret       = data.sops_file.secrets.data["github-webhook-secret"]
     content_type = "form"
     insecure_ssl = false
   }
@@ -59,15 +54,4 @@ resource "github_repository_webhook" "gitlab" {
   active = true
 
   events = ["push", "pull_request"]
-}
-
-resource "null_resource" "update-runner-token" {
-  provisioner "local-exec" {
-    command = <<EOT
-    val="CI_SERVER_URL=https://gitlab.com\nREGISTRATION_TOKEN=${gitlab_project.repos["Mic92/dotfiles"].runners_token}"
-sops \
-   --set "[\"gitlab-runner-registration\"] \"$val\"" \
-   ../nixos/eve/secrets/secrets.yaml
-EOT
-  }
 }
