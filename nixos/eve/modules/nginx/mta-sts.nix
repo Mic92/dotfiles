c4abb87e44284ce665494eeac7f290e35eb85881
Jörg Thalheim <joerg@thalheim.io>
2022-08-11 08:42:05 +0200

    mta-sts: enforce policy

--- nixos/eve/modules/nginx/mta-sts.nix
+++ nixos/eve/modules/nginx/mta-sts.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }:
 
 let
-  mta-sts-web.locations."=/.well-known/mta-sts.txt".alias = pkgs.writeText "mta-sts.txt" ''
-    version: STSv1
-    mode: testing
-    mx: mail.thalheim.io
-    max_age: 86400
-  '';
+  mta-sts-web = {
+    useACMEHost = "thalheim.io";
+    forceSSL = true;
+    locations."=/.well-known/mta-sts.txt".alias = pkgs.writeText "mta-sts.txt" ''
+      version: STSv1
+      mode: enforce
+      mx: mail.thalheim.io
+      max_age: 86400
+    '';
+  };
 in
 {
-  services.nginx.virtualHosts."thalheim.io" = mta-sts-web;
-  services.nginx.virtualHosts."devkid.net" = mta-sts-web;
-  services.nginx.virtualHosts."lekwati.com" = mta-sts-web;
+  services.nginx.virtualHosts."mta-sts.thalheim.io" = mta-sts-web;
+  services.nginx.virtualHosts."mta-sts.devkid.net" = mta-sts-web;
+  services.nginx.virtualHosts."mta-sts.lekwati.com" = mta-sts-web;
 }
