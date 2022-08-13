9bb65e81456e0e67f563a391364a7e465ec18ba6
Jörg Thalheim <joerg@thalheim.io>
2022-08-13 17:01:54 +0200

    test failing build

--- ci.nix
+++ ci.nix
@@ -4,5 +4,10 @@ let
 in {
   flake.hydraJobs =
     (lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations)
-    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.hmConfigurations);
+    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.hmConfigurations)
+    // {
+      alwaysFails = self.inputs.nixpkgs.legacyPackages.x86_64-linux.runCommand "foo" {} ''
+        false
+      '';
+    };
 }
