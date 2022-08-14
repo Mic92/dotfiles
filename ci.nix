4849853793405ee280dee0484f61239fa00f0448
Jörg Thalheim <joerg@thalheim.io>
2022-08-14 14:48:32 +0200

    ci: drop failing build

--- ci.nix
+++ ci.nix
@@ -4,10 +4,5 @@ let
 in {
   flake.hydraJobs =
     (lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations)
-    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.hmConfigurations)
-    // {
-      alwaysFails = self.inputs.nixpkgs.legacyPackages.x86_64-linux.runCommand "foo" {} ''
-        false
-      '';
-    };
+    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.hmConfigurations);
 }
