87e40007e91f425341133802c0fd19f5cf58ca00
Jörg Thalheim <joerg@thalheim.io>
2022-09-01 17:14:15 +0200

    tailscale: deploy derper

--- nixos/modules/tailscale.nix
+++ nixos/modules/tailscale.nix
@@ -1,4 +1,24 @@
-{
-  services.tailscale.enable = true;
-  networking.firewall.checkReversePath = "loose";
+{ pkgs, config, lib, ...}: {
+  options.tailscale.derper.hostname = lib.mkOption {
+    type = lib.types.str;
+    description = "hostname for cert";
+  };
+  config = {
+    networking.domain = "thalheim.io";
+    services.tailscale.enable = true;
+    # this port is open in university so lets use it.
+    services.tailscale.port = 51820;
+    networking.firewall.allowedUDPPorts = [ 51820 ];
+
+    networking.firewall.checkReversePath = "loose";
+
+    systemd.services.derper = {
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        StateDirectory = "derper";
+        ExecStart = "${pkgs.tailscale}/bin/derper --certdir /var/lib/derper --hostname=${config.networking.fqdn} --verify-clients";
+      };
+    };
+  };
 }
