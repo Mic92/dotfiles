7111edf76812cf99ecf5fabd7367b212678ec311
Jörg Thalheim <joerg@thalheim.io>
2022-09-13 20:36:42 +0200

    bbc: fixup state file

--- terraform/bbc/backend.tf
+++ terraform/bbc/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab"
-    lock_address   = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/31493944/terraform/state/gitlab/lock"
+    address        = "https://gitlab.com/api/v4/projects/31493944/terraform/state/bbc"
+    lock_address   = "https://gitlab.com/api/v4/projects/31493944/terraform/state/bbc/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/31493944/terraform/state/bbc/lock"
     username       = "Mic92"
     lock_method    = "POST"
     unlock_method  = "DELETE"
