768bcd5cd10e6becd85e821231ef275c7089a5fd
Jörg Thalheim <joerg@thalheim.io>
2022-10-18 11:02:27 +0200

    hass: custom components and cards

--- nixos/eve/modules/home-assistant/transmission.nix
+++ nixos/eve/modules/home-assistant/transmission.nix
@@ -4,8 +4,59 @@
   pkgs,
   ...
 }: {
+  services.home-assistant.customComponents = {
+    var = pkgs.stdenv.mkDerivation rec {
+      pname = "home-assistant-variables";
+      version = "0.15.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "snarky-snark";
+        repo = "home-assistant-variables";
+        rev = "v${version}";
+        sha256 = "sha256-HKO73B8kARuJxUv8bc0TVvpWQHXeYMe+OncodL8LpP8=";
+      };
+
+      installPhase = ''
+        cp -r custom_components/var $out
+      '';
+    };
+  };
+  services.home-assistant.customCards = {
+    "lovelace-multiline-text-input-card.js" = pkgs.stdenv.mkDerivation rec {
+      name = "lovelace-multiline-text-input-card.js";
+      src = pkgs.fetchFromGitHub {
+        owner = "faeibson";
+        repo = "lovelace-multiline-text-input-card";
+        rev = "1.0.4";
+        sha256 = "sha256-nuVijc5vuzKlMCjy/DNaZgzNnl11CXlwyLX5H9fNoH4=";
+      };
+
+      installPhase = ''
+        install -m644 lovelace-multiline-text-input-card.js $out
+      '';
+    };
+  };
   services.home-assistant.config = {
-    transmission = [{host = "yellow.r";}];
+
+    transmission = [];
+    var.torrent_url = {
+      friendly_name = "Torrent magnet url";
+      initial_value = "";
+    };
+
+    script.add_torrent.sequence = [
+      {
+        service = "transmission.add_torrent";
+        data_template = {
+          name = "Transmission";
+          torrent = ''{{ states("var.torrent_url") }}'';
+        };
+      }
+      {
+        service = "var.set";
+        data.value = "";
+        data.entity_id = ["var.torrent_url"];
+      }
+    ];
     automation = [
       {
         alias = "Completed Torrent";
