526a1fcd6dda1aa9ef15b66807d36abd803949f6
Jörg Thalheim <joerg@thalheim.io>
2022-10-25 12:22:25 +0200

    convert both systemd-network/systemd-resolved to restart-only services

--- nixos/modules/networkd.nix
+++ nixos/modules/networkd.nix
@@ -37,5 +37,10 @@ in {
   # often hangs
   systemd.services.systemd-networkd-wait-online.enable = false;
 
+  # don't take down the network for too long
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  # Services that are only restarted might be not able to resolve when this is stopped before
+  systemd.services.systemd-resolved.stopIfChanged = false;
+
   networking.dhcpcd.enable = false;
 }
