0ae0cb025f3c5b5b4298321c0d0611391e5f2913
Jörg Thalheim <joerg@thalheim.io>
2022-11-11 00:37:22 +0100

    disable zsh-nix-shell

--- nixos/modules/zsh.nix
+++ nixos/modules/zsh.nix
@@ -3,7 +3,7 @@
   programs.zsh.enableCompletion = true;
   # do this in private dotfiles
   programs.zsh.enableGlobalCompInit = false;
-  programs.zsh.interactiveShellInit = ''
-    source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
-  '';
+  #programs.zsh.interactiveShellInit = ''
+  #  source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+  #'';
 }
