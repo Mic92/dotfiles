a580a44c812d0d979d084e184c3f6dc200d74410
Jörg Thalheim <joerg@thalheim.io>
2022-12-05 10:58:40 +0100

    eva: fix eval

--- nixos/eva/configuration.nix
+++ nixos/eva/configuration.nix
@@ -26,6 +26,7 @@
     ./modules/borgbackup-repos
     ./modules/buildbot.nix
     ./modules/go-neb.nix
+    ./modules/hardware-configuration.nix
     ./modules/prometheus
     ./modules/loki.nix
     ./modules/nginx.nix
