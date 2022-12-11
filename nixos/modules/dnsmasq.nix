6347e540003dafb0049aefefb72e40d532e54e1e
Jörg Thalheim <joerg@thalheim.io>
2022-12-11 20:19:25 +0100

    dnsmas: switch to .settings

--- nixos/modules/dnsmasq.nix
+++ nixos/modules/dnsmasq.nix
@@ -26,21 +26,24 @@ in {
   # $ ip link set eth0 master internal
   services.dnsmasq.enable = !config.virtualisation.libvirtd.enable;
   services.dnsmasq.resolveLocalQueries = lib.mkDefault false; # don't use it locally for dns
-  services.dnsmasq.extraConfig = ''
-    interface=internal
+  services.dnsmasq.settings = {
+    interface = "internal";
     #interface=virttap
-    listen-address=127.0.0.1
-    dhcp-range=192.168.32.50,192.168.32.100,12h
+    listen-address = "127.0.0.1";
+    dhcp-range = "192.168.32.50,192.168.32.100,12h";
     # disable dns
-    port=0
-    ## no gateway
-    #dhcp-option=3
-    ## no dns
-    #dhcp-option=6
-    ## static leases
-    #dhcp-host=52:54:00:1d:e1:33,192.168.32.2
-    #dhcp-host=52:54:00:8f:73:d7,192.168.32.3
-  '';
+    port = 0;
+    # no gateway and no dns
+    dhcp-option = [ 3 6 ];
+    # static leases
+    dhcp-host = [
+      "52:54:00:1d:e1:33,192.168.32.2"
+      "52:54:00:8f:73:d7,192.168.32.3"
+    ];
+  };
+
+  #''
+  #'';
 
   networking.nat = {
     enable = true;
