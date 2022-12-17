fcf3d4d65311d72be85b64b481707e4122b2156d
Jörg Thalheim <joerg@thalheim.io>
2022-12-17 20:42:05 +0100

    add script to hack on kernel modules

--- nixos/modules/self.nix
+++ nixos/modules/self.nix
@@ -2,8 +2,13 @@
 # { _module.args.self = self; }
 { self, ... }:
 # ## Examples
-# Get a development shell for the currently running kernel:
+# Get a development shell for the currently running kernel i.e. to hack on a kernel module
 # $ nix develop "$(realpath /run/booted-system/flake)#nixosConfigurations.turingmachine.config.boot.kernelPackages.kernel"
+# $ tar -xvf $src
+# $ cd linux-*
+# $ zcat /proc/config.gz  > .config
+# $ make scripts prepare modules_prepare
+# $ make -C . M=drivers/block/null_blk
 {
   system.extraSystemBuilderCmds = ''
     ln -s ${self} $out/flake
