95a646103bdfa11318d9e4dff7fe19708fa9f8a6
Jörg Thalheim <joerg@thalheim.io>
2022-12-19 21:38:29 +0100

    zfs: make module more generic

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -1,20 +1,20 @@
+{ config, pkgs, lib, ... }:
 {
-  pkgs,
-  config,
-  ...
-}: {
-  services.zfs = {
+  services.zfs = lib.mkIf (config.boot.zfs.enabled) {
     autoSnapshot.enable = true;
-    autoSnapshot.monthly = 2;
+    # defaults to 12, which is a bit much given how much data is written
+    autoSnapshot.monthly = lib.mkDefault 1;
     autoScrub.enable = true;
   };
-  boot.zfs.enableUnstable = true;
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
   environment.systemPackages = [
     pkgs.zfs-prune-snapshots
   ];
+
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
   # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when i nix build something.
-  services.udev.extraRules = ''
+  services.udev.extraRules = lib.optionalString (config.boot.zfs.enabled) ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
   '';
 }
