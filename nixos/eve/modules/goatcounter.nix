34d15bc0aea5156493c4788ab723791ae265263a
Jörg Thalheim <joerg@thalheim.io>
2022-12-19 11:29:02 +0100

    fix goatcounter

--- nixos/eve/modules/goatcounter.nix
+++ nixos/eve/modules/goatcounter.nix
@@ -1,8 +1,7 @@
 { config, ... }: {
   systemd.services.goatcounter = {
     wantedBy = ["multi-user.target"];
-    # broken
-    enable = false;
+    enable = true;
 
     serviceConfig = {
       Restart = "on-failure";
@@ -12,27 +11,30 @@
         ${config.nur.repos.mic92.goatcounter}/bin/goatcounter \
           serve \
           -automigrate \
-          -listen localhost:3003 \
+          -listen localhost:3004 \
           -tls none \
-          -db postgresql://?host=/run/postgresql&dbname=goatcounter&sslmode=disable \
+          -db 'postgresql+host=/run/postgresql dbname=goatcounter sslmode=disable' \
           -smtp smtp://goatcounter@thalheim.io:''${SMTP_PASSWORD}@mail.thalheim.io
       '';
       User = "goatcounter";
     };
   };
-  #-db postgresql://?host=/run/postgresql&dbname=goatcounter&sslmode=disable \
 
   services.nginx.virtualHosts."goatcounter.thalheim.io" = {
     useACMEHost = "thalheim.io";
     forceSSL = true;
     locations."/".extraConfig = ''
-      proxy_pass http://localhost:3003;
+      proxy_pass http://localhost:3004;
       proxy_set_header Host $host;
       proxy_set_header X-Real-IP $remote_addr;
       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
       proxy_set_header X-Forwarded-Proto $scheme;
     '';
   };
+
+  environment.systemPackages = [
+    config.nur.repos.mic92.goatcounter
+  ];
 
   services.postgresql.ensureDatabases = ["goatcounter"];
   services.postgresql.ensureUsers = [
