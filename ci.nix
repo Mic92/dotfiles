05bc1bc2c7105d832f9f7b36e5d868013f00a701
Jörg Thalheim <joerg@thalheim.io>
2022-12-25 12:58:34 +0100

    ci: fix eval

--- ci.nix
+++ ci.nix
@@ -3,5 +3,5 @@
 in {
   flake.hydraJobs =
     (lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations)
-    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.hmConfigurations);
+    // (lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}" config.activation-script) self.homeConfigurations);
 }
