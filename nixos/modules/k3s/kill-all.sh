39a1ddc96b750c96d443a7c600bd0064d77179d2
Jörg Thalheim <joerg@thalheim.io>
2022-12-28 14:20:29 +0100

    kill-all.sh: shellcheck fixes

--- nixos/modules/k3s/kill-all.sh
+++ nixos/modules/k3s/kill-all.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -eux -o pipefail
 shopt -s nullglob
 systemctl stop containerd k3s
-find /sys/fs/cgroup/systemd/system.slice/containerd.service* /sys/fs/cgroup/systemd/kubepods* /sys/fs/cgroup/kubepods* -name cgroup.procs | \
-    xargs -r cat | xargs -r kill -9
+find /sys/fs/cgroup/systemd/system.slice/containerd.service* /sys/fs/cgroup/systemd/kubepods* /sys/fs/cgroup/kubepods* -name cgroup.procs -print0 | \
+    xargs -0 -r cat | xargs -r kill -9
 mount | awk '/\/var\/lib\/kubelet|\/run\/netns|\/run\/containerd/ {print $3}' | xargs -r umount
-dataset=$((grep /var/lib/containerd/io.containerd.snapshotter.v1.zfs /proc/mounts || :) | awk '{print $1}')
+dataset=$( (grep /var/lib/containerd/io.containerd.snapshotter.v1.zfs /proc/mounts || :) | awk '{print $1}')
 if [[ -n "$dataset" ]]; then
     zfs destroy -R "$dataset"
 fi
