1d62d14fd7628181500839d453ed08e7a251be08
Jörg Thalheim <joerg@thalheim.io>
2022-12-30 01:18:42 +0100

    drop sshd module

--- nixos/eva/modules/sshd.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [ ../../modules/sshd ];
-  services.openssh.extraConfig = ''
-    ListenAddress 10.243.29.185
-    ListenAddress [42:0:3c46:8a42:2b1:5ef8:7562:676a]
-  '';
-  services.openssh.startWhenNeeded = false;
-}
