d9fe0a99a004b33fe0c3667f797849bf28073380
Jörg Thalheim <joerg@thalheim.io>
2022-12-31 09:41:08 +0100

    apply statix fixes

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 # Other useful settings come from srvos's zfs module
 {
-  config = lib.mkIf (config.boot.zfs.enabled) {
+  config = lib.mkIf config.boot.zfs.enabled {
     environment.systemPackages = [
       pkgs.zfs-prune-snapshots
     ];
