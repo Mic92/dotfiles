d87fab8e64177fe95a844ae50ec9c6cd2e691f20
Jörg Thalheim <joerg@thalheim.io>
2023-01-18 17:03:45 +0100

    fix treefmt

--- home-manager/flake-module.nix
+++ home-manager/flake-module.nix
@@ -1,30 +1,32 @@
 { self, inputs, ... }: {
-  perSystem = { pkgs, ... }: let
-    homeManagerConfiguration =
-      { extraModules ? [ ]
-      , system ? "x86_64-linux"
-      ,
-      }: (inputs.home-manager.lib.homeManagerConfiguration {
-        modules = [
-          {
-            _module.args.self = self;
-            _module.args.inputs = self.inputs;
-            imports =
-              extraModules
-              ++ [
-                ./common.nix
-                inputs.nur.hmModules.nur
-              ];
-            home.username = "joerg";
-            home.homeDirectory = "/home/joerg";
-          }
-        ];
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      });
-  in {
-    apps.hm = {
-      type = "app";
-      program = "${pkgs.writeShellScriptBin "hm" ''
+  perSystem = { pkgs, ... }:
+    let
+      homeManagerConfiguration =
+        { extraModules ? [ ]
+        , system ? "x86_64-linux"
+        ,
+        }: (inputs.home-manager.lib.homeManagerConfiguration {
+          modules = [
+            {
+              _module.args.self = self;
+              _module.args.inputs = self.inputs;
+              imports =
+                extraModules
+                ++ [
+                  ./common.nix
+                  inputs.nur.hmModules.nur
+                ];
+              home.username = "joerg";
+              home.homeDirectory = "/home/joerg";
+            }
+          ];
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        });
+    in
+    {
+      apps.hm = {
+        type = "app";
+        program = "${pkgs.writeShellScriptBin "hm" ''
         set -x
         export PATH=${pkgs.lib.makeBinPath [pkgs.git pkgs.coreutils pkgs.nix pkgs.jq pkgs.unixtools.hostname]}
         declare -A profiles=(["turingmachine"]="desktop" ["eddie"]="desktop" ["eve"]="eve" ["bernie"]="bernie")
@@ -35,35 +37,35 @@
         fi
         ${inputs.home-manager.packages.${pkgs.system}.home-manager}/bin/home-manager --flake "${self}#$profile" "$@"
       ''}/bin/hm";
-    };
-    legacyPackages = {
-      homeConfigurations = {
-        common = homeManagerConfiguration { };
+      };
+      legacyPackages = {
+        homeConfigurations = {
+          common = homeManagerConfiguration { };
 
-        desktop = homeManagerConfiguration {
-          extraModules = [
-            ./desktop.nix
-            inputs.nix-index-database.hmModules.nix-index
-            inputs.hyprland.homeManagerModules.default
-            ({ pkgs, ... }: {
-              programs.waybar.package = inputs.hyprland.packages.${pkgs.system}.waybar-hyprland;
-              wayland.windowManager.hyprland.enable = true;
-              home.packages = [
-                inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+          desktop = homeManagerConfiguration {
+            extraModules = [
+              ./desktop.nix
+              inputs.nix-index-database.hmModules.nix-index
+              inputs.hyprland.homeManagerModules.default
+              ({ pkgs, ... }: {
+                programs.waybar.package = inputs.hyprland.packages.${pkgs.system}.waybar-hyprland;
+                wayland.windowManager.hyprland.enable = true;
+                home.packages = [
+                  inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
 
-                inputs.nurl.packages.${pkgs.system}.default
-              ];
-            })
-          ];
-        };
+                  inputs.nurl.packages.${pkgs.system}.default
+                ];
+              })
+            ];
+          };
 
-        eve = homeManagerConfiguration {
-          extraModules = [ ./eve.nix ];
-        };
-        bernie = homeManagerConfiguration {
-          extraModules = [ ./bernie.nix ];
+          eve = homeManagerConfiguration {
+            extraModules = [ ./eve.nix ];
+          };
+          bernie = homeManagerConfiguration {
+            extraModules = [ ./bernie.nix ];
+          };
         };
       };
     };
-  };
 }
