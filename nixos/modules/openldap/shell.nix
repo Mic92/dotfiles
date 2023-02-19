f4d22afa715be9bf5dad0939a039f3c687f772f7
Jörg Thalheim <joerg@thalheim.io>
2023-02-19 06:36:03 +0100

    openldap: pin old nixpkgs for apache-directory-studio

--- nixos/modules/openldap/shell.nix
+++ nixos/modules/openldap/shell.nix
@@ -1,5 +1,5 @@
-#{pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {}}:
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") { } }:
+#{ pkgs ? import <nixpkgs> { } }:
 pkgs.stdenvNoCC.mkDerivation {
   name = "env";
   nativeBuildInputs = [ pkgs.apache-directory-studio ];
