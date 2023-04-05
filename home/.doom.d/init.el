4efdef4e1859b26f86cc6b252bba2946edbf4019
Jörg Thalheim <joerg@thalheim.io>
2023-04-05 12:35:35 +0200

    update copilot settings

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -19,7 +19,7 @@
        ;;japanese
 
        :completion
-       company             ; the ultimate code completion backend
+       (company +childframe) ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
