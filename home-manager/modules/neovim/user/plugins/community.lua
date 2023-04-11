3f764cfe724e91ad1fec40bc6c8f4ed9bcfbab92
Jörg Thalheim <joerg@thalheim.io>
2023-04-11 12:24:41 +0000

    nvim: cleanup plugin config

--- home-manager/modules/neovim/user/plugins/community.lua
+++ home-manager/modules/neovim/user/plugins/community.lua
@@ -3,28 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
- --{ import = "astrocommunity.colorscheme.catppuccin" },
- --{ "catppuccin",
- --  opts = {
- --    flavour = "mocha",
- --  }
- --},
  { import = "astrocommunity.colorscheme.gruvbox" },
  { import = "astrocommunity.completion.copilot-lua" },
- { -- further customize the options set by the community
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+ { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
