a453c25fc13c99da129fdcd6208b1444f997cdc6
Jörg Thalheim <joerg@thalheim.io>
2023-04-11 12:18:50 +0000

    switch to gruvbox

--- home-manager/modules/neovim/user/init.lua
+++ home-manager/modules/neovim/user/init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "catppuccin-mocha",
+  colorscheme = "gruvbox",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
--- home-manager/modules/neovim/user/plugins/community.lua
+++ home-manager/modules/neovim/user/plugins/community.lua
@@ -3,12 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
- { import = "astrocommunity.colorscheme.catppuccin" },
- { "catppuccin",
-   opts = {
-     flavour = "mocha",
-   }
- },
+ --{ import = "astrocommunity.colorscheme.catppuccin" },
+ --{ "catppuccin",
+ --  opts = {
+ --    flavour = "mocha",
+ --  }
+ --},
+ { import = "astrocommunity.colorscheme.gruvbox" },
  { import = "astrocommunity.completion.copilot-lua" },
  { -- further customize the options set by the community
     "copilot.lua",
