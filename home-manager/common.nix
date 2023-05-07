39862f7dfd68e72ce72e35bc8a41fe3b57a57683
Jörg Thalheim <joerg@thalheim.io>
2023-05-07 07:32:47 +0200

    comma: switch back to upstream

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -14,14 +14,7 @@
         #(pkgs.callPackage /home/joerg/git/nix-review {})
         config.nur.repos.mic92.nixpkgs-review-unstable
         config.nur.repos.mic92.tmux-thumbs
-        (callPackage
-          (fetchFromGitHub {
-            owner = "nix-community";
-            repo = "comma";
-            rev = "02e3e5545b0c62595a77f3d5de1223c536af0614";
-            sha256 = "sha256-WBIQmwlkb/GMoOq+Dnyrk8YmgiM/wJnc5HYZP8Uw72E=";
-          })
-          { })
+        comma
         nix-prefetch
 
         hexyl
