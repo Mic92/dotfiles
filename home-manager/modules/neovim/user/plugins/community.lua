fe3592f75782850b6debaa3989d9f191776ee8f4
Jörg Thalheim <joerg@thalheim.io>
2023-05-30 08:48:50 +0000

    nvim: fix colorscheme import

--- home-manager/modules/neovim/user/plugins/community.lua
+++ home-manager/modules/neovim/user/plugins/community.lua
@@ -3,7 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
- { import = "astrocommunity.colorscheme.gruvbox" },
+ { import = "astrocommunity.colorscheme.gruvbox-nvim" },
  { import = "astrocommunity.completion.copilot-lua" },
  { import = "astrocommunity.completion.copilot-lua-cmp" },
  { import = "astrocommunity.project.project-nvim" },
