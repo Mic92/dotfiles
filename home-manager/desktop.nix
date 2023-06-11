e26cb2c5da95c0ea9450e0735f2b78205d893cc6
Jörg Thalheim <joerg@thalheim.io>
2023-06-11 05:13:42 +0200

    drop lorri from home-manager

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -81,7 +81,6 @@
       zoom-us
       gnupg
       jmtpfs # needed for charging? WTF
-      lorri
       (pkgs.runCommand "slack-aliases" { } ''
         mkdir -p $out/bin
         declare -A rooms=([manifold]=manifoldfinance \
