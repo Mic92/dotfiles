91c1ba64127a63249d43e1e05c520832bc9262cf
Jörg Thalheim <joerg@thalheim.io>
2023-06-13 18:26:29 +0200

    comment no-hz

--- nixos/modules/no-hz.nix
+++ nixos/modules/no-hz.nix
@@ -1,3 +1,5 @@
 {
+  # NixOS produces many wakeups per second, which is bad for battery life.
+  # This kernel parameter disables the timer tick on the last 4 cores
   boot.kernelParams = [ "nohz_full=4-7" ];
 }
