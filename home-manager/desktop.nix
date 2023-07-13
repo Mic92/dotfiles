8bc6b93f76bea4ea49ad4128e07008dc5b1c1649
Jörg Thalheim <joerg@thalheim.io>
2023-07-13 14:58:13 +0200

    add tea for gitea

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -25,120 +25,119 @@
 
   services.syncthing.enable = true;
 
-  home.packages = with pkgs;
-    [
-      league-of-moveable-type
-      dejavu_fonts
-      ubuntu_font_family
-      unifont
-      twitter-color-emoji
-      upterm
-      eternal-terminal
-      gimp
+  home.packages = with pkgs; [
+    league-of-moveable-type
+    dejavu_fonts
+    ubuntu_font_family
+    unifont
+    twitter-color-emoji
+    upterm
+    eternal-terminal
+    gimp
 
-      arandr
-      signal-desktop
-      config.nur.repos.mic92.pandoc-bin
-      nix-output-monitor
-      gnome.adwaita-icon-theme
-      hicolor-icon-theme
-      graphicsmagick
-      aspell
-      aspellDicts.de
-      aspellDicts.fr
-      aspellDicts.en
-      hunspell
-      hunspellDicts.en-gb-ise
-      dino
-      foot
-      screen-message
-      sshfs-fuse
-      sshuttle
-      jq
-      go2nix
-      cheat
-      hydra-check
-      xdg-utils
-      patool
-      tio
+    arandr
+    signal-desktop
+    config.nur.repos.mic92.pandoc-bin
+    nix-output-monitor
+    gnome.adwaita-icon-theme
+    hicolor-icon-theme
+    graphicsmagick
+    aspell
+    aspellDicts.de
+    aspellDicts.fr
+    aspellDicts.en
+    hunspell
+    hunspellDicts.en-gb-ise
+    dino
+    foot
+    screen-message
+    sshfs-fuse
+    sshuttle
+    jq
+    go2nix
+    cheat
+    hydra-check
+    xdg-utils
+    patool
+    tio
 
-      (mpv.override {
-        scripts = [ mpvScripts.mpris ];
-      })
-      playerctl
-      yt-dlp
-      mumble
-      ferdium
-      kubectl
-      hyperfine
-      inkscape
+    (mpv.override {
+      scripts = [ mpvScripts.mpris ];
+    })
+    playerctl
+    yt-dlp
+    mumble
+    ferdium
+    kubectl
+    hyperfine
+    inkscape
 
-      q
-      rbw
-      isync
-      # to fix xdg-open
-      glib
-      zoom-us
-      gnupg
-      jmtpfs # needed for charging? WTF
-      (pkgs.runCommand "slack-aliases" { } ''
-        mkdir -p $out/bin
-        declare -A rooms=([manifold]=manifoldfinance \
-               [numtide]=numtide \
-               ["numtide-labs"]="numtide-labs" \
-               ["tum"]="ls1-tum" \
-               ["tum-courses"]="ls1-tum-course")
-        for name in "''${!rooms[@]}"; do
-          cat > "$out/bin/slack-''${name}" <<EOF
-        #!${runtimeShell}
-        exec chromium --app="https://''${rooms[$name]}.slack.com" "$@"
-        EOF
-        done
-        chmod +x $out/bin/slack-*
-      '')
-      (pkgs.writeScriptBin "jellyfinmediaplayer" ''
-        # bluetooth speaker
-        bluetoothctl connect E6:4D:D6:0A:CC:9B &
-        systemd-inhibit \
-          --why="Jellyfin Media Player" \
-          --who="Jellyfin Media Player" \
-          --mode=block \
-          ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer
-      '')
-      #(retroarch.override {
-      #  cores = [
-      #    libretro.bsnes-hd
-      #    libretro.mupen64plus
-      #    libretro.beetle-psx-hw
-      #    libretro.dolphin
-      #    libretro.pcsx2
-      #  ];
-      #})
+    q
+    rbw
+    isync
+    # to fix xdg-open
+    glib
+    zoom-us
+    gnupg
+    jmtpfs # needed for charging? WTF
+    (pkgs.runCommand "slack-aliases" { } ''
+      mkdir -p $out/bin
+      declare -A rooms=([manifold]=manifoldfinance \
+             [numtide]=numtide \
+             ["numtide-labs"]="numtide-labs" \
+             ["tum"]="ls1-tum" \
+             ["tum-courses"]="ls1-tum-course")
+      for name in "''${!rooms[@]}"; do
+        cat > "$out/bin/slack-''${name}" <<EOF
+      #!${runtimeShell}
+      exec chromium --app="https://''${rooms[$name]}.slack.com" "$@"
+      EOF
+      done
+      chmod +x $out/bin/slack-*
+    '')
+    (pkgs.writeScriptBin "jellyfinmediaplayer" ''
+      # bluetooth speaker
+      bluetoothctl connect E6:4D:D6:0A:CC:9B &
+      systemd-inhibit \
+        --why="Jellyfin Media Player" \
+        --who="Jellyfin Media Player" \
+        --mode=block \
+        ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer
+    '')
+    #(retroarch.override {
+    #  cores = [
+    #    libretro.bsnes-hd
+    #    libretro.mupen64plus
+    #    libretro.beetle-psx-hw
+    #    libretro.dolphin
+    #    libretro.pcsx2
+    #  ];
+    #})
 
-      (pkgs.writeScriptBin "rhasspy-play" ''
-        #!${pkgs.runtimeShell}
-        set -eux -o pipefail
-        export PATH=${pkgs.pulseaudioFull}/bin:$PATH
-        sink=alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink
+    (pkgs.writeScriptBin "rhasspy-play" ''
+      #!${pkgs.runtimeShell}
+      set -eux -o pipefail
+      export PATH=${pkgs.pulseaudioFull}/bin:$PATH
+      sink=alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink
 
-        if pamixer --get-mute --sink="$sink"; then
-            pamixer --sink=$sink --unmute
-            paplay --device=$sink
-            pamixer --sink=$sink --mute
-        else
-            paplay --device=$sink
-        fi
-      '')
+      if pamixer --get-mute --sink="$sink"; then
+          pamixer --sink=$sink --unmute
+          paplay --device=$sink
+          pamixer --sink=$sink --mute
+      else
+          paplay --device=$sink
+      fi
+    '')
 
-      nixos-shell
-    ]
-    ++ (with config.nur.repos.mic92; [
-      speedscope
-      inxi
-      source-code-pro-nerdfonts
-      edge-gpt
-      (pkgs.nerdfonts.override {
-        fonts = [ "FiraCode" ];
-      })
-    ]);
+    nixos-shell
+  ]
+  ++ (with config.nur.repos.mic92; [
+    speedscope
+    inxi
+    source-code-pro-nerdfonts
+    edge-gpt
+    (pkgs.nerdfonts.override {
+      fonts = [ "FiraCode" ];
+    })
+  ]);
 }
