9bd39af7c83ba48c23b190bbc56e6ee0d21f96ca
zowoq <59103226+zowoq@users.noreply.github.com>
2023-07-16 15:47:52 +1000

    eva/telegraf: add lemmy.nix-community.org

--- nixos/eva/modules/telegraf/nix-community.nix
+++ nixos/eva/modules/telegraf/nix-community.nix
@@ -14,6 +14,12 @@ in
   services.telegraf.extraConfig.inputs = {
     http_response = [
       {
+        urls = [ "https://lemmy.nix-community.org/" ];
+        response_string_match = "Lemmy for Nix";
+        tags.host = "web01.nix-community.org";
+        tags.org = "nix-community";
+      }
+      {
         urls = [ "https://nur-update.nix-community.org/" ];
         response_string_match = "NUR update endpoint";
         tags.host = "build03.nix-community.org";
