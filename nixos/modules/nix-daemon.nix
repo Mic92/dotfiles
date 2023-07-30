4458f18fe6ccc933f6b7df32e5e5f00ca0b1fc9a
Jörg Thalheim <joerg@thalheim.io>
2023-07-30 09:26:51 +0200

    nix-daemon: unset nix package

--- nixos/modules/nix-daemon.nix
+++ nixos/modules/nix-daemon.nix
@@ -1,6 +1,5 @@
 { lib
 , config
-, pkgs
 , ...
 }:
 {
@@ -11,7 +10,6 @@
     gc.options = "--delete-older-than 10d";
     # should be enough?
     nrBuildUsers = lib.mkDefault 32;
-    package = pkgs.nixVersions.unstable;
 
     settings = {
       # for nix-direnv
