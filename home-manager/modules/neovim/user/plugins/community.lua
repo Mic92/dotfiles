6a02451f07ec0ae3ceebcb0be9cbb87d0b8e8721
Jörg Thalheim <joerg@thalheim.io>
2023-08-07 00:00:37 +0200

    add chatgpt.nvim

--- home-manager/modules/neovim/user/plugins/community.lua
+++ home-manager/modules/neovim/user/plugins/community.lua
@@ -1,8 +1,8 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+ -- Add the community repository of plugin specifications
+ "AstroNvim/astrocommunity",
+ -- example of imporing a plugin, comment out to use it or add your own
+ -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
  { import = "astrocommunity.completion.copilot-lua" },
  { import = "astrocommunity.completion.copilot-lua-cmp" },
@@ -19,4 +19,11 @@ return {
  { import = "astrocommunity.terminal-integration.vim-tpipeline" },
  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
  { import = "astrocommunity.editing-support.auto-save-nvim" },
+ { import = "astrocommunity.editing-support.chatgpt-nvim" },
+ {
+  "jackMort/ChatGPT.nvim",
+  opts = {
+   api_key_cmd = "rbw get openai-api-key",
+  },
+ },
 }
