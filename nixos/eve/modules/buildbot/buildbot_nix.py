3023aa0f5a4d9131ad73d7b32f655d30ea1b3617
Jörg Thalheim <joerg@thalheim.io>
2023-08-21 17:55:59 +0200

    fix merge-check

--- nixos/eve/modules/buildbot/buildbot_nix.py
+++ nixos/eve/modules/buildbot/buildbot_nix.py
@@ -444,6 +444,19 @@ def nix_eval_config(
         )
     )
     if len(automerge_users) > 0:
+        def check_auto_merge(step: steps.BuildStep) -> bool:
+            log = yield step.addLog("merge-check")
+            if step.getProperty("event") != "pull_request":
+                log.addStderr("Not a pull request")
+                return False
+            if step.getProperty("github.repository.default_branch") != step.getProperty("branch"):
+                log.addStderr("Not on default branch")
+                return False
+            if not any(owner in automerge_users for owner in step.getProperty("owners")):
+                log.addStderr(f"PR opened by {step.getProperty('owner')} not in {automerge_users}")
+                return False
+            return True
+
         factory.addStep(
             steps.ShellCommand(
                 name="Merge pull-request",
@@ -457,12 +470,7 @@ def nix_eval_config(
                     "--rebase",
                     util.Property("pullrequesturl"),
                 ],
-                doStepIf=lambda step: step.getProperty("basename")
-                == step.getProperty("github.repository.default_branch")
-                and steps.getProperty("event") == "pull_request"
-                and any(
-                    owner in automerge_users for owner in step.getProperty("owners")
-                ),
+                doStepIf=check_auto_merge,
             )
         )
 
