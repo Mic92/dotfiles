e34f9c8f1a81c1351e5667210ecc232794db8e94
Jörg Thalheim <joerg@thalheim.io>
2023-08-21 18:11:30 +0200

    update-prefetch: fix home manager

--- nixos/modules/update-prefetch.nix
+++ nixos/modules/update-prefetch.nix
@@ -19,7 +19,8 @@
 
       if [[ -f /home/joerg/.homesick/repos/dotfiles/flake.nix ]]; then
         profile=$(nix run "/home/joerg/.homesick/repos/dotfiles/#hm" -- profile)
-        store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/home-manager-$profile)"
+        system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+        store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/home-manager-$profile-$system)"
         nix-store --add-root /run/next-home -r "$store_path"
       fi
     '';
