5c58a9d7f7bc8f5fb63220ab964c696e300acd79
Jörg Thalheim <joerg@thalheim.io>
2023-08-21 17:35:53 +0200

    openldap: fix indentation on schema rules

--- nixos/modules/openldap/default.nix
+++ nixos/modules/openldap/default.nix
@@ -47,9 +47,9 @@
         olcDatabase = "{2}monitor";
         objectClass = [ "olcDatabaseConfig" "olcMonitorConfig" ];
         olcAccess = [
-          ''            {0}to *
-                         by dn.exact="cn=netdata,ou=system,ou=users,dc=eve" read
-                         by * none''
+          ''{0}to *
+             by dn.exact="cn=netdata,ou=system,ou=users,dc=eve" read
+             by * none''
         ];
       };
 
@@ -67,10 +67,10 @@
         cn = "{1}squid";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.16548.1.2.4 NAME 'proxyUser'
-                        SUP top AUXILIARY
-                        DESC 'Account to allow a user to use the Squid proxy'
-                        MUST ( mail $ userPassword ))
+          ''(1.3.6.1.4.1.16548.1.2.4 NAME 'proxyUser'
+            SUP top AUXILIARY
+            DESC 'Account to allow a user to use the Squid proxy'
+            MUST ( mail $ userPassword ))
           ''
         ];
       };
@@ -78,10 +78,10 @@
         cn = "{1}grafana";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.28293.1.2.5 NAME 'grafana'
-                         SUP uidObject AUXILIARY
-                         DESC 'Added to an account to allow grafana access'
-                         MUST (mail))
+          ''(1.3.6.1.4.1.28293.1.2.5 NAME 'grafana'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow grafana access'
+             MUST (mail))
           ''
         ];
       };
@@ -89,62 +89,62 @@
         cn = "{2}postfix";
         objectClass = "olcSchemaConfig";
         olcAttributeTypes = [
-          ''            (1.3.6.1.4.1.12461.1.1.1 NAME 'postfixTransport'
-                         DESC 'A string directing postfix which transport to use'
-                         EQUALITY caseExactIA5Match
-                         SYNTAX 1.3.6.1.4.1.1466.115.121.1.26{20} SINGLE-VALUE)''
-          ''            (1.3.6.1.4.1.12461.1.1.5 NAME 'mailbox'
-                         DESC 'The absolute path to the mailbox for a mail account in a non-default location'
-                         EQUALITY caseExactIA5Match
-                         SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)''
-          ''            (1.3.6.1.4.1.12461.1.1.6 NAME 'quota'
-                         DESC 'A string that represents the quota on a mailbox'
-                         EQUALITY caseExactIA5Match
-                         SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)''
-          ''            (1.3.6.1.4.1.12461.1.1.8 NAME 'maildrop'
-                         DESC 'RFC822 Mailbox - mail alias'
-                         EQUALITY caseIgnoreIA5Match
-                         SUBSTR caseIgnoreIA5SubstringsMatch
-                         SYNTAX 1.3.6.1.4.1.1466.115.121.1.26{256})''
+          ''(1.3.6.1.4.1.12461.1.1.1 NAME 'postfixTransport'
+             DESC 'A string directing postfix which transport to use'
+             EQUALITY caseExactIA5Match
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.26{20} SINGLE-VALUE)''
+          ''(1.3.6.1.4.1.12461.1.1.5 NAME 'mailbox'
+             DESC 'The absolute path to the mailbox for a mail account in a non-default location'
+             EQUALITY caseExactIA5Match
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)''
+          ''(1.3.6.1.4.1.12461.1.1.6 NAME 'quota'
+             DESC 'A string that represents the quota on a mailbox'
+             EQUALITY caseExactIA5Match
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 SINGLE-VALUE)''
+          ''(1.3.6.1.4.1.12461.1.1.8 NAME 'maildrop'
+             DESC 'RFC822 Mailbox - mail alias'
+             EQUALITY caseIgnoreIA5Match
+             SUBSTR caseIgnoreIA5SubstringsMatch
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.26{256})''
         ];
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.12461.1.2.1 NAME 'mailAccount'
-                         SUP top AUXILIARY
-                         DESC 'Mail account objects'
-                         MUST ( mail $ userPassword )
-                         MAY (  cn $ description $ quota))''
-          ''            (1.3.6.1.4.1.12461.1.2.2 NAME 'mailAlias'
-                         SUP top STRUCTURAL
-                         DESC 'Mail aliasing/forwarding entry'
-                         MUST ( mail $ maildrop )
-                         MAY ( cn $ description ))''
-          ''            (1.3.6.1.4.1.12461.1.2.3 NAME 'mailDomain'
-                         SUP domain STRUCTURAL
-                         DESC 'Virtual Domain entry to be used with postfix transport maps'
-                         MUST ( dc )
-                         MAY ( postfixTransport $ description  ))''
-          ''            (1.3.6.1.4.1.12461.1.2.4 NAME 'mailPostmaster'
-                         SUP top AUXILIARY
-                         DESC 'Added to a mailAlias to create a postmaster entry'
-                         MUST roleOccupant)''
+          ''(1.3.6.1.4.1.12461.1.2.1 NAME 'mailAccount'
+             SUP top AUXILIARY
+             DESC 'Mail account objects'
+             MUST ( mail $ userPassword )
+             MAY (  cn $ description $ quota))''
+          ''(1.3.6.1.4.1.12461.1.2.2 NAME 'mailAlias'
+             SUP top STRUCTURAL
+             DESC 'Mail aliasing/forwarding entry'
+             MUST ( mail $ maildrop )
+             MAY ( cn $ description ))''
+          ''(1.3.6.1.4.1.12461.1.2.3 NAME 'mailDomain'
+             SUP domain STRUCTURAL
+             DESC 'Virtual Domain entry to be used with postfix transport maps'
+             MUST ( dc )
+             MAY ( postfixTransport $ description  ))''
+          ''(1.3.6.1.4.1.12461.1.2.4 NAME 'mailPostmaster'
+             SUP top AUXILIARY
+             DESC 'Added to a mailAlias to create a postmaster entry'
+             MUST roleOccupant)''
         ];
       };
       "cn={1}openssh,cn=schema".attrs = {
         cn = "{1}openssh";
         objectClass = "olcSchemaConfig";
         olcAttributeTypes = [
-          ''            (1.3.6.1.4.1.24552.500.1.1.1.13
-                      NAME 'sshPublicKey'
-                      DESC 'MANDATORY: OpenSSH Public key'
-                      EQUALITY octetStringMatch
-                      SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )''
+          ''(1.3.6.1.4.1.24552.500.1.1.1.13
+             NAME 'sshPublicKey'
+             DESC 'MANDATORY: OpenSSH Public key'
+             EQUALITY octetStringMatch
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )''
         ];
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.24552.500.1.1.2.0
-                      NAME 'ldapPublicKey'
-                      SUP top AUXILIARY
-                      DESC 'MANDATORY: OpenSSH LPK objectclass'
-                      MUST ( sshPublicKey $ uid ))
+          ''(1.3.6.1.4.1.24552.500.1.1.2.0
+             NAME 'ldapPublicKey'
+             SUP top AUXILIARY
+             DESC 'MANDATORY: OpenSSH LPK objectclass'
+             MUST ( sshPublicKey $ uid ))
           ''
         ];
       };
@@ -152,10 +152,10 @@
         cn = "{1}nginx";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.28295.1.2.4 NAME 'nginx'
-                         SUP top AUXILIARY
-                         DESC 'Added to an account to allow nginx access'
-                         MUST ( mail $ userPassword ))
+          ''(1.3.6.1.4.1.28295.1.2.4 NAME 'nginx'
+             SUP top AUXILIARY
+             DESC 'Added to an account to allow nginx access'
+             MUST ( mail $ userPassword ))
           ''
         ];
       };
@@ -164,28 +164,28 @@
         cn = "{1}nextcloud";
         objectClass = "olcSchemaConfig";
         olcAttributeTypes = [
-          ''            (1.3.6.1.4.1.39430.1.1.1
-                       NAME 'ownCloudQuota'
-                       DESC 'User Quota (e.g. 15 GB)'
-                       SYNTAX '1.3.6.1.4.1.1466.115.121.1.15')''
+          ''(1.3.6.1.4.1.39430.1.1.1
+             NAME 'ownCloudQuota'
+             DESC 'User Quota (e.g. 15 GB)'
+             SYNTAX '1.3.6.1.4.1.1466.115.121.1.15')''
         ];
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.39430.1.2.1
-                       NAME 'ownCloud'
-                       DESC 'ownCloud LDAP Schema'
-                       AUXILIARY
-                       MUST ( mail $ userPassword )
-                       MAY ( ownCloudQuota ))''
+          ''(1.3.6.1.4.1.39430.1.2.1
+             NAME 'ownCloud'
+             DESC 'ownCloud LDAP Schema'
+             AUXILIARY
+             MUST ( mail $ userPassword )
+             MAY ( ownCloudQuota ))''
         ];
       };
       "cn={1}gitlab,cn=schema".attrs = {
         cn = "{1}gitlab";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            ( 1.3.6.1.4.1.28293.1.2.4 NAME 'gitlab'
-                      SUP uidObject AUXILIARY
-                      DESC 'Added to an account to allow gitlab access'
-                      MUST (mail))
+          ''(1.3.6.1.4.1.28293.1.2.4 NAME 'gitlab'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow gitlab access'
+             MUST (mail))
           ''
         ];
       };
@@ -193,11 +193,11 @@
         cn = "{1}ejabberd";
         objectClass = "olcSchemaConfig";
         olcAttributeTypes = [
-          ''            (1.2.752.43.9.1.1
-                      NAME 'jabberID'
-                      DESC 'The Jabber ID(s) associated with this object. Used to map a JID to an LDAP account.'
-                      EQUALITY caseIgnoreMatch
-                      SYNTAX 1.3.6.1.4.1.1466.115.121.1.15)
+          ''(1.2.752.43.9.1.1
+             NAME 'jabberID'
+             DESC 'The Jabber ID(s) associated with this object. Used to map a JID to an LDAP account.'
+             EQUALITY caseIgnoreMatch
+             SYNTAX 1.3.6.1.4.1.1466.115.121.1.15)
           ''
         ];
       };
@@ -205,11 +205,11 @@
         cn = "{2}ejabberd";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.2.752.43.9.2.1
-                      NAME 'jabberUser'
-                      DESC 'A jabber user'
-                      AUXILIARY
-                      MUST ( jabberID ))
+          ''(1.2.752.43.9.2.1
+             NAME 'jabberUser'
+             DESC 'A jabber user'
+             AUXILIARY
+             MUST ( jabberID ))
           ''
         ];
       };
@@ -217,30 +217,31 @@
         cn = "{1}homeAssistant";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.28297.1.2.4 NAME 'homeAssistant'
-                       SUP uidObject AUXILIARY
-                       DESC 'Added to an account to allow home-assistant access'
-                       MUST (mail) )
+          ''(1.3.6.1.4.1.28297.1.2.4 NAME 'homeAssistant'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow home-assistant access'
+             MUST (mail) )
           ''
         ];
       };
       "cn={1}ttrss,cn=schema".attrs = {
         cn = "{1}ttrss";
         objectClass = "olcSchemaConfig";
-        olcObjectClasses = ''          ( 1.3.6.1.4.1.28294.1.2.4 NAME 'ttrss'
-                    SUP top AUXILIARY
-                    DESC 'Added to an account to allow tinytinyrss access'
-                    MUST ( mail $ userPassword ))'';
+        olcObjectClasses = 
+        ''(1.3.6.1.4.1.28294.1.2.4 NAME 'ttrss'
+            SUP top AUXILIARY
+            DESC 'Added to an account to allow tinytinyrss access'
+            MUST ( mail $ userPassword ))'';
       };
       "cn={1}prometheus,cn=schema".attrs = {
         cn = "{1}prometheus";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            ( 1.3.6.1.4.1.28296.1.2.4
-                      NAME 'prometheus'
-                      SUP uidObject AUXILIARY
-                      DESC 'Added to an account to allow prometheus access'
-                      MUST (mail))
+          ''(1.3.6.1.4.1.28296.1.2.4
+             NAME 'prometheus'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow prometheus access'
+             MUST (mail))
           ''
         ];
       };
@@ -248,11 +249,11 @@
         cn = "{1}loki";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            ( 1.3.6.1.4.1.28299.1.2.4
-                      NAME 'loki'
-                      SUP uidObject AUXILIARY
-                      DESC 'Added to an account to allow loki access'
-                      MUST (mail))
+          ''(1.3.6.1.4.1.28299.1.2.4
+             NAME 'loki'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow loki access'
+             MUST (mail))
           ''
         ];
       };
@@ -261,10 +262,10 @@
         cn = "{1}flood";
         objectClass = "olcSchemaConfig";
         olcObjectClasses = [
-          ''            (1.3.6.1.4.1.28300.1.2.4 NAME 'flood'
-                         SUP uidObject AUXILIARY
-                         DESC 'Added to an account to allow flood access'
-                         MUST (mail))
+          ''(1.3.6.1.4.1.28300.1.2.4 NAME 'flood'
+             SUP uidObject AUXILIARY
+             DESC 'Added to an account to allow flood access'
+             MUST (mail))
           ''
         ];
       };
