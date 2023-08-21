937ad53ca8731dc4bf637a15810508c3a2fef185
Jörg Thalheim <joerg@thalheim.io>
2023-08-21 15:55:46 +0200

    home-manager/manual: improve eval time

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -8,73 +8,76 @@
     #./modules/emacs
   ];
 
-  config = {
-    home.packages = with pkgs;
-      [
-        #(pkgs.callPackage /home/joerg/git/nix-review {})
-        nixpkgs-review
-        nix-prefetch
+  home.packages = with pkgs;
+    [
+      #(pkgs.callPackage /home/joerg/git/nix-review {})
+      nixpkgs-review
+      nix-prefetch
 
-        hexyl
-        binutils
-        clang-tools
-        nixpkgs-fmt
-        shfmt
-        terraform-ls
-        ouch
+      hexyl
+      binutils
+      clang-tools
+      nixpkgs-fmt
+      shfmt
+      terraform-ls
+      ouch
 
-        python3.pkgs.black
-        pyright
-        nil
+      python3.pkgs.black
+      pyright
+      nil
 
-        du-dust
-        pkgs.nix
-        procs
+      du-dust
+      pkgs.nix
+      procs
 
-        socat
-        tmux
-        nurl
-        htop
-        hub
-        tea
-        gh
-        hyperfine
-        jq
-        yq-go
-        tig
-        lazygit
-        git-absorb
-        delta
-        scc
-        direnv
-        (nix-direnv.override { enableFlakes = true; })
-        fzf
-        lsd
-        zoxide
-        pinentry
-        fd
-        bat
-        vivid
-        ripgrep
-        zsh
-        less
-        bashInteractive
-        ncurses
-        coreutils
-        git
-      ]
-      ++ (lib.optionals pkgs.stdenv.isLinux [
-        strace
-        psmisc
-        glibcLocales
-        gdb
-      ]);
+      socat
+      tmux
+      nurl
+      htop
+      hub
+      tea
+      gh
+      hyperfine
+      jq
+      yq-go
+      tig
+      lazygit
+      git-absorb
+      delta
+      scc
+      direnv
+      (nix-direnv.override { enableFlakes = true; })
+      fzf
+      lsd
+      zoxide
+      pinentry
+      fd
+      bat
+      vivid
+      ripgrep
+      zsh
+      less
+      bashInteractive
+      ncurses
+      coreutils
+      git
+    ]
+    ++ (lib.optionals pkgs.stdenv.isLinux [
+      strace
+      psmisc
+      glibcLocales
+      gdb
+    ]);
 
-    home.enableNixpkgsReleaseCheck = false;
+  home.enableNixpkgsReleaseCheck = false;
 
-    home.stateVersion = "20.09";
-    home.username = lib.mkDefault "joerg";
-    home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
-    programs.home-manager.enable = true;
-  };
+  # better eval time
+  manual.html.enable = false;
+  manual.manpages.enable = false;
+  manual.json.enable = false;
+
+  home.stateVersion = "20.09";
+  home.username = lib.mkDefault "joerg";
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
+  programs.home-manager.enable = true;
 }
