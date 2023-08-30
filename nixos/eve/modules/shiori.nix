f6d5077ac18401e3b53f0075f919f66351482cab
Jörg Thalheim <joerg@thalheim.io>
2023-08-31 01:19:40 +0200

    shiori: make it connnect to postgresql

--- nixos/eve/modules/shiori.nix
+++ nixos/eve/modules/shiori.nix
@@ -1,9 +1,34 @@
-{
+{ lib, pkgs, ... }: {
   services.shiori.enable = true;
   services.shiori.port = 4378;
+  services.shiori.package = pkgs.shiori.overrideAttrs (_: {
+    patches = [
+      ./0001-drop-password.patch
+    ];
+  });
   systemd.services.shiori.environment = {
     SHIORI_PG_HOST = "/run/postgresql";
+    SHIORI_PG_PORT = "5432";
+    SHIORI_PG_USER = "shiori";
+    SHIORI_PG_NAME = "shiori";
+    SHIORI_DBMS = "postgresql";
   };
+  systemd.services.shiori = {
+    serviceConfig = {
+      DynamicUser = lib.mkForce false;
+      User = "shiori";
+      Group = "shiori";
+      RestrictAddressFamilies= [ "AF_UNIX" ];
+      BindPaths = [ "/run/postgresql" ];
+    };
+  };
+  users.users.shiori = {
+    isSystemUser = true;
+    home = "/var/lib/shiori";
+    group = "shiori";
+  };
+  users.groups.shiori = {};
+
   services.postgresql.ensureDatabases = [ "shiori" ];
   services.postgresql.ensureUsers = [{
     name = "shiori";
