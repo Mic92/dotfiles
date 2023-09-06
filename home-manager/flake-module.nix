d20589e99872e106e90153f60d66c6ed5c2aaaf5
Jörg Thalheim <joerg@thalheim.io>
2023-09-06 07:47:50 +0000

    rename dev2 -> bld2

--- home-manager/flake-module.nix
+++ home-manager/flake-module.nix
@@ -30,7 +30,7 @@
         program = "${pkgs.writeShellScriptBin "hm" ''
         set -x
         export PATH=${pkgs.lib.makeBinPath [pkgs.git pkgs.coreutils pkgs.nix pkgs.jq pkgs.unixtools.hostname]}
-        declare -A profiles=(["turingmachine"]="desktop" ["eddie"]="desktop" ["dev2"]="dev2" ["eve"]="eve" ["bernie"]="bernie" ["mac01.numtide.com"]="mac-hetzner")
+        declare -A profiles=(["turingmachine"]="desktop" ["eddie"]="desktop" ["bld2"]="bld2" ["eve"]="eve" ["bernie"]="bernie" ["mac01.numtide.com"]="mac-hetzner")
         profile="common"
         if [[ -n ''${profiles[$(hostname)]:-} ]]; then
           profile=''${profiles[$(hostname)]}
@@ -73,7 +73,7 @@
           desktop = homeManagerConfiguration {
             extraModules = [ ./desktop.nix ];
           };
-          dev2 = homeManagerConfiguration {
+          bld2 = homeManagerConfiguration {
             extraModules = [{ home.username = "mic92"; }];
           };
 
