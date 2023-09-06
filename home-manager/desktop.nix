bfb93f9bfdbd0507d617a03981a9e82a97f9e7a8
Jörg Thalheim <joerg@thalheim.io>
2023-09-06 09:56:29 +0200

    home-manager: switch to upstream mpris

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -15,14 +15,7 @@
 
   programs.vscode.enable = true;
 
-  systemd.user.services.mpris-proxy = {
-    Unit.Description = "Mpris proxy";
-    Unit.After = [ "network.target" "sound.target" ];
-    Install.WantedBy = [ "default.target" ];
-    Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-    Service.Restart = "always";
-  };
-
+  services.mpris-proxy.enable = true;
   services.syncthing.enable = true;
 
   home.packages = with pkgs; [
