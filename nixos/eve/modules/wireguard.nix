2eaff32ff4ca428cd0a36d56a5e7d9969373eef7
Jörg Thalheim <joerg@thalheim.io>
2023-09-16 15:27:15 +0200

    eve: update wireguard

--- nixos/eve/modules/wireguard.nix
+++ nixos/eve/modules/wireguard.nix
@@ -11,6 +11,9 @@ in
     ips = [ "fd98:c3d0:bec8:0::1/64" ];
 
     privateKeyFile = config.sops.secrets.eve-wireguard.path;
-    generatePrivateKeyFile = true;
+    peers = [{
+      allowedIPs = [ "fd98:c3d0:bec8:0::2/128" ];
+      publicKey = "RTOE/69lnlAFwIXiFJAn+Q90ON4TfFdu417yJcpCjCM=";
+    }];
   };
 }
