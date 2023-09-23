f72fe2194b3aa117393b6474f4ce41597828643e
Jörg Thalheim <joerg@thalheim.io>
2023-09-23 16:01:00 +0200

    mark cache.thalheim.io as trusted substituter as well

--- nixos/modules/nix-daemon.nix
+++ nixos/modules/nix-daemon.nix
@@ -25,6 +25,11 @@
         #"https://mic92.cachix.org"
         "https://cache.thalheim.io"
       ];
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        #"https://mic92.cachix.org"
+        "https://cache.thalheim.io"
+      ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         #"mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
