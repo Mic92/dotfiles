fd8a66f0693e2cea3b8de14bf503eeeb9351a70e
Jörg Thalheim <joerg@thalheim.io>
2023-11-05 08:12:02 +0100

    switch to greetd

--- nixos/modules/qtile.nix
+++ nixos/modules/qtile.nix
@@ -2,12 +2,6 @@
 , lib
 , ...
 }:
-let
-  pyEnv = pkgs.python3.withPackages (_p: [
-    pkgs.python3.pkgs.qtile
-    pkgs.python3.pkgs.iwlib
-  ]);
-in
 {
   xdg.portal.enable = true;
   xdg.portal.wlr.enable = true;
@@ -28,6 +22,20 @@ in
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     _JAVA_AWT_WM_NONREPARENTING = "1";
   };
+
+  services.greetd.enable = true;
+
+  # For greetd, we need a shell script into path, which lets us start qtile.service (after importing the environment of the login shell).
+  services.greetd.settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --cmd ${pkgs.writeScript "startqtile" ''
+    #! ${pkgs.bash}/bin/bash
+
+    # first import environment variables from the login manager
+    export XDG_DATA_DIRS=/run/current-system/sw/share/gsettings-schemas:$XDG_DATA_DIRS
+    systemctl --user unset-environment DISPLAY WAYLAND_DISPLAY
+    systemctl --user import-environment XDG_DATA_DIRS
+    # then start the service
+    exec systemctl --user --wait start qtile.service
+  ''}";
 
   # used in ping widget
   security.wrappers.fping = {
@@ -57,6 +65,7 @@ in
     blueberry
     grim # screenshots
     wtype
+    gsettings-desktop-schemas
 
     pavucontrol
     evince
@@ -74,36 +83,12 @@ in
 
     # screen brightness
     brightnessctl
-
-    # Here we but a shell script into path, which lets us start qtile.service (after importing the environment of the login shell).
-    (pkgs.writeTextFile {
-      name = "startqtile";
-      destination = "/bin/startqtile";
-      executable = true;
-      text =
-        let
-          schema = pkgs.gsettings-desktop-schemas;
-          datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-        in
-        ''
-          #! ${pkgs.bash}/bin/bash
-
-          ${pkgs.rbw}/bin/rbw unlock
-          if ssh-add -l | grep -q 'The agent has no identities.'; then
-            ${pkgs.openssh}/bin/ssh-add
-          fi
-          export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-          # first import environment variables from the login manager
-          systemctl --user unset-environment DISPLAY WAYLAND_DISPLAY
-          systemctl --user import-environment
-          # then start the service
-          exec systemctl --user start qtile.service
-        '';
-    })
   ];
 
-  # for polkit
-  environment.pathsToLink = [ "/libexec" ];
+  environment.pathsToLink = [
+    "/libexec" # for polkit
+    "/share/gsettings-schemas" # for XDG_DATA_DIRS
+  ];
 
   qt.platformTheme = "qt5ct";
 
@@ -118,22 +103,29 @@ in
     after = [ "graphical-session-pre.target" ];
   };
 
-  systemd.user.services.qtile = {
-    description = "Qtile - Wayland window manager";
-    documentation = [ "man:qtile(5)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-    # We explicitly unset PATH here, as we want it to be set by
-    # systemctl --user import-environment in startqtile
-    environment.PATH = lib.mkForce null;
-    environment.PYTHONPATH = lib.mkForce null;
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pyEnv}/bin/qtile start -b wayland";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
+  systemd.user.services.qtile =
+    let
+      pyEnv = pkgs.python3.withPackages (_p: [
+        pkgs.python3.pkgs.qtile
+        pkgs.python3.pkgs.iwlib
+      ]);
+    in
+    {
+      description = "Qtile - Wayland window manager";
+      documentation = [ "man:qtile(5)" ];
+      bindsTo = [ "graphical-session.target" ];
+      wants = [ "graphical-session-pre.target" ];
+      after = [ "graphical-session-pre.target" ];
+      # We explicitly unset PATH here, as we want it to be set by
+      # systemctl --user import-environment in startqtile
+      environment.PATH = lib.mkForce null;
+      environment.PYTHONPATH = lib.mkForce null;
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pyEnv}/bin/qtile start -b wayland";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
     };
-  };
 }
