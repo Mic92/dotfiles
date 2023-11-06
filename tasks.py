2561c70a4357d23e2bb898c41e31f5a8e050e3e1
Jörg Thalheim <joerg@thalheim.io>
2023-11-06 10:42:59 +0100

    update nixpkgs faster

--- tasks.py
+++ tasks.py
@@ -383,7 +383,10 @@ def add_github_user(c: Any, hosts: str = "", github_user: str = "Mic92") -> None
 
 @task
 def update_flakes(c: Any) -> None:
-    c.run("fast-flake-update --rev origin/main nixpkgs ~/git/nixpkgs", echo=True)
+    c.run(
+        "TMPDIR=/run/user/$(id -u)/ fast-flake-update --rev origin/main nixpkgs_2 ~/git/nixpkgs",
+        echo=True,
+    )
     c.run("nix flake update --commit-lock-file", echo=True)
 
 
