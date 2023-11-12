458820ee46c88aee54e08772098a6a058b5d9467
Jörg Thalheim <joerg@thalheim.io>
2023-11-12 09:32:37 +0100

    buildbot: also build i686-linux

--- nixos/eve/modules/buildbot.nix
+++ nixos/eve/modules/buildbot.nix
@@ -4,8 +4,9 @@
     domain = "buildbot.thalheim.io";
     workersFile = config.sops.secrets.buildbot-nix-workers.path;
     buildSystems = [
-      "aarch64-linux"
+      "i686-linux"
       "x86_64-linux"
+      "aarch64-linux"
       "aarch64-darwin"
     ];
     evalWorkerCount = 8;
