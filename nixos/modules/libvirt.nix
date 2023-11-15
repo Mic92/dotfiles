37bcf287182cc01d2bda4beacebd98804841fc12
Jörg Thalheim <joerg@thalheim.io>
2023-11-15 21:26:38 +0100

    libvirt: add virt-manager

--- nixos/modules/libvirt.nix
+++ nixos/modules/libvirt.nix
@@ -2,4 +2,5 @@
   virtualisation.libvirtd.enable = true;
   users.users.joerg.extraGroups = [ "libvirtd" ];
   networking.firewall.checkReversePath = false;
+  programs.virt-manager.enable = true;
 }
