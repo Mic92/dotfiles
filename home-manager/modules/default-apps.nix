921819db1cebb6bf3508c10ee430286612e7c43a
Jörg Thalheim <joerg@thalheim.io>
2023-11-18 13:50:18 +0100

    home-manager: cleanup mimetypes

--- home-manager/modules/default-apps.nix
+++ home-manager/modules/default-apps.nix
@@ -194,71 +194,6 @@
           "audio/m3u"
         ];
 
-        archives = [
-          "application/bzip2"
-          "application/gzip"
-          "application/vnd.android.package-archive"
-          "application/vnd.ms-cab-compressed"
-          "application/vnd.debian.binary-package"
-          "application/x-7z-compressed"
-          "application/x-7z-compressed-tar"
-          "application/x-ace"
-          "application/x-alz"
-          "application/x-ar"
-          "application/x-archive"
-          "application/x-arj"
-          "application/x-brotli"
-          "application/x-bzip-brotli-tar"
-          "application/x-bzip"
-          "application/x-bzip-compressed-tar"
-          "application/x-bzip1"
-          "application/x-bzip1-compressed-tar"
-          "application/x-cabinet"
-          "application/x-cd-image"
-          "application/x-compress"
-          "application/x-compressed-tar"
-          "application/x-cpio"
-          "application/x-chrome-extension"
-          "application/x-deb"
-          "application/x-ear"
-          "application/x-ms-dos-executable"
-          "application/x-gtar"
-          "application/x-gzip"
-          "application/x-gzpostscript"
-          "application/x-java-archive"
-          "application/x-lha"
-          "application/x-lhz"
-          "application/x-lrzip"
-          "application/x-lrzip-compressed-tar"
-          "application/x-lz4"
-          "application/x-lzip"
-          "application/x-lzip-compressed-tar"
-          "application/x-lzma"
-          "application/x-lzma-compressed-tar"
-          "application/x-lzop"
-          "application/x-lz4-compressed-tar"
-          "application/x-ms-wim"
-          "application/x-rar"
-          "application/x-rar-compressed"
-          "application/x-rpm"
-          "application/x-source-rpm"
-          "application/x-rzip"
-          "application/x-rzip-compressed-tar"
-          "application/x-tar"
-          "application/x-tarz"
-          "application/x-tzo"
-          "application/x-stuffit"
-          "application/x-war"
-          "application/x-xar"
-          "application/x-xz"
-          "application/x-xz-compressed-tar"
-          "application/x-zip"
-          "application/x-zip-compressed"
-          "application/x-zstd-compressed-tar"
-          "application/x-zoo"
-          "application/zip"
-          "application/zstd"
-        ];
         code = [
           "text/english"
           "text/plain"
@@ -277,12 +212,11 @@
           "text/x-c++"
         ];
       in
-      (lib.genAttrs code (_: [ "emacs.desktop" ]))
+      (lib.genAttrs code (_: [ "nvim.desktop" ]))
       // (lib.genAttrs images (_: [ "org.gnome.eog.desktop" ]))
       // (lib.genAttrs urls (_: [ "firefox.desktop" ]))
       // (lib.genAttrs documents (_: [ "org.gnome.Evince.desktop" ]))
       // (lib.genAttrs audioVideo (_: [ "mpv.desktop" ]))
-      // (lib.genAttrs archives (_: [ "org.gnome.FileRoller.desktop" ]))
-      // (lib.genAttrs archives (_: [ "org.gnome.Nautilus.desktop" ]));
+      // (lib.genAttrs [ "x-scheme-handler/sgnl" "x-scheme-handler/signalcaptcha" ] (_: [ "signal-desktop.desktop" ]));
   };
 }
