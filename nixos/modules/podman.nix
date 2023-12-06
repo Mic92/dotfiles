ff0f838134f3fca681aedea5eedbe91544cbc3e8
Jörg Thalheim <joerg@thalheim.io>
2023-12-06 22:01:30 +0100

    podman: drop custom network configuration

--- nixos/modules/podman.nix
+++ nixos/modules/podman.nix
@@ -1,5 +1,4 @@
-{ lib
-, pkgs
+{ pkgs
 , ...
 }: {
   virtualisation = {
@@ -20,53 +19,4 @@
     storage.runroot = "/run/containers/storage";
     storage.options.zfs.fsname = "zroot/podman";
   };
-
-  # dual-stack containers
-  environment.etc."cni/net.d/87-podman-bridge.conflist".source = lib.mkForce (builtins.toFile "87-podman-bridge.conflist" (builtins.toJSON {
-    cniVersion = "0.4.0";
-    name = "podman";
-    plugins = [
-      {
-        type = "bridge";
-        bridge = "cni-podman0";
-        isGateway = true;
-        ipMasq = true;
-        hairpinMode = true;
-        ipam = {
-          type = "host-local";
-          routes = [
-            { dst = "0.0.0.0/0"; }
-            { dst = "::/0"; }
-          ];
-          ranges = [
-            [
-              {
-                subnet = "10.88.0.0/16";
-                gateway = "10.88.0.1";
-              }
-            ]
-            [
-              {
-                subnet = "fd42:4492:6a6d:0041::/64";
-                rangeStart = "fd42:4492:6a6d:0041::0";
-                rangeEnd = "fd42:4492:6a6d:0041::ffff";
-              }
-            ]
-          ];
-        };
-      }
-      {
-        type = "portmap";
-        capabilities = {
-          portMappings = true;
-        };
-      }
-      {
-        type = "firewall";
-      }
-      {
-        type = "tuning";
-      }
-    ];
-  }));
 }
