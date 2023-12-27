195b9a8f45a1bd170fecaa73f3a78e25a6ad1a89
Jörg Thalheim <joerg@thalheim.io>
2023-12-27 14:56:29 +0100

    go back to main branc

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    buildbot-nix.url = "github:Mic92/buildbot-nix/fixes";
+    buildbot-nix.url = "github:Mic92/buildbot-nix";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
