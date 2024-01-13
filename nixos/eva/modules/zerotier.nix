8b38476a5192455e5b5fe74a8a6be91457f4aa87
Jörg Thalheim <joerg@thalheim.io>
2024-01-13 10:17:32 +0100

    add zerotier moons

--- nixos/eva/modules/zerotier.nix
+++ nixos/eva/modules/zerotier.nix
@@ -2,10 +2,8 @@
   imports = [
     ../../modules/zerotier.nix
   ];
-
-  clan.networking.zerotier.controller.enable = true;
   clan.networking.zerotier.moon.stableEndpoints = [
-    "95.217.199.121"
-    "2a01:4f9:4a:42e8::1"
+    "89.58.27.144"
+    "2a03:4000:62:fdb::"
   ];
 }
