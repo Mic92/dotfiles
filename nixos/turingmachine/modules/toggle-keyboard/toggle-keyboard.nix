c33e67c4fa152a9026873a287eb45ae6873259e2
Jörg Thalheim <joerg@thalheim.io>
2024-01-15 15:50:38 +0100

    toggle-keyboard: fix setuid security issues

--- nixos/turingmachine/modules/toggle-keyboard/toggle-keyboard.nix
+++ nixos/turingmachine/modules/toggle-keyboard/toggle-keyboard.nix
@@ -1,9 +1,12 @@
-{ stdenv }:
+{ stdenv, kmod }:
 stdenv.mkDerivation {
   name = "toggle-keyboard";
   src = ./.;
   buildPhase = ''
     mkdir -p $out/bin
+    substituteInPlace toggle-keyboard.c \
+      --replace @modprobe@ ${kmod}/bin/modprobe \
+      --replace @rmmod@ ${kmod}/bin/rmmod
     $CC $CFLAGS $LDFLAGS -o $out/bin/toggle-keyboard toggle-keyboard.c
   '';
 }
