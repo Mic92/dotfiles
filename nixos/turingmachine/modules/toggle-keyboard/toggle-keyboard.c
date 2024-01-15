711586ece6f6c324fdce5be40ccead1c9352ba44
Jörg Thalheim <joerg@thalheim.io>
2024-01-15 15:50:38 +0100

    toggle-keyboard: fix setuid security issues

--- nixos/turingmachine/modules/toggle-keyboard/toggle-keyboard.c
+++ nixos/turingmachine/modules/toggle-keyboard/toggle-keyboard.c
@@ -4,15 +4,15 @@
 
 int main() {
   struct stat st = {0};
-  if (stat("/sys/module/i8042", &st) != 0) {
+  if (stat("/sys/module/i8042", &st) == 0) {
     printf("Disabling keyboard\n");
-    execlp("rmmod", "rmmod", "i8042", NULL);
+    execl("@rmmod@", "rmmod", "i8042", NULL);
     perror("fail to rmmod");
     return 1;
   }
 
   printf("Enabling keyboard\n");
-  execlp("modprobe", "modprobe", "i8042", NULL);
+  execl("@modprobe@", "modprobe", "i8042", NULL);
   perror("fail to modprobe");
   return 1;
 }
