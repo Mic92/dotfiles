d4db13c1832a40d6f9285a649658bb1257d199a1
Jörg Thalheim <joerg@thalheim.io>
2024-01-17 23:11:05 +0100

    zerotier: fix interface blacklist

--- nixos/modules/zerotier.nix
+++ nixos/modules/zerotier.nix
@@ -3,11 +3,8 @@
   clan.networking.zerotier.networkId = lib.mkDefault (builtins.readFile (config.clanCore.clanDir + "/machines/eve/facts/zerotier-network-id"));
   services.zerotierone.joinNetworks = [ "7c31a21e86f9a75c" ];
 
-  services.zerotierone.localConf.settings.physical = {
-    "10.243.0.0/16".blacklist = true;
-    "10.244.0.0/16".blacklist = true;
-    "10.250.0.0/16".blacklist = true;
-    "42::/16".blacklist = true;
+  services.zerotierone.localConf.settings = {
+    interfacePrefixBlacklist = [ "tinc" "wiregrill" ];
   };
 
   clan.networking.zerotier.moon.orbitMoons = [ "267efd4a15" "a03b47494d" ];
