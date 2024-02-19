c38ec1713e49160546536362ae5c02105ecd0515
Jörg Thalheim <joerg@thalheim.io>
2024-02-19 15:40:11 +0100

    switch to better solarized plugin

--- home-manager/modules/neovim/user/init.lua
+++ home-manager/modules/neovim/user/init.lua
@@ -10,39 +10,39 @@ vim.api.nvim_create_user_command("RaiseTmuxPane", function()
 end, {})
 
 vim.api.nvim_create_user_command("Nurl", function()
-  local url = vim.fn.input("Enter a URL: ")
-  local rev = vim.fn.input("Enter the revision (e.g., v0.2.0 or empty string): ")
+  local url = vim.fn.input "Enter a URL: "
+  local rev = vim.fn.input "Enter the revision (e.g., v0.2.0 or empty string): "
 
-  local cmd = string.format('nurl %s %s 2>/dev/null', url, rev)
+  local cmd = string.format("nurl %s %s 2>/dev/null", url, rev)
 
   local output = vim.fn.systemlist(cmd)
   if vim.v.shell_error == 0 and #output > 0 then
     vim.api.nvim_put(output, "l", true, true)
   else
-    print("Error executing command or command returned empty result.")
+    print "Error executing command or command returned empty result."
   end
 end, {})
 
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin",     -- remote to use
-    channel = "stable",    -- "stable" or "nightly"
-    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly",    -- branch name (NIGHTLY ONLY)
-    commit = nil,          -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,  -- skip prompts about breaking changes
+    remote = "origin", -- remote to use
+    channel = "stable", -- "stable" or "nightly"
+    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly", -- branch name (NIGHTLY ONLY)
+    commit = nil, -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false,     -- automatically quit the current session after a successful update
-    remotes = {            -- easily add new remotes to track
+    auto_quit = false, -- automatically quit the current session after a successful update
+    remotes = { -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
   -- Set colorscheme to use
-  colorscheme = "neosolarized",
+  colorscheme = "NeoSolarized",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
@@ -62,7 +62,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           "go",
         },
@@ -102,7 +102,7 @@ return {
   lazy = {
     defaults = { lazy = true },
     -- Disable change detection because it caused nvim to constantly need 3-5% CPU, which is bad for battery life
-    change_detection = { enabled = false, },
+    change_detection = { enabled = false },
     performance = {
       rtp = {
         -- customize default disabled vim plugins
