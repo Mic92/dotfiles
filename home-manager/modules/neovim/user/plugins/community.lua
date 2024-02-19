b6d80e127ffb2dae2deb8ec54ac2d1057c1e69e2
Jörg Thalheim <joerg@thalheim.io>
2024-02-19 15:25:28 +0100

    switch to neosolarized theme

--- home-manager/modules/neovim/user/plugins/community.lua
+++ home-manager/modules/neovim/user/plugins/community.lua
@@ -3,7 +3,7 @@ return {
  "AstroNvim/astrocommunity",
  -- example of imporing a plugin, comment out to use it or add your own
  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
- { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+ { import = "astrocommunity.colorscheme.neosolarized-nvim" },
  { import = "astrocommunity.completion.copilot-lua" },
  { import = "astrocommunity.completion.copilot-lua-cmp" },
  {
