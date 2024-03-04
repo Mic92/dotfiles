5cc7ff4416e37838ad9a609aa6901d7b83745aa5
Jörg Thalheim <joerg@thalheim.io>
2024-03-04 12:50:14 +0100

    neovim: commit wip copilotchat

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -7,7 +7,7 @@ let
   inherit (self.packages.${pkgs.hostPlatform.system}) astro-nvim-config nvim-open;
 in
 {
-  home.packages = [ pkgs.neovim nvim-open ] ++ astro-nvim-config.lspPackages;
+  home.packages = [ astro-nvim-config.neovim nvim-open ] ++ astro-nvim-config.lspPackages;
   xdg.dataHome = "${config.home.homeDirectory}/.data";
   xdg.dataFile."nvim/lazy/telescope-fzf-native.nvim/build/libfzf.so".source = "${pkgs.vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so";
   xdg.configFile."nvim".source = astro-nvim-config;
