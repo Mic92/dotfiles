ec025cfee12116b51a9f0422b7018364025a9869
Jörg Thalheim <joerg@thalheim.io>
2024-03-04 19:19:13 +0100

    fix eval for zfs

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -2,14 +2,8 @@
 # Other useful settings come from srvos's zfs module
 {
   config = lib.mkIf config.boot.zfs.enabled {
-    environment.systemPackages = [
-      pkgs.zfs-prune-snapshots
-    ];
-
-    boot.kernelPackages = lib.mkDefault (pkgs.zfsUnstable.override {
-      removeLinuxDRM = pkgs.hostPlatform.isAarch64;
-    }).latestCompatibleLinuxPackages;
-    boot.zfs.removeLinuxDRM = lib.mkDefault pkgs.hostPlatform.isAarch64;
-    boot.zfs.enableUnstable = true;
+    environment.systemPackages = [ pkgs.zfs-prune-snapshots ];
+    boot.kernelPackages = lib.mkDefault pkgs.zfsUnstable.latestCompatibleLinuxPackages;
+    boot.zfs.package = pkgs.zfsUnstable;
   };
 }
