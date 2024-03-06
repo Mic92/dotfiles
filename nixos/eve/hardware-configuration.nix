a6549b5559f4985ec267134e80d57ab278de9296
Jörg Thalheim <joerg@thalheim.io>
2024-03-06 10:20:47 +0100

    fix eval errors

--- nixos/eve/hardware-configuration.nix
+++ nixos/eve/hardware-configuration.nix
@@ -6,17 +6,12 @@
     "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
-  boot = {
-    zfs.enableUnstable = true;
-    zfs.requestEncryptionCredentials = true;
-
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "sd_mod"
-      "nvme"
-    ];
-  };
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "sd_mod"
+    "nvme"
+  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
