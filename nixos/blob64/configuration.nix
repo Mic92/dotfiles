8865dc1cce246a30ca6d6a47ca774ac807d75821
Jörg Thalheim <joerg@thalheim.io>
2024-03-13 07:44:55 +0100

    blob64: use normal packages.nix list for systemPackages

--- nixos/blob64/configuration.nix
+++ nixos/blob64/configuration.nix
@@ -6,9 +6,12 @@
     ../modules/minimal-docs.nix
     ../modules/networkd.nix
     ../modules/ip-update.nix
+    ../modules/packages.nix
     ./modules/photoprism.nix
     ./hardware-configuration.nix
   ];
+
+  networking.hostName = "blob64";
 
   clan.networking.buildHost = "root@eve.i";
 
@@ -30,22 +33,6 @@
     };
 
   services.openssh.enable = true;
-
-  networking.hostName = "blob64";
-
-  environment.systemPackages = with pkgs; [
-    vim
-    ethtool
-    iperf
-    parted
-    fio
-    wget
-    htop
-    tcpdump
-    nmap
-    python3
-    tmux
-  ];
 
   systemd.network.networks."10-uplink" = {
     matchConfig.Type = "ether";
