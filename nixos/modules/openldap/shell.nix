8e307fe69861dd9b43f1da6f859860c1c2a08631
Jörg Thalheim <joerg@thalheim.io>
2024-03-14 11:41:35 +0100

    bump openldap shell

--- nixos/modules/openldap/shell.nix
+++ nixos/modules/openldap/shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") { } }:
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") { } }:
 #{ pkgs ? import <nixpkgs> { } }:
 pkgs.stdenvNoCC.mkDerivation {
   name = "env";
