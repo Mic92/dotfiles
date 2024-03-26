22e768ab41c024f9dddaec3b62da019ca701a9f6
Jörg Thalheim <joerg@thalheim.io>
2024-03-26 12:57:58 +0100

    ruff switch to check subcommand

--- devshell/flake-module.nix
+++ devshell/flake-module.nix
@@ -72,7 +72,7 @@
             options = [
               "-eucx"
               ''
-                ${pkgs.ruff}/bin/ruff --fix "$@"
+                ${pkgs.ruff}/bin/ruff check --fix "$@"
                 ${pkgs.ruff}/bin/ruff format "$@"
               ''
               "--" # this argument is ignored by bash
