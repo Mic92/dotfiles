d8d106421de08ea4d289cdb64f9a864daeaa1d4c
Jörg Thalheim <joerg@thalheim.io>
2024-03-26 15:01:51 +0100

    re-fine localbackup

--- nixos/turingmachine/modules/localbackup.nix
+++ nixos/turingmachine/modules/localbackup.nix
@@ -1,17 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 {
-  environment.systemPackages = [
-    pkgs.bcachefs-tools
-  ];
   clan.localbackup.targets.hdd = {
     directory = "/backup-2tb/turingmachine";
-    mountpoint = "/backup-2tb/turingmachine";
-    mountHook = ''
+    mountpoint = "/backup-2tb";
+    preMountHook = ''
       set -x
-      bcache unlock /dev/disk/by-partuuid/b315b307-bc54-4918-8ac4-5dd99c68fa70
+      export PATH=${lib.makeBinPath [pkgs.bcachefs-tools pkgs.keyutils pkgs.util-linux]}
+      keyctl link @u @s
+      ${pkgs.bcachefs-tools}/bin/bcachefs unlock /dev/disk/by-partuuid/b315b307-bc54-4918-8ac4-5dd99c68fa70 < ${config.sops.secrets.turingmachine-bcachefs-password.path}
     '';
   };
-  fileSystems."/backup-2tb/turingmachine" = {
+  fileSystems."/backup-2tb" = {
     device = "/dev/disk/by-partuuid/b315b307-bc54-4918-8ac4-5dd99c68fa70";
     fsType = "bcachefs";
     options = [ "defaults" "noauto" ];
