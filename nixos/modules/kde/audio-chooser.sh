2a35f93ff4cc37d9f535e5a589d2cc17b303aafd
Jörg Thalheim <joerg@thalheim.io>
2024-04-10 10:19:50 +0200

    audio-chooser: add soundbar

--- nixos/modules/kde/audio-chooser.sh
+++ nixos/modules/kde/audio-chooser.sh
@@ -3,7 +3,7 @@
 #shellcheck shell=bash
 set -exuo pipefail
 
-choices=("headphones" "speakers" "headphones (output-only)" "earphones")
+choices=("headphones" "speakers" "headphones (output-only)" "earphones" "soundbar")
 selected=$(printf "\n%s" "${choices[@]}" | fuzzel -d)
 
 echo You Picked: " $selected"
@@ -12,6 +12,7 @@ headset=alsa_card.usb-0b0e_Jabra_Link_380_08C8C2E6DA47-00
 speakers=alsa_card.pci-0000_00_1f.3
 earphones=bluez_output.50_C2_75_67_67_8C.1
 address="50:C2:75:67:67:8C"
+soundbar=alsa_card.usb-DELL_DELL_Slim_Soundbar_SB522A_0-02
 
 notify() {
   notify-send -a audio-chooser "Audio Chooser" "$1"
@@ -23,6 +24,7 @@ headphones)
   pactl set-card-profile "$headset" output:analog-stereo+input:mono-fallback || true
   pactl set-card-profile "$speakers" off || true
   pactl set-card-profile "$earphones" off || true
+  pactl set-card-profile "$soundbar" off || true
   notify "Headphones Connected"
   ;;
 speakers)
@@ -30,6 +32,7 @@ speakers)
   pactl set-card-profile "$speakers" output:analog-stereo+input:analog-stereo || true
   pactl set-card-profile "$headset" off || true
   pactl set-card-profile "$earphones" off || true
+  pactl set-card-profile "$soundbar" off || true
   notify "Speakers Connected"
   ;;
 "headphones (output-only)")
@@ -37,6 +40,7 @@ speakers)
   pactl set-card-profile "$headset" output:analog-stereo || true
   pactl set-card-profile "$speakers" off || true
   pactl set-card-profile "$earphones" off || true
+  pactl set-card-profile "$soundbar" off || true
   notify "Headphones (Output-Only) Connected"
   ;;
 "earphones")
@@ -44,7 +48,16 @@ speakers)
   pactl set-card-profile "$earphones" a2dp-sink || true
   pactl set-card-profile "$headset" off || true
   pactl set-card-profile "$speakers" off || true
+  pactl set-card-profile "$soundbar" off || true
   notify "Earphones Connected"
+  ;;
+"soundbar")
+  bluetoothctl disconnect "$address" || true
+  pactl set-card-profile "$soundbar" output:analog-stereo || true
+  pactl set-card-profile "$headset" off || true
+  pactl set-card-profile "$speakers" off || true
+  pactl set-card-profile "$earphones" off || true
+  notify "Soundbar Connected"
   ;;
 *)
   echo "Invalid Option $selected"
