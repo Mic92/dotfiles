4f6fec3832ecc9ba25717a37db8398c63410d9c3
Jörg Thalheim <joerg@thalheim.io>
2024-04-21 08:36:07 +0200

    fixup sunshine configuration

---
+++ nixos/turingmachine/modules/sunshine.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.sunshine
+    pkgs.moonlight-qt # for testing purposes.
+  ];
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 47998;
+        to = 48000;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
+    ];
+  };
+  # Prevents this error:
+  # Fatal: You must run [sudo setcap cap_sys_admin+p $(readlink -f sunshine)] for KMS display capture to work!
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+  # Needed for network discovery
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+}
