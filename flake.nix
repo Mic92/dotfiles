c984ac89f02887e7dbba7725e6cef8c8899cd138
Jörg Thalheim <joerg@thalheim.io>
2024-05-02 05:49:05 +0200

    update buildbot

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    buildbot-nix.url = "github:MagicRB/buildbot-nix/remove_github_assumption";
+    buildbot-nix.url = "github:Mic92/buildbot-nix/gitea-fixes";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
