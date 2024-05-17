9a21adceb12f8f9535efa9b13ba8125a72ccafa3
Jörg Thalheim <joerg@thalheim.io>
2024-05-17 13:29:03 +0200

    drop nvim-open

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -1,13 +1,10 @@
 { pkgs, self, ... }:
 let
-  inherit (self.packages.${pkgs.hostPlatform.system}) neovim nvim-open treesitter-grammars;
+  inherit (self.packages.${pkgs.hostPlatform.system}) neovim treesitter-grammars;
   inherit (self.legacyPackages.${pkgs.hostPlatform.system}) nvim-lsp-packages;
 in
 {
-  home.packages = nvim-lsp-packages ++ [
-    neovim
-    nvim-open
-  ];
+  home.packages = nvim-lsp-packages ++ [ neovim ];
   # treesitter-grammars
   xdg.dataFile."nvim/site/parser".source = treesitter-grammars;
   xdg.dataFile."nvim/lib/libfzf.so".source = "${pkgs.vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so";
