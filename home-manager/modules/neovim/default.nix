470361f0a054ef68a3239b6d5bfc0648892613dd
Jörg Thalheim <joerg@thalheim.io>
2024-07-06 10:10:15 +0200

    update lazy-lock.json when out of sync with tree-sitter

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -15,6 +15,13 @@ in
 
   home.activation.nvim = ''
     echo "${treesitter-grammars.rev}" > "${config.xdg.configHome}/nvim/treesitter-rev"
+    XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
+    NVIM_APPNAME=''${NVIM_APPNAME:-nvim}
+    if [[ -f $XDG_CONFIG_HOME/$NVIM_APPNAME/lazy-lock.json ]]; then
+      if ! grep -q "${treesitter-grammars.rev}" "$XDG_CONFIG_HOME/$NVIM_APPNAME/lazy-lock.json"; then
+        nvim --headless "+Lazy! update" +qa
+      fi
+    fi
   '';
 
   xdg.dataFile."nvim/lib/libfzf.so".source = "${pkgs.vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so";
--- home-manager/modules/neovim/nvim-standalone.nix
+++ home-manager/modules/neovim/nvim-standalone.nix
@@ -16,7 +16,7 @@ in
 writeShellScriptBin "nvim" ''
   set -efux
   unset VIMINIT
-  export PATH=${lspEnv}/bin:$PATH
+  export PATH=${lspEnv}/bin:${neovim}/bin:$PATH
   export NVIM_APPNAME=${nvim-appname}
 
   XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
@@ -27,11 +27,18 @@ writeShellScriptBin "nvim" ''
   rm -rf "$XDG_CONFIG_HOME/$NVIM_APPNAME"
   cp -arfT '${../../../home/.config/nvim}'/ "$XDG_CONFIG_HOME/$NVIM_APPNAME"
   chmod -R u+w "$XDG_CONFIG_HOME/$NVIM_APPNAME"
-  ${neovim}/bin/nvim --headless -c 'quitall' # install plugins
+  echo "${treesitter-grammars.rev}" > "$XDG_CONFIG_HOME/$NVIM_APPNAME/treesitter-rev"
+
+  # lock file is not in sync with treesitter-rev, force update of lazy-lock.json
+  if ! grep -q "${treesitter-grammars.rev}" "$XDG_CONFIG_HOME/$NVIM_APPNAME/lazy-lock.json"; then
+    # annoyingly we would run this on every nvim invocation again because we overwrite the lock file
+    nvim --headless "+Lazy! update" +qa
+  else
+    nvim --headless -c 'quitall' # install plugins, if needed
+  fi
   mkdir -p "$XDG_DATA_HOME/$NVIM_APPNAME/lib/" "$XDG_DATA_HOME/$NVIM_APPNAME/site/"
   ln -sfT "${vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so" "$XDG_DATA_HOME/$NVIM_APPNAME/lib/libfzf.so"
   ln -sfT "${treesitter-grammars}" "$XDG_DATA_HOME/$NVIM_APPNAME/site/parser"
-  echo "${treesitter-grammars.rev}" > "$XDG_CONFIG_HOME/$NVIM_APPNAME/treesitter-rev"
 
-  exec ${neovim}/bin/nvim "$@"
+  exec nvim "$@"
 ''
