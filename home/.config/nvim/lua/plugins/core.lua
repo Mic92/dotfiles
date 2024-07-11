44679c67c4c50b1b3a657a2455d5c49d750086b0
Jörg Thalheim <joerg@thalheim.io>
2024-07-11 13:18:21 +0200

    fix format

--- home/.config/nvim/lua/plugins/core.lua
+++ home/.config/nvim/lua/plugins/core.lua
@@ -1,6 +1,6 @@
 return {
 	"AstroNvim/astrocore",
-	lazy = false,    -- disable lazy loading
+	lazy = false, -- disable lazy loading
 	priority = 10000, -- load AstroCore first
 	opts = {
 		options = {
