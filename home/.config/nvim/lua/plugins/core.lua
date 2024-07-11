21c47acf47bf9dad34b113bc01a2a76aac8a4246
Jörg Thalheim <joerg@thalheim.io>
2024-07-11 13:13:29 +0200

    nvim: absolute line numbers

--- home/.config/nvim/lua/plugins/core.lua
+++ home/.config/nvim/lua/plugins/core.lua
@@ -1,11 +1,13 @@
 return {
 	"AstroNvim/astrocore",
-	lazy = false, -- disable lazy loading
+	lazy = false,    -- disable lazy loading
 	priority = 10000, -- load AstroCore first
-	options = {
-		opt = {
-			-- set configuration options  as described below
-			relativenumber = false, -- sets vim.opt.relativenumber
+	opts = {
+		options = {
+			opt = {
+				-- set configuration options  as described below
+				relativenumber = false, -- sets vim.opt.relativenumber
+			},
 		},
 	},
 }
