6f8e024975daed735c12645eee60062a8b4566d3
Jörg Thalheim <joerg@thalheim.io>
2024-07-20 22:31:34 +0200

    test buildbot-effetcts

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    buildbot-nix.url = "github:nix-community/buildbot-nix";
+    buildbot-nix.url = "github:nix-community/buildbot-nix/hercules";
     #buildbot-nix.url = "github:MagicRB/buildbot-nix/per-installation-tokens-in-github";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
