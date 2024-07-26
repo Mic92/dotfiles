1a23bbfa27f55a10cdca6d6243efd357ae7845ea
Jörg Thalheim <joerg@thalheim.io>
2024-07-26 08:05:18 +0200

    test out different buildbot branch

--- flake.nix
+++ flake.nix
@@ -50,8 +50,8 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    buildbot-nix.url = "github:nix-community/buildbot-nix/hercules";
-    #buildbot-nix.url = "github:MagicRB/buildbot-nix/pydantic-convert";
+    #buildbot-nix.url = "github:nix-community/buildbot-nix/hercules";
+    buildbot-nix.url = "github:MagicRB/buildbot-nix/pydantic-convert";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
