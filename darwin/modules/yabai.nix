62c71f545ee8417ad8368b754d41bf47903d3ae7
Jörg Thalheim <joerg@thalheim.io>
2024-08-12 13:49:35 +0200

    yabai: also configure skhd

--- darwin/modules/yabai.nix
+++ darwin/modules/yabai.nix
@@ -1,1 +1,10 @@
-{ services.yabai.enable = true; }
+{
+  services.yabai.enable = true;
+  services.skhd.enable = true;
+  services.skhd.skhdConfig = ''
+    # focus next window in stack
+    alt - j : yabai -m window --focus stack.next
+    # focus previous window in stack
+    alt - k : yabai -m window --focus stack.prev
+  '';
+}
