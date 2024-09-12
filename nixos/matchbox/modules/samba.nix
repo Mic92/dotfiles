64e72e98e593b8087c707e99fc6971ff5c8ec569
Jörg Thalheim <joerg@thalheim.io>
2024-09-12 16:48:08 +0200

    update samba config

--- nixos/matchbox/modules/samba.nix
+++ nixos/matchbox/modules/samba.nix
@@ -3,30 +3,29 @@
   services.samba = {
     enable = true;
     package = pkgs.samba;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      netbios name = MATCHBOX
-      server string = Raspberry Pi
-      hosts allow = 192.168.178.0/255.255.255.0 42:0:3c46:70c7:8526:2adf:7451:8bbb/128
-      interfaces = tinc.retiolum eth* end*
-      map to guest = Bad User
-      max log size = 50
-      dns proxy = no
-      security = user
-
-      [global]
-        syslog only = yes
-    '';
-    shares.public = {
-      comment = "Netzlaufwerk";
-      path = "/mnt/hdd/public";
-      public = "yes";
-      "only guest" = "yes";
-      "create mask" = "0644";
-      "directory mask" = "2777";
-      writable = "yes";
-      browseable = "yes";
-      printable = "no";
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "server string" = "Raspberry Pi";
+        "hosts allow" = "192.168.178.0/255.255.255.0 42:0:3c46:70c7:8526:2adf:7451:8bbb/128";
+        interfaces = "tinc.retiolum eth* end*";
+        "map to guest" = "Bad User";
+        "max log size" = "50";
+        "dns proxy" = false;
+        security = "user";
+        "syslog only" = true;
+      };
+      public = {
+        comment = "Netzlaufwerk";
+        path = "/mnt/hdd/public";
+        public = "yes";
+        "only guest" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "2777";
+        writable = "yes";
+        browseable = "yes";
+        printable = "no";
+      };
     };
   };
 
