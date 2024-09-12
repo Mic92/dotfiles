ac615892ecbb556eb384731488f65d00b7b3f386
Jörg Thalheim <joerg@thalheim.io>
2024-09-12 16:29:30 +0200

    test next buildbot pull request

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    buildbot-nix.url = "github:MagicRB/buildbot-nix/cache_failed_builds";
+    buildbot-nix.url = "github:MagicRB/buildbot-nix/different-combined-reports";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
