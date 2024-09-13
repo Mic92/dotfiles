a96ff5b213f12541405866462afc32739302f16c
Jörg Thalheim <joerg@thalheim.io>
2024-09-13 17:29:31 +0200

    update nixos facter modules

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
 
-    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+    nixos-facter-modules.url = "github:numtide/nixos-facter-modules/graphics";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
