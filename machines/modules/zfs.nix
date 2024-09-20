1a039736e1070e0a3cbc97614211e11a648d32a5
Jörg Thalheim <joerg@thalheim.io>
2024-09-20 19:00:35 +0200

    switch to srvos's latest-zfs-kernel module

--- machines/modules/zfs.nix
+++ machines/modules/zfs.nix
@@ -2,13 +2,14 @@
   config,
   pkgs,
   lib,
+  inputs,
   ...
 }:
 # Other useful settings come from srvos's zfs module
 {
+  imports = [ inputs.srvos.nixosModules.mixins-latest-zfs-kernel ];
   config = lib.mkIf config.boot.zfs.enabled {
     environment.systemPackages = [ pkgs.zfs-prune-snapshots ];
-    boot.kernelPackages = pkgs.zfsUnstable.latestCompatibleLinuxPackages;
     boot.zfs.package = pkgs.zfsUnstable;
   };
 }
