fdbeaec8fba9684ed533e12d01b3a0025e180aba
Jörg Thalheim <joerg@thalheim.io>
2024-09-23 15:51:20 +0200

    fix update-prefetch

--- machines/modules/update-prefetch.nix
+++ machines/modules/update-prefetch.nix
@@ -11,25 +11,25 @@
     script = ''
       set -eux -o pipefail
       export PATH=${
-        lib.makeBinPath ([
+        lib.makeBinPath [
           config.nix.package
           pkgs.curl
           pkgs.iproute2
           pkgs.nettools
           pkgs.git
           pkgs.openssh
-        ])
+          pkgs.util-linux
+        ]
       }
       # skip service if do not have a default route
       if ! ip r g 8.8.8.8; then
         exit
       fi
-      HOST=$(</proc/sys/kernel/hostname)
-      store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/Mic92/dotfiles/main/${pkgs.hostPlatform.system}.nixos-$HOST)"
+      store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/Mic92/dotfiles/main/${pkgs.hostPlatform.system}.nixos-${config.networking.hostName})"
       nix-store --add-root /run/next-system -r "$store_path"
 
       if [[ -f /home/joerg/.homesick/repos/dotfiles/flake.nix ]]; then
-        profile=$(nix run "/home/joerg/.homesick/repos/dotfiles/#hm" -- profile)
+        profile=$(runuser -u joerg -- nix run "/home/joerg/.homesick/repos/dotfiles/#hm" -- profile)
         store_path="$(curl -L https://buildbot.thalheim.io/nix-outputs/Mic92/dotfiles/main/${pkgs.hostPlatform.system}.home-manager-$profile)"
         nix-store --add-root /run/next-home -r "$store_path"
       fi
