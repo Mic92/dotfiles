b1ae8f80aa89251721ac800fe8689a38744a0cc1
DavHau <hsngrmpf+github@gmail.com>
2024-09-24 13:35:26 +0200

    dave: add host to monitoring

--- machines/eva/modules/telegraf/dave.nix
+++ machines/eva/modules/telegraf/dave.nix
@@ -1,13 +1,19 @@
 {
   services.telegraf.extraConfig.inputs = {
-    net_response = map (host: {
-      protocol = "tcp";
-      address = "${host}:22";
-      send = "SSH-2.0-Telegraf";
-      expect = "SSH-2.0";
-      tags.host = host;
-      tags.org = "dave";
-      timeout = "10s";
-    }) [ "[fc9f:379f:7191:058f:82a1:0000:0000:0001]" ];
+    net_response =
+      map
+        (host: {
+          protocol = "tcp";
+          address = "${host}:22";
+          send = "SSH-2.0-Telegraf";
+          expect = "SSH-2.0";
+          tags.host = host;
+          tags.org = "dave";
+          timeout = "10s";
+        })
+        [
+          "[fc9f:379f:7191:058f:82a1:0000:0000:0001]"
+          "[fc9f:379f:71f5:8aeb:6e2b:0000:0000:0001]"
+        ];
   };
 }
