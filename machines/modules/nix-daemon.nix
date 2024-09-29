1ed6ba101f40ad80ea3b8018ee6042218160e86e
Jörg Thalheim <joerg@thalheim.io>
2024-09-29 15:26:19 +0200

    add systemd timer to cleanup gcroots

--- machines/modules/nix-daemon.nix
+++ machines/modules/nix-daemon.nix
@@ -51,5 +51,26 @@
 
   imports = [ ./builder.nix ];
 
+  systemd.timers.nix-cleanup-gcroots = {
+    timerConfig = {
+      OnCalendar = [ "weekly" ];
+      Persistent = true;
+    };
+    wantedBy = [ "timers.target" ];
+  };
+  systemd.services.nix-cleanup-gcroots = {
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        # delete automatic gcroots older than 30 days
+        "${pkgs.findutils}/bin/find /nix/var/nix/gcroots/auto /nix/var/nix/gcroots/per-user -type l -mtime +30 -delete"
+        # created by nix-collect-garbage, might be stale
+        "${pkgs.findutils}/bin/find /nix/var/nix/temproots -type f -mtime +10 -delete"
+        # delete broken symlinks
+        "${pkgs.findutils}/bin/find /nix/var/nix/gcroots -xtype l -delete"
+      ];
+    };
+  };
+
   programs.command-not-found.enable = false;
 }
