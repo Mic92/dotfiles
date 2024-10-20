dc102e56b3fc5e8d2de289678e3ae873b814ec1b
Jörg Thalheim <joerg@thalheim.io>
2024-10-20 14:23:09 +0200

    homebrew: remove casks no longer defined in nix

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -1,4 +1,13 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
+let
+  configuredCasks = builtins.toFile "new-casks" (
+    lib.concatMapStringsSep "\n" (cask: cask.name) config.homebrew.casks
+  );
+in
 {
   homebrew.enable = true;
   system.activationScripts.homebrew.text = lib.mkIf config.homebrew.enable (
@@ -6,6 +15,9 @@
       if [[ ! -f "${config.homebrew.brewPrefix}/brew" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
+      comm -23 \
+        <(/opt/homebrew/bin/brew list --casks -q -1 | sort -u) \
+        <(sort -u ${configuredCasks}) | xargs --no-run-if-empty -I {} /opt/homebrew/bin/brew uninstall --cask {}
     ''
   );
   homebrew.casks = [
