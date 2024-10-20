c6fb9532cc776f6946e40b891cf7df2978835d3e
Jörg Thalheim <joerg@thalheim.io>
2024-10-20 15:05:02 +0200

    homebrew: simplify package listing a bit

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -10,14 +10,16 @@ let
 in
 {
   homebrew.enable = true;
+
   system.activationScripts.homebrew.text = lib.mkIf config.homebrew.enable (
     lib.mkBefore ''
-      if [[ ! -f "${config.homebrew.brewPrefix}/brew" ]]; then
+      if [[ -f "${config.homebrew.brewPrefix}/brew" ]]; then
+        comm -23 \
+          <(${config.homebrew.brewPrefix}/brew list --casks -q -1 | sort -u) \
+          <(sort -u ${configuredCasks}) | xargs --no-run-if-empty -I {} ${config.homebrew.brewPrefix}/brew uninstall --cask {}
+      else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
-      comm -23 \
-        <(/opt/homebrew/bin/brew list --casks -q -1 | sort -u) \
-        <(sort -u ${configuredCasks}) | xargs --no-run-if-empty -I {} /opt/homebrew/bin/brew uninstall --cask {}
     ''
   );
   homebrew.casks = [
