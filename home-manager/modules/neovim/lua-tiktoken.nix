9b5837c9d28c7f8b1227bb316d285064ab5948c1
Jörg Thalheim <joerg@thalheim.io>
2024-10-31 07:51:19 +0100

    lua-tiktoken: update version

--- home-manager/modules/neovim/lua-tiktoken.nix
+++ home-manager/modules/neovim/lua-tiktoken.nix
@@ -11,18 +11,18 @@ let
   src = fetchFromGitHub {
     owner = "gptlang";
     repo = "lua-tiktoken";
-    rev = "0.2.1";
-    sha256 = "sha256-drSAVGHrdDdaWUEAfCE/2ZCI2nuffpbupO+TVWv/l4Y=";
+    rev = "v0.2.2";
+    sha256 = "sha256-H83kk9dsH/cWBEx2AXQQ82l8sNfhzO864jwDd7vwAQc=";
   };
 in
 buildLuarocksPackage rec {
   pname = "tiktoken_core";
-  version = "0.2.1-1";
+  version = "0.2.2-1";
   inherit src;
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-1UXTom5LpDqSOgBrGc8INq1Y0VNYZnXtavD1hIctFfs=";
+    hash = "sha256-6dC+3x91Itp3jkqH/mXlwMfhQ/E3D52dxpv+7f/0fyU=";
   };
 
   buildInputs = [
