e3ac0d76c8000e1781d589ec0542130a599a2fbc
Jörg Thalheim <joerg@thalheim.io>
2024-11-20 00:27:30 +0100

    unbound: integrate with hyprspace

--- machines/modules/unbound.nix
+++ machines/modules/unbound.nix
@@ -1,13 +1,33 @@
+{ ... }:
 {
   services.unbound = {
     enable = true;
-    settings.server = {
-      prefetch = "yes";
-      prefetch-key = true;
-      qname-minimisation = true;
-      # Too many broken dnssec setups even at big companies such as amazon.
-      # Breaks my email setup. Better rely on tls for security.
-      val-permissive-mode = "yes";
+    settings = {
+      forward-zone = [
+        {
+          name = "\"hyprspace.\"";
+          forward-addr = "127.43.104.80@11355";
+        }
+        # not implemeneted in hyprspace yet
+        #{
+        #  name = "\"0.0.0.0.0.0.0.0.0.0.5.6.3.6.1.6.0.7.3.7.2.7.0.7.9.7.8.6.0.0.d.f.ip6.arpa.\"";
+        #  forward-addr = "127.43.104.80@11355";
+        #}
+        #{
+        #  name = "\"64.100.in-addr.arpa.\"";
+        #  forward-addr = "127.43.104.80@11355";
+        #}
+      ];
+      server = {
+        #verbosity = 3;
+        prefetch = "yes";
+        prefetch-key = true;
+        qname-minimisation = true;
+        do-not-query-localhost = false;
+        # Too many broken dnssec setups even at big companies such as amazon.
+        # Breaks my email setup. Better rely on tls for security.
+        val-permissive-mode = "yes";
+      };
     };
   };
 
