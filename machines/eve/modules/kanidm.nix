fff4ad1b3c5c0bbc11c32f93a1d5d2ed6a39c374
Jörg Thalheim <joerg@thalheim.io>
2024-11-22 11:02:54 +0100

    fix kanidm integration

--- machines/eve/modules/kanidm.nix
+++ machines/eve/modules/kanidm.nix
@@ -5,9 +5,11 @@
     tls_chain = "/var/lib/acme/kanidm.thalheim.io/fullchain.pem";
     tls_key = "/var/lib/acme/kanidm.thalheim.io/key.pem";
     bindaddress = "[::1]:3013";
+    ldapbindaddress = "[::1]:3636";
     domain = "kanidm.thalheim.io";
     origin = "https://kanidm.thalheim.io";
     trust_x_forward_for = true;
+    db_fs_type = "zfs";
 
     online_backup = {
       path = "/var/lib/kanidm/backup";
@@ -26,7 +28,7 @@
     useACMEHost = "thalheim.io";
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://[::1]:3013";
+      proxyPass = "https://[::1]:3013";
     };
   };
 }
