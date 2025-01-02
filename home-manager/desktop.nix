4f6143402394734a47fcc8df2aab02c252ace571
Jörg Thalheim <joerg@thalheim.io>
2025-01-02 11:17:10 +0100

    cleanup packages

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./common.nix
@@ -89,30 +89,6 @@
         --who="Jellyfin Media Player" \
         --mode=block \
         ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer
-    '')
-    #(retroarch.override {
-    #  cores = [
-    #    libretro.bsnes-hd
-    #    libretro.mupen64plus
-    #    libretro.beetle-psx-hw
-    #    libretro.dolphin
-    #    #libretro.pcsx2
-    #  ];
-    #})
-
-    (pkgs.writeScriptBin "rhasspy-play" ''
-      #!${pkgs.runtimeShell}
-      set -eux -o pipefail
-      export PATH=${pkgs.pulseaudioFull}/bin:$PATH
-      sink=alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink
-
-      if pamixer --get-mute --sink="$sink"; then
-          pamixer --sink=$sink --unmute
-          paplay --device=$sink
-          pamixer --sink=$sink --mute
-      else
-          paplay --device=$sink
-      fi
     '')
 
     nixos-shell
