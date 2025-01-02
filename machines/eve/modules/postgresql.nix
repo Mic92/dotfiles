6c236c8993b1b696e05e88271bda5845e9acc730
Jörg Thalheim <joerg@thalheim.io>
2025-01-02 11:47:01 +0100

    eve: update postgresql to 17

--- machines/eve/modules/postgresql.nix
+++ machines/eve/modules/postgresql.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_14;
+  services.postgresql.package = pkgs.postgresql_17;
   services.postgresql.settings = {
     max_connections = "300";
     shared_buffers = "80MB";
