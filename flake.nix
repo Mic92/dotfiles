7d783a0f65ae41ff66ff782c02d77f2d28feebf8
Jörg Thalheim <joerg@thalheim.io>
2025-01-09 15:05:59 +0100

    update buildbot

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
 
     systems.url = "github:nix-systems/default";
 
-    buildbot-nix.url = "git+https://github.com/nix-community/buildbot-nix?shallow=1&ref=better-logs";
+    buildbot-nix.url = "git+https://github.com/nix-community/buildbot-nix?shallow=1&ref=buildbot-gitea";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
