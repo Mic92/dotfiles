2790fbea37b9037aa8b7b5cadc72eafb10aba40b
Jörg Thalheim <joerg@thalheim.io>
2025-01-22 10:29:29 +0100

    disable broken nix-eval-jobs for now

--- machines/eve/modules/buildbot.nix
+++ machines/eve/modules/buildbot.nix
@@ -61,19 +61,20 @@
   services.buildbot-nix.worker = {
     enable = true;
     workerPasswordFile = config.sops.secrets.buildbot-nix-worker-password.path;
-    nixEvalJobs.package =
-      (pkgs.nix-eval-jobs.override {
-        nix = config.nix.package;
-      }).overrideAttrs
-        (_oldAttrs: {
-          src = pkgs.fetchFromGitHub {
-            owner = "nix-community";
-            repo = "nix-eval-jobs";
-            # https://github.com/nix-community/nix-eval-jobs/pull/341
-            rev = "331aa136a3414f41e7524c9614d29a35122d6275";
-            sha256 = "sha256-+ga7K6xenkpyoJgsM7ZYjacTLIoaCVxt33SzfTQrZpE=";
-          };
-        });
+    # Broken atm
+    #nixEvalJobs.package =
+    #  (pkgs.nix-eval-jobs.override {
+    #    nix = config.nix.package;
+    #  }).overrideAttrs
+    #    (_oldAttrs: {
+    #      src = pkgs.fetchFromGitHub {
+    #        owner = "nix-community";
+    #        repo = "nix-eval-jobs";
+    #        # https://github.com/nix-community/nix-eval-jobs/pull/341
+    #        rev = "331aa136a3414f41e7524c9614d29a35122d6275";
+    #        sha256 = "sha256-+ga7K6xenkpyoJgsM7ZYjacTLIoaCVxt33SzfTQrZpE=";
+    #      };
+    #    });
 
   };
 
