adf1b73cc4f3ecb16c6d496c39172e4f3867c066
Jörg Thalheim <joerg@thalheim.io>
2025-02-01 11:24:12 +0700

    nvim: add undotree

--- home/.config/nvim/lua/plugins/community.lua
+++ home/.config/nvim/lua/plugins/community.lua
@@ -36,4 +36,5 @@ return {
 	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 	{ import = "astrocommunity.editing-support.auto-save-nvim" },
 	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.editing-support.undotree" },
 }
