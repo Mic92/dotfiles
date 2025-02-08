f90cda7c31b2c3ce4e4273ffd8dd362354be20ca
Jörg Thalheim <joerg@thalheim.io>
2025-02-08 07:01:45 +0100

    disable data-mesher for nwo

--- machines/turingmachine/modules/data-mesher.nix
+++ machines/turingmachine/modules/data-mesher.nix
@@ -1,56 +1,56 @@
 {
-  config,
-  inputs,
-  pkgs,
+  #  config,
+  #  inputs,
+  #  pkgs,
   ...
 }:
 {
-  imports = [
-    inputs.data-mesher.nixosModules.data-mesher
-  ];
+  #imports = [
+  #  inputs.data-mesher.nixosModules.data-mesher
+  #];
 
-  services.data-mesher = {
-    enable = true;
-    openFirewall = true;
+  #services.data-mesher = {
+  #  enable = true;
+  #  openFirewall = true;
 
-    settings = {
-      log_level = "debug";
-      network.id = "dontknow";
+  #  settings = {
+  #    log_level = "debug";
+  #    network.id = "dontknow";
 
-      key_path = config.clan.core.vars.generators.data-mesher.files."dm.pem".path;
-      host.names = [
-        "turingmachine"
-        #"earth"
-      ];
-      network.public_key = "MCowBQYDK2VwAyEAL+ajPxkanYcLFURnlyo31uFEzuymbDyvtMcYjT/H9nw=";
-      cluster = {
-        interface = "tailscale0";
-        port = 7946;
-        bootstrap_peers = [
-          "[fd7a:115c:a1e0::8f01:9e1f]:7946"
-        ];
-        push_pull_interval = "15s";
-      };
-    };
+  #    key_path = config.clan.core.vars.generators.data-mesher.files."dm.pem".path;
+  #    host.names = [
+  #      "turingmachine"
+  #      #"earth"
+  #    ];
+  #    network.public_key = "MCowBQYDK2VwAyEAL+ajPxkanYcLFURnlyo31uFEzuymbDyvtMcYjT/H9nw=";
+  #    cluster = {
+  #      interface = "tailscale0";
+  #      port = 7946;
+  #      bootstrap_peers = [
+  #        "[fd7a:115c:a1e0::8f01:9e1f]:7946"
+  #      ];
+  #      push_pull_interval = "15s";
+  #    };
+  #  };
 
-    #initNetwork = {
-    #  enable = true;
-    #  tld = "sol";
-    #  hostTTL = "1m";
-    #  signingKeys = [
-    #    "MCowBQYDK2VwAyEAL+ajPxkanYcLFURnlyo31uFEzuymbDyvtMcYjT/H9nw="
-    #  ];
-    #};
-  };
-  services.tailscale.enable = true;
+  #  #initNetwork = {
+  #  #  enable = true;
+  #  #  tld = "sol";
+  #  #  hostTTL = "1m";
+  #  #  signingKeys = [
+  #  #    "MCowBQYDK2VwAyEAL+ajPxkanYcLFURnlyo31uFEzuymbDyvtMcYjT/H9nw="
+  #  #  ];
+  #  #};
+  #};
+  #services.tailscale.enable = true;
 
-  clan.core.vars.generators.data-mesher = {
-    files."dm.pem".owner = "data-mesher";
-    runtimeInputs = [
-      pkgs.openssl
-    ];
-    script = ''
-      openssl genpkey -algorithm ed25519 -out $out/dm.pem
-    '';
-  };
+  #clan.core.vars.generators.data-mesher = {
+  #  files."dm.pem".owner = "data-mesher";
+  #  runtimeInputs = [
+  #    pkgs.openssl
+  #  ];
+  #  script = ''
+  #    openssl genpkey -algorithm ed25519 -out $out/dm.pem
+  #  '';
+  #};
 }
