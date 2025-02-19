85b40c9572e2503770aa9e58a9f01b1bc752a86f
Jörg Thalheim <joerg@thalheim.io>
2025-02-19 14:58:08 +0700

    homebrew: use option to cleanup packages

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -3,29 +3,21 @@
   lib,
   ...
 }:
-let
-  configuredCasks = builtins.toFile "new-casks" (
-    lib.concatMapStringsSep "\n" (cask: cask.name) config.homebrew.casks
-  );
-in
 {
   homebrew.enable = true;
 
   system.activationScripts.homebrew.text = lib.mkIf config.homebrew.enable (
     lib.mkBefore ''
-      if [[ -f "${config.homebrew.brewPrefix}/brew" ]]; then
-        comm -23 \
-          <(${config.homebrew.brewPrefix}/brew list --casks -q -1 | sort -u) \
-          <(sort -u ${configuredCasks}) | xargs --no-run-if-empty -I {} ${config.homebrew.brewPrefix}/brew uninstall --cask {}
-      else
+      if [[ ! -f "${config.homebrew.brewPrefix}/brew" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
     ''
   );
+  homebrew.onActivation.cleanup = "uninstall";
   homebrew.casks = [
-    "amethyst"
     "ferdium"
     "firefox"
     "signal"
+    "ghostty"
   ];
 }
