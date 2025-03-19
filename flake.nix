2345c16c01477332b1b8454124fba9746d10f648
Jörg Thalheim <joerg@thalheim.io>
2025-03-19 13:29:40 +0100

    add clan template for testing

--- flake.nix
+++ flake.nix
@@ -163,6 +163,11 @@
           );
         };
 
+        clan.templates.clan."hetzner" = {
+          description = "hetzner";
+          path = ./templates/hetzner;
+        };
+
         perSystem =
           {
             inputs',
---
+++ templates/hetzner/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment for this project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = lib.systems.flakeExposed;
+        perSystem =
+          { pkgs, ... }:
+          {
+            packages.default = pkgs.mkShell {
+              packages = [
+                pkgs.bashInteractive
+              ];
+            };
+          };
+      }
+    );
+}
