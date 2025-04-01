2b9a5bf5a1cffaaf35218fb264950e70d08ea5c3
Jörg Thalheim <joerg@thalheim.io>
2025-04-01 13:35:59 +0200

    switch to nixpkgs module for renovate

--- machines/eve/modules/renovate/default.nix
+++ machines/eve/modules/renovate/default.nix
@@ -1,5 +1,14 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
+  # Based on https://docs.renovatebot.com/modules/platform/github/#running-as-a-github-app
+  # https://github.com/settings/apps/mic92-renovate
+  appId = "375151"; # app id of the renovate app
+  appLogin = "Mic92"; # user or organization name that installed the app
   apptoken = pkgs.writeShellApplication {
     name = "apptoken";
     runtimeInputs = with pkgs; [
@@ -9,32 +18,11 @@ let
     ];
     text = builtins.readFile ./apptoken.sh;
   };
-  # Based on https://docs.renovatebot.com/modules/platform/github/#running-as-a-github-app
-  # https://github.com/settings/apps/mic92-renovate
-  appId = "375151"; # app id of the renovate app
-  appLogin = "Mic92"; # user or organization name that installed the app
 in
 {
-  systemd.services.renovate = {
-    environment = {
-      RENOVATE_CONFIG_FILE = pkgs.writers.writeJSON "renovate.json" {
-        labels = [
-          "dependencies"
-          "renovate"
-        ];
-        nix.enabled = true;
-        lockFileMaintenance.enabled = true;
-        autodiscover = true;
-        autodiscoverTopics = [ "managed-by-renovate" ];
-        baseDir = "/var/lib/renovate/";
-        cacheDir = "/var/lib/renovate/cache";
-        username = "mic92-renovate[bot]";
-        gitAuthor = "Mic92's Renovate Bot <142113131+mic92-renovate[bot]@users.noreply.github.com>";
-      };
-      LOG_LEVEL = "debug";
-    };
-    startAt = "hourly";
-    path = [
+  services.renovate = {
+    enable = true;
+    runtimePackages = [
       pkgs.git
       pkgs.openssh
       config.nix.package
@@ -43,39 +31,64 @@ in
       pkgs.cargo
       # for go.mod
       pkgs.go
+      pkgs.nodejs
     ];
-    # We don't want to restart the service on updates
-    # as the timer will run the new version eventually anyway.
-    restartIfChanged = true;
-    serviceConfig = {
-      Type = "simple";
-      RemainAfterExit = true;
-      Restart = "on-failure";
-      StartLimitBurst = 3;
-      RuntimeDirectory = "renovate";
-      StateDirectory = "renovate";
-      User = "renovate";
-      Group = "renovate";
-      ExecStartPre = "+${pkgs.writeShellScript "setup-apptoken" ''
+    settings = {
+      labels = [
+        "dependencies"
+        "renovate"
+      ];
+      nix.enabled = true;
+      lockFileMaintenance.enabled = true;
+      automerge = true;
+      autodiscover = true;
+      autodiscoverTopics = [ "managed-by-renovate" ];
+      platform = "gitea";
+      endpoint = "https://git.clan.lol/api/v1/";
+      onboarding = true;
+      username = "mic92-renovate[bot]";
+      gitAuthor = "Mic92's Renovate Bot <142113131+mic92-renovate[bot]@users.noreply.github.com>";
+      #allowedCommands = [
+      #  "^tslint --fix$"
+      #  "^tslint --[a-z]+$"
+      #];
+    };
+    schedule = "*:0/10";
+    package = pkgs.renovate.overrideAttrs (
+      {
+        patches ? [ ],
+        ...
+      }:
+      {
+        patches = patches ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/renovatebot/renovate/pull/33991.diff";
+            hash = "sha256-6ME048IiptweOkJhnK9QvQqfJ6QaXWX23SlY5TAmsFE=";
+          })
+        ];
+      }
+    );
+  };
+  systemd.services.renovate-token = {
+    serviceConfig.ExecStartPre = [
+      "+${pkgs.writeShellScript "setup-apptoken" ''
         set -euo pipefail
         export APP_ID=${appId}
         export APP_LOGIN=${appLogin}
-        export APP_PRIVATE_KEY=$(cat ${config.sops.secrets.github-renovate-app-private-key.path})
-        echo "RENOVATE_TOKEN=$(${apptoken}/bin/apptoken)" > /var/lib/renovate/env
-      ''}";
-    };
-    script = ''
-      set -exuo pipefail
-      export $(cat /var/lib/renovate/env)
-      rm /var/lib/renovate/env
-      ${pkgs.lib.getExe pkgs.renovate}
+        export APP_PRIVATE_KEY=$(cat ${config.clan.core.vars.generators.renovate.files.app-private-key.path})
+        export "RENOVATE_TOKEN=$(${apptoken}/bin/apptoken)" > /var/lib/renovate/env
+      ''}"
+    ];
+    script = lib.mkBefore ''
+      source "/var/lib/renovate/env"
     '';
   };
-  users.users.renovate = {
-    isSystemUser = true;
-    createHome = false;
-    home = "/var/lib/renovate";
-    group = "renovate";
+
+  clan.core.vars.generators.renovate = {
+    prompts.app-private-key = {
+      type = "multiline";
+      persist = true;
+      description = "Go to https://git.clan.lol/user/settings/applications and create a new token";
+    };
   };
-  users.groups.renovate = { };
 }
