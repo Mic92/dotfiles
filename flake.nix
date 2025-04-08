305930e046f401bf28946101b279e74e420d6812
Jörg Thalheim <joerg@thalheim.io>
2025-04-08 11:12:05 +0200

    unpin clan

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
     srvos.url = "github:numtide/srvos/dotfiles";
     srvos.inputs.nixpkgs.follows = "nixpkgs";
 
-    clan-core.url = "https://git.clan.lol/clan/clan-core/archive/mac.tar.gz";
+    clan-core.url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
     #clan-core.url = "path:///Users/joerg/git/clan-core";
     #clan-core.url = "git+https://git.clan.lol/clan/clan-core?ref=nixos-rebuild";
     #clan-core.url = "git+file:///home/joerg/work/clan/clan-core";
