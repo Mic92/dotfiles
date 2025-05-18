184ef10eb745c3a71bba32d174f6c464c2feabad
Jörg Thalheim <joerg@thalheim.io>
2025-05-18 21:11:04 +0200

    nvim: switch back to relativenumber

--- home/.config/nvim/lua/plugins/core.lua
+++ home/.config/nvim/lua/plugins/core.lua
@@ -4,10 +4,7 @@ return {
 	priority = 10000, -- load AstroCore first
 	opts = {
 		options = {
-			opt = {
-				-- set configuration options  as described below
-				relativenumber = false, -- sets vim.opt.relativenumber
-			},
+			opt = {},
 		},
 	},
 }
