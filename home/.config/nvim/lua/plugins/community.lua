86649afad92091e6ba80eb23268202439684af20
Jörg Thalheim <joerg@thalheim.io>
2025-05-20 13:44:20 +0200

    drop leap-nvim

--- home/.config/nvim/lua/plugins/community.lua
+++ home/.config/nvim/lua/plugins/community.lua
@@ -38,6 +38,5 @@ return {
 	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 	{ import = "astrocommunity.editing-support.auto-save-nvim" },
 	{ import = "astrocommunity.motion.nvim-surround" },
-	{ import = "astrocommunity.motion.leap-nvim" },
 	{ import = "astrocommunity.editing-support.undotree" },
 }
