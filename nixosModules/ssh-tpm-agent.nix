13189a269a47e3b8e3713dde3430fcf6d9a5d618
Jörg Thalheim <joerg@thalheim.io>
2025-05-30 20:18:42 +0200

    ssh-tpm-agent: support for fido keys

--- nixosModules/ssh-tpm-agent.nix
+++ nixosModules/ssh-tpm-agent.nix
@@ -7,6 +7,14 @@ let
     export WAYLAND_DISPLAY="$(systemctl --user show-environment | sed 's/^WAYLAND_DISPLAY=\(.*\)/\1/; t; d')"
     exec ${config.programs.ssh.askPassword} "$@"
   '';
+  # Example SSH config for using the TPM2 agent + FIDO keys for selected hosts:
+  #Host *.hosts-with-yubikeys.tld
+  #   IdentityFile ~/.ssh/id_ed25519_sk
+  #   IdentityAgent /run/user/1000/ssh-fido-agent.sock
+  #Host *
+  #  IdentitiesOnly yes
+  #  IdentityFile ~/.ssh/id_ecdsa_tpm.pub
+  #  IdentityAgent /run/user/1000/ssh-tpm-agent.sock
 in
 {
   security.tpm2.enable = true;
@@ -44,6 +52,39 @@ in
       ListenStream = "%t/ssh-tpm-agent.sock";
       SocketMode = "0600";
       Service = "ssh-tpm-agent.service";
+    };
+  };
+
+  # For fido keys, we have an on-demand ssh-agent that is started
+
+  systemd.user.services.ssh-fido-agent = {
+    path = [ pkgs.gnused ];
+    description = "SSH Agent";
+    wantedBy = [ "default.target" ];
+    unitConfig = {
+      ConditionUser = "!@system";
+      ConditionEnvironment = "!SSH_AGENT_PID";
+    };
+    environment = {
+      SSH_ASKPASS = askPasswordWrapper;
+      SSH_AUTH_SOCK = "%t/ssh-fido-agent.sock";
+    };
+    serviceConfig = {
+      ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/ssh-agent";
+      ExecStart = "${pkgs.openssh}/bin/ssh-agent";
+      PassEnvironment = "SSH_AGENT_PID";
+      SuccessExitStatus = 2;
+      Type = "simple";
+    };
+  };
+
+  systemd.user.sockets.ssh-fido-agent = {
+    wantedBy = [ "sockets.target" ];
+    unitConfig.Description = "SSH Agent Socket";
+    socketConfig = {
+      ListenStream = "%t/ssh-fido-agent.sock";
+      SocketMode = "0600";
+      Service = "ssh-fido-agent.service";
     };
   };
 }
