c3e996a266c1955f4fb474bf08f3c0a4b0e430b2
Jörg Thalheim <joerg@thalheim.io>
2025-06-11 08:22:50 +0200

    ssh-tpm-agent: align ssh-agent with upstream and add seperate ssh-fido-agent

--- nixosModules/ssh-tpm-agent.nix
+++ nixosModules/ssh-tpm-agent.nix
@@ -28,19 +28,20 @@ in
   ];
 
   systemd.user.services.ssh-tpm-agent = {
-    path = [ pkgs.gnused ];
-    description = "SSH Agent";
-    wantedBy = [ "default.target" ];
+    description = "ssh-tpm-agent service";
+    documentation = [
+      "man:ssh-agent(1)"
+      "man:ssh-add(1)"
+      "man:ssh(1)"
+    ];
     unitConfig = {
-      ConditionUser = "!@system";
       ConditionEnvironment = "!SSH_AGENT_PID";
+      Requires = "ssh-tpm-agent.socket";
     };
     environment = {
       SSH_ASKPASS = askPasswordWrapper;
-      SSH_AUTH_SOCK = "%t/ssh-tpm-agent.sock";
     };
     serviceConfig = {
-      ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/ssh-agent";
       ExecStart = "${pkgs.ssh-tpm-agent}/bin/ssh-tpm-agent";
       PassEnvironment = "SSH_AGENT_PID";
       SuccessExitStatus = 2;
@@ -50,7 +51,14 @@ in
 
   systemd.user.sockets.ssh-tpm-agent = {
     wantedBy = [ "sockets.target" ];
-    unitConfig.Description = "SSH Agent Socket";
+    unitConfig = {
+      Description = "SSH TPM agent socket";
+      Documentation = [
+        "man:ssh-agent(1)"
+        "man:ssh-add(1)"
+        "man:ssh(1)"
+      ];
+    };
     socketConfig = {
       ListenStream = "%t/ssh-tpm-agent.sock";
       SocketMode = "0600";
@@ -62,32 +70,20 @@ in
 
   systemd.user.services.ssh-fido-agent = {
     path = [ pkgs.gnused ];
-    description = "SSH Agent";
+    description = "SSH Agent for FIDO keys";
     wantedBy = [ "default.target" ];
     unitConfig = {
       ConditionUser = "!@system";
-      ConditionEnvironment = "!SSH_AGENT_PID";
     };
     environment = {
       SSH_ASKPASS = askPasswordWrapper;
-      SSH_AUTH_SOCK = "%t/ssh-fido-agent.sock";
     };
     serviceConfig = {
-      ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/ssh-agent";
-      ExecStart = "${pkgs.openssh}/bin/ssh-agent";
-      PassEnvironment = "SSH_AGENT_PID";
-      SuccessExitStatus = 2;
+      ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/ssh-fido-agent.sock";
+      ExecStart = "${pkgs.openssh}/bin/ssh-agent -D -a %t/ssh-fido-agent.sock";
+      Restart = "on-failure";
+      RestartSec = "1s";
       Type = "simple";
-    };
-  };
-
-  systemd.user.sockets.ssh-fido-agent = {
-    wantedBy = [ "sockets.target" ];
-    unitConfig.Description = "SSH Agent Socket";
-    socketConfig = {
-      ListenStream = "%t/ssh-fido-agent.sock";
-      SocketMode = "0600";
-      Service = "ssh-fido-agent.service";
     };
   };
 }
