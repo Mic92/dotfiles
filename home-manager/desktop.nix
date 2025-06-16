421117f1499323765dffc6f7002d33d89c136cf0
Jörg Thalheim <joerg@thalheim.io>
2025-06-16 17:21:54 +0200

    fix home-manager desktop: make ghostty conditional on Linux
    ghostty is only available on Linux platforms, so conditionally
    include it in home.packages to fix Darwin builds

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -13,54 +13,58 @@
   services.mpris-proxy.enable = true;
   services.syncthing.enable = true;
 
-  home.packages = with pkgs; [
-    league-of-moveable-type
-    dejavu_fonts
-    ubuntu_font_family
-    unifont
-    twitter-color-emoji
-    upterm
-    eternal-terminal
-    gimp
-    signal-desktop
-    adwaita-icon-theme
-    hicolor-icon-theme
-    graphicsmagick
-    self.packages.${pkgs.system}.ghostty
-    screen-message
-    sshfs-fuse
-    sshuttle
-    jq
-    git-lfs
-    cheat
-    xdg-utils
-    tio
+  home.packages =
+    with pkgs;
+    [
+      league-of-moveable-type
+      dejavu_fonts
+      ubuntu_font_family
+      unifont
+      twitter-color-emoji
+      upterm
+      eternal-terminal
+      gimp
+      signal-desktop
+      adwaita-icon-theme
+      hicolor-icon-theme
+      graphicsmagick
+      screen-message
+      sshfs-fuse
+      sshuttle
+      jq
+      git-lfs
+      cheat
+      xdg-utils
+      tio
 
-    (mpv.override { scripts = [ mpvScripts.mpris ]; })
-    playerctl
-    yt-dlp
-    mumble
-    ferdium
-    kubectl
-    hyperfine
+      (mpv.override { scripts = [ mpvScripts.mpris ]; })
+      playerctl
+      yt-dlp
+      mumble
+      ferdium
+      kubectl
+      hyperfine
 
-    q
-    rbw
-    # to fix xdg-open
-    glib
-    zoom-us
-    (pkgs.writeScriptBin "jellyfinmediaplayer" ''
-      # bluetooth speaker
-      bluetoothctl connect E6:4D:D6:0A:CC:9B &
-      systemd-inhibit \
-        --why="Jellyfin Media Player" \
-        --who="Jellyfin Media Player" \
-        --mode=block \
-        ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer
-    '')
+      q
+      rbw
+      # to fix xdg-open
+      glib
+      zoom-us
+      (pkgs.writeScriptBin "jellyfinmediaplayer" ''
+        # bluetooth speaker
+        bluetoothctl connect E6:4D:D6:0A:CC:9B &
+        systemd-inhibit \
+          --why="Jellyfin Media Player" \
+          --who="Jellyfin Media Player" \
+          --mode=block \
+          ${pkgs.jellyfin-media-player}/bin/jellyfinmediaplayer
+      '')
 
-    nixos-shell
-    nerd-fonts.fira-code
-    inxi
-  ];
+      nixos-shell
+      nerd-fonts.fira-code
+      inxi
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      self.packages.${pkgs.system}.ghostty
+    ];
 }
