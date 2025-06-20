274f794a5ec9d07133f94540cbebc61bdce18b82
Jörg Thalheim <joerg@thalheim.io>
2025-06-20 13:09:14 +0200

    nextcloud: fix ldap configuration

--- machines/eve/modules/nextcloud.nix
+++ machines/eve/modules/nextcloud.nix
@@ -29,7 +29,39 @@
     };
   };
 
+  # LDAP configuration will be applied via occ commands
+  systemd.services.nextcloud-ldap-config = {
+    after = [ "nextcloud-setup.service" ];
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      # Set LDAP configuration (using default configuration ID)
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapHost "127.0.0.1"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapPort "389"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapAgentName "cn=nextcloud,ou=system,ou=users,dc=eve"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapAgentPassword "$(cat ${config.sops.secrets.nextcloud-ldap-password.path})"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapBase "ou=users,dc=eve"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapBaseUsers "ou=users,dc=eve"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapUserFilter "(objectClass=ownCloud)"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapUserDisplayName "cn"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapEmailAttribute "mail"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapLoginFilter "(&(objectClass=ownCloud)(mail=%uid))"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapExpertUsernameAttr "mail"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapQuotaAttribute "owncloudquota"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapQuotaDefault "10000000000"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ ldap:set-config "" ldapConfigurationActive "1"
+
+      # Enable LDAP app
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ app:enable user_ldap || true
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nextcloud";
+      RemainAfterExit = true;
+    };
+  };
+
   sops.secrets.nextcloud-admin-password.owner = "nextcloud";
+  sops.secrets.nextcloud-ldap-password.owner = "nextcloud";
 
   services.nginx.virtualHosts."cloud.thalheim.io" = {
     useACMEHost = "thalheim.io";
