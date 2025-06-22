ed8410d239c0543b31c1dcb8699a8864123fb978
Jörg Thalheim <joerg@thalheim.io>
2025-06-22 21:14:17 +0200

    ssh-tpm-agent: no need to enable pkgcs11

--- nixosModules/ssh-tpm-agent.nix
+++ nixosModules/ssh-tpm-agent.nix
@@ -18,7 +18,6 @@ let
 in
 {
   security.tpm2.enable = true;
-  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
   security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
   users.users.joerg.extraGroups = [ "tss" ]; # tss group has access to TPM devices
 
