182d2760412b7b6c9424f2c624894ecec1b7b4d8
Jörg Thalheim <joerg@thalheim.io>
2025-07-09 10:12:02 +0200

    eva: bump ips

--- machines/eva/modules/zerotier.nix
+++ machines/eva/modules/zerotier.nix
@@ -1,8 +1,8 @@
 {
   imports = [ ../../../nixosModules/zerotier.nix ];
   clan.zerotier.moon.stableEndpoints = [
-    "89.58.27.144"
-    "2a03:4000:62:fdb::"
+    "116.203.179.132"
+    "2a01:4f8:1c1a:37b2::1"
   ];
   services.zerotierone.blockRfc1918Addresses = true;
 }
