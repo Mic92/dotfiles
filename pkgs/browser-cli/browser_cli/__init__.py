b671ac884ad056265037ea360cb4fb16834f69ad
Jörg Thalheim <joerg@thalheim.io>
2025-07-13 22:20:31 +0200

    browser-cli: split of bridge module

--- pkgs/browser-cli/browser_cli/__init__.py
+++ pkgs/browser-cli/browser_cli/__init__.py
@@ -1,5 +1,6 @@
 """Browser CLI - Control Firefox through WebSocket connection."""
 
+from browser_cli.bridge import NativeMessagingBridge
 from browser_cli.cli import main
 
-__all__ = ["main"]
+__all__ = ["main", "NativeMessagingBridge"]
