e30206e36877e07ddffc67c60c21df41241b1431
Jörg Thalheim <joerg@thalheim.io>
2025-07-21 17:32:14 +0200

    migrate to clan instances

--- machines/eva/modules/zerotier.nix
+++ machines/eva/modules/zerotier.nix
@@ -1,8 +1,4 @@
 {
   imports = [ ../../../nixosModules/zerotier.nix ];
-  clan.zerotier.moon.stableEndpoints = [
-    "116.203.179.132"
-    "2a01:4f8:1c1a:37b2::1"
-  ];
   services.zerotierone.blockRfc1918Addresses = true;
 }
