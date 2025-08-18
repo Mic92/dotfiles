e1d4e2b8d103da3088d9fa59aa9b2ee6bc5dc634
Jörg Thalheim <joerg@thalheim.io>
2025-08-18 08:55:21 +0200

    audio-chooser: rewrite in python

--- nixosModules/kde/choosers.nix
+++ nixosModules/kde/choosers.nix
@@ -7,10 +7,36 @@
   runtimeShell,
   bluez,
   libnotify,
+  python3,
 }:
 runCommand "audio-chooser" { } ''
   mkdir -p $out/bin $out/share/applications
 
+  # Audio chooser (Python)
+  cat > $out/bin/audio-chooser <<'EOF'
+  #!${python3}/bin/python3
+  import sys
+  import os
+  os.environ['PATH'] = '${
+    lib.makeBinPath [
+      pamixer
+      fuzzel
+      pulseaudio
+      bluez
+      libnotify
+    ]
+  }:' + os.environ.get('PATH', "")
+  ${builtins.readFile ./audio_chooser.py}
+  EOF
+  chmod +x $out/bin/audio-chooser
+  cat > $out/share/applications/audio-chooser.desktop <<EOF
+  [Desktop Entry]
+  Name=audio-chooser
+  Exec=$out/bin/audio-chooser
+  Type=Application
+  EOF
+
+  # Other choosers (shell scripts)
   ${lib.concatMapStringsSep "\n"
     (chooser: ''
       cat > $out/bin/${chooser} <<'EOF'
@@ -35,7 +61,6 @@ runCommand "audio-chooser" { } ''
       EOF
     '')
     [
-      "audio-chooser"
       "internet-chooser"
       "display-chooser"
     ]
