80a92f3b0bc71bd835f87a2862a0123611339c93
Jörg Thalheim <joerg@thalheim.io>
2025-09-13 18:30:06 +0200

    kanidm: switch to 1.7

--- machines/eve/modules/kanidm.nix
+++ machines/eve/modules/kanidm.nix
@@ -1,5 +1,7 @@
+{ pkgs, ... }:
 {
   services.kanidm.enableServer = true;
+  services.kanidm.package = pkgs.kanidm_1_7;
   services.kanidm.serverSettings = {
     tls_chain = "/var/lib/acme/kanidm.thalheim.io/fullchain.pem";
     tls_key = "/var/lib/acme/kanidm.thalheim.io/key.pem";
