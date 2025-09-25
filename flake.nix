2f089bce07928c1acba6280ebb3484582e351d6c
Jörg Thalheim <joerg@thalheim.io>
2025-09-25 09:46:17 +0200

    bump buildbot

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
     data-mesher.inputs.flake-parts.follows = "flake-parts";
     data-mesher.inputs.treefmt-nix.follows = "treefmt-nix";
 
-    buildbot-nix.url = "git+https://github.com/nix-community/buildbot-nix?shallow=1&ref=cancel-evals";
+    buildbot-nix.url = "git+https://github.com/nix-community/buildbot-nix?shallow=1";
     buildbot-nix.inputs.nixpkgs.follows = "nixpkgs";
     buildbot-nix.inputs.flake-parts.follows = "flake-parts";
     buildbot-nix.inputs.treefmt-nix.follows = "treefmt-nix";
