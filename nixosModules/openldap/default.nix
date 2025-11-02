925be5304a2484ac3eac6dffaa2957a4d12df7c5
Jörg Thalheim <joerg@thalheim.io>
2025-11-02 17:15:02 +0100

    ttrss: drop schema

--- nixosModules/openldap/default.nix
+++ nixosModules/openldap/default.nix
@@ -209,15 +209,6 @@
                          MAY ( ownCloudQuota ))''
         ];
       };
-      "cn={1}ttrss,cn=schema".attrs = {
-        cn = "{1}ttrss";
-        objectClass = "olcSchemaConfig";
-        olcObjectClasses = ''
-          (1.3.6.1.4.1.28294.1.2.4 NAME 'ttrss'
-                      SUP top AUXILIARY
-                      DESC 'Added to an account to allow tinytinyrss access'
-                      MUST ( mail $ userPassword ))'';
-      };
       "cn={1}prometheus,cn=schema".attrs = {
         cn = "{1}prometheus";
         objectClass = "olcSchemaConfig";
