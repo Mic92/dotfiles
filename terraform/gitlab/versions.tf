5648ae771ac7e94e62dedad4b2b87cdd835644fd
Jörg Thalheim <joerg@thalheim.io>
2025-12-04 12:50:53 +0100

    gitlab: add version constraints.tf

---
+++ terraform/gitlab/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
