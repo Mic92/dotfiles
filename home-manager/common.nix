e0ff8a607fe85642b6569fbd15bbe5f732703f28
Jörg Thalheim <joerg@thalheim.io>
2025-12-24 08:46:47 +0000

    nix-eval-warnings: install into home

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -80,6 +80,7 @@
 
       self.packages.${pkgs.stdenv.hostPlatform.system}.merge-when-green
       self.packages.${pkgs.stdenv.hostPlatform.system}.buildbot-pr-check
+      self.packages.${pkgs.stdenv.hostPlatform.system}.nix-eval-warnings
       self.inputs.direnv-instant.packages.${pkgs.stdenv.hostPlatform.system}.default
       self.inputs.flake-fmt.packages.${pkgs.stdenv.hostPlatform.system}.default
       self.inputs.nix-diff-rs.packages.${pkgs.stdenv.hostPlatform.system}.default
