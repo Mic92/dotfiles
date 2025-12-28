619a7eed62feac18a2e84a3147da83bcf80ee4cf
Jörg Thalheim <joerg@thalheim.io>
2025-12-28 10:26:31 +0000

    telegraf: add http-sd

--- machines/eva/modules/telegraf/default.nix
+++ machines/eva/modules/telegraf/default.nix
@@ -3,9 +3,10 @@
   imports = [
     ./private.nix
     ./dave.nix
-    ./uni.nix
+    ./http-sd.nix
     ./krebs.nix
     ./nixos-wiki-infra.nix
+    ./uni.nix
   ];
 
   sops.secrets.eva-telegraf.owner = config.systemd.services.telegraf.serviceConfig.User;
--- machines/eva/modules/telegraf/uni.nix
+++ machines/eva/modules/telegraf/uni.nix
@@ -1,60 +1,14 @@
-{ lib, ... }:
-let
-  urls = [
-    # university
-    "ace.r"
-    "adelaide.r"
-    "amy.r"
-    "astrid.r"
-    "christina.r"
-    "clara.r"
-    "dan.r"
-    "doctor.r"
-    "donna.r"
-    "ian.r"
-    "graham.r"
-    "irene.r"
-    "jack.r"
-    "jackson.r"
-    "mickey.r"
-    "river.r"
-    "rose.r"
-    "ruby.r"
-    "ryan.r"
-    "tegan.r"
-    "vislor.r"
-    "wilfred.r"
-    "xavier.r"
-    "yasmin.r"
-    # TODO: add tower
-    #"vicki.r"
-  ];
-in
 {
-  services.telegraf.extraConfig.inputs = {
-    ping = map (url: {
-      method = "native";
-      urls = [ "6.${url}" ];
-      ipv6 = true;
-      tags.org = "uni";
-      tags.host = lib.removeSuffix ".r" url;
-    }) urls;
-    net_response = map (host: {
-      protocol = "tcp";
-      address = "${host}:22";
-      tags.host = host;
-      tags.org = "uni";
-      send = "SSH-2.0-Telegraf";
-      expect = "SSH-2.0";
-      timeout = "10s";
-    }) urls;
-
-    x509_cert = [
-      {
-        sources = [ "https://web.dse.in.tum.de:443" ];
-        tags.host = "vmbhatotia19";
-        tags.org = "uni";
-      }
-    ];
+  services.telegraf.httpSd.targets.uni = {
+    name = "uni";
+    url = "https://tum-dse.github.io/doctor-cluster-config/telegraf.json";
   };
+
+  services.telegraf.extraConfig.inputs.x509_cert = [
+    {
+      sources = [ "https://web.dse.in.tum.de:443" ];
+      tags.host = "vmbhatotia19";
+      tags.org = "uni";
+    }
+  ];
 }
