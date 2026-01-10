f6dc81842e17a95034acc7a6a8f6c3806281d653
Jörg Thalheim <joerg@thalheim.io>
2026-01-10 13:54:13 +0000

    neovim: switch from bundled tree-sitter to on-demand tree-sitter
    this is less complex

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = "nvim-install-treesitter",
 	opts = {
-		-- Disable auto_install since we provide all grammars through Nix
-		auto_install = false,
+		-- tree-sitter CLI is wrapped with cc in PATH, so grammars compile from source
+		auto_install = true,
 	},
 }
