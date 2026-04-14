3800db96a9ea3c0d8de160413246383143abbff1
Jörg Thalheim <joerg@thalheim.io>
2026-04-14 19:00:07 +0200

    remote-builder: add missing uid-range to remote builder

--- darwinModules/remote-builder.nix
+++ darwinModules/remote-builder.nix
@@ -18,6 +18,7 @@
         "kvm"
         "nixos-test"
         "recursive-nix"
+        "uid-range"
       ];
     }
     {
@@ -32,6 +33,7 @@
         "kvm"
         "nixos-test"
         "recursive-nix"
+        "uid-range"
       ];
     }
     #{
