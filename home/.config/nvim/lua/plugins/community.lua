a5420e671fd986d37ddf9c31db88ecd08d64dffc
Jörg Thalheim <joerg@thalheim.io>
2026-04-19 13:45:21 +0200

    astronvim: tokyio colors

--- home/.config/nvim/lua/plugins/community.lua
+++ home/.config/nvim/lua/plugins/community.lua
@@ -4,6 +4,7 @@ return {
 	branch = "main",
 	-- example of importing a plugin, comment out to use it or add your own
 	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
 	{ import = "astrocommunity.completion.copilot-lua" },
 	{ import = "astrocommunity.completion.copilot-lua-cmp" },
 	{ import = "astrocommunity.completion.blink-cmp-tmux" },
