15c6d186f1ce6a184e778350ec17f79a74984217
Jörg Thalheim <joerg@thalheim.io>
2026-06-21 22:47:47 +0200

    eve: bump tribuchet

--- machines/eve/modules/telegraf.nix
+++ machines/eve/modules/telegraf.nix
@@ -36,6 +36,11 @@
           urls = [ "http://127.0.0.1:5000/metrics" ];
           metric_version = 2;
         }
+        {
+          # tribuchet build hub
+          urls = [ "http://127.0.0.1:7438/metrics" ];
+          metric_version = 2;
+        }
       ];
     };
   };
