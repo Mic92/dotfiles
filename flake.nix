0e2775ffeaa7cc07e9ba90ebd74a01eebbb07897
Jörg Thalheim <joerg@thalheim.io>
2026-07-13 12:35:14 +0200

    nixbot: bump

--- flake.nix
+++ flake.nix
@@ -84,7 +84,7 @@
     data-mesher.inputs.treefmt-nix.follows = "treefmt-nix";
     data-mesher.inputs.flake-parts.follows = "flake-parts";
 
-    nixbot.url = "git+https://github.com/Mic92/nixbot?shallow=1&ref=better-live-logs";
+    nixbot.url = "git+https://github.com/Mic92/nixbot?shallow=1";
     nixbot.inputs.nixpkgs.follows = "nixpkgs";
     nixbot.inputs.treefmt-nix.follows = "treefmt-nix";
 
