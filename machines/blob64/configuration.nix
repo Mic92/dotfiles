2e5d8c9de150a3382ef99a4927fbc3114682c5e8
Jörg Thalheim <joerg@thalheim.io>
2026-08-17 23:11:18 +0200

    blob64: remove broken ghorg backups
    GitHub repos are mirrored to Radicle by radicle-mirror on eve now, so
    the broken ghorg-based backup service is no longer needed.

--- machines/blob64/configuration.nix
+++ machines/blob64/configuration.nix
@@ -17,7 +17,6 @@
     ./modules/photoprism.nix
     ./modules/phantun.nix
     ./modules/pinchflat.nix
-    ./modules/ghorg.nix
   ];
 
   nixpkgs.pkgs = self.inputs.nixpkgs.legacyPackages.aarch64-linux;
@@ -54,40 +53,4 @@
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "ac174b52";
 
-  # GitHub/Gitea repository backups
-  services.ghorg.backups = {
-    mic92 = {
-      target = "Mic92";
-      cloneType = "user";
-      tokenFile = config.clan.core.vars.generators.ghorg-github-token.files.token.path;
-    };
-    clan = {
-      target = "clan";
-      scm = "gitea";
-      baseUrl = "https://git.clan.lol";
-      tokenFile = config.clan.core.vars.generators.ghorg-gitea-token.files.token.path;
-    };
-    nix-community = {
-      target = "nix-community";
-      tokenFile = config.clan.core.vars.generators.ghorg-github-token.files.token.path;
-      descriptionFilter = "Mic92";
-    };
-    numtide = {
-      target = "numtide";
-      tokenFile = config.clan.core.vars.generators.ghorg-github-token.files.token.path;
-      matchRegex = "^(llm-agents\\.nix|treefmt|treefmt-nix)$";
-    };
-    tum-dse = {
-      target = "TUM-DSE";
-      tokenFile = config.clan.core.vars.generators.ghorg-github-token.files.token.path;
-      matchRegex = "^doctor-cluster-config$";
-    };
-    mic92-forks = {
-      target = "Mic92";
-      cloneType = "user";
-      tokenFile = config.clan.core.vars.generators.ghorg-github-token.files.token.path;
-      matchRegex = "^(nix-1|nixpkgs)$";
-      skipForks = false;
-    };
-  };
 }
