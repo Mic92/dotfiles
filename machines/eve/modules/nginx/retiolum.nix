3e55ab4d8e69949bd01b86eb11659e1066305af9
Jörg Thalheim <joerg@thalheim.io>
2026-08-20 09:11:33 +0200

    remove no longer needed gitea hooks

--- machines/eve/modules/nginx/retiolum.nix
+++ machines/eve/modules/nginx/retiolum.nix
@@ -1,31 +1,7 @@
-{ pkgs, ... }:
 {
   services.nginx.virtualHosts."retiolum.thalheim.io" = {
     useACMEHost = "thalheim.io";
     forceSSL = true;
     root = "/var/www/retiolum.thalheim.io";
-  };
-
-  systemd.services.stockholm-autosync = {
-    startAt = "hourly";
-    script = ''
-      set -eu -o pipefail
-      readonly workdir=/var/lib/gitea/builds/stockholm-auto-update
-      readonly downstream=gitea@git.thalheim.io:Mic92/stockholm
-      readonly upstream=https://cgit.lassul.us/stockholm
-      rm -rf "$workdir"
-      git clone "$downstream" "$workdir"
-      cd "$workdir"
-      git pull --rebase "$upstream" master
-      git push --force origin master
-    '';
-    path = [
-      pkgs.gitMinimal
-      pkgs.openssh
-    ];
-    serviceConfig = {
-      Type = "oneshot";
-      User = "gitea";
-    };
   };
 }
