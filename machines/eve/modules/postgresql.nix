57ada4331b99c2a2db98f3cfc51051f223f3e32d
Jörg Thalheim <joerg@thalheim.io>
2026-09-06 09:05:58 +0200

    eve/postgres: switch to postgresql 18
    Change-Id: Ieaf30176af3a1825692c1bedf4a338bbce3b0771

--- machines/eve/modules/postgresql.nix
+++ machines/eve/modules/postgresql.nix
@@ -6,7 +6,7 @@
 }:
 {
   services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_14;
+  services.postgresql.package = pkgs.postgresql_18;
   services.postgresql.settings = {
     max_connections = "300";
     shared_buffers = "80MB";
